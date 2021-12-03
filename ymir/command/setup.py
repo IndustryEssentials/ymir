0a99bf90e3349ce658a20aa226d37476c2a1a135
Phoenix Huang <huang.xuan@intellif.com>
2021-12-04 07:59:11 +0800

    update command CI

--- ymir/command/setup.py
+++ ymir/command/setup.py
@@ -15,7 +15,7 @@ setup(
     version=version.__version__,
     python_requires=">=3.8.10",
     author_email="ymir-team@intellif.com",
-    description="mir: A data version control tool for Ymir",
+    description="mir: A data version control tool for YMIR",
     url="https://github.com/IndustryEssentials/ymir",
     packages=find_packages(exclude=["*tests*"]),
     install_requires=requirements,
