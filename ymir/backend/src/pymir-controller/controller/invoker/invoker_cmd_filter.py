5fdd89f54fb6e18fc90cd58cc6c74dfbcc4ee5ae
Chao Liu <kel@malong.com>
2021-12-07 17:26:52 +0800

    update

--- ymir/backend/src/pymir-controller/controller/invoker/invoker_cmd_filter.py
+++ ymir/backend/src/pymir-controller/controller/invoker/invoker_cmd_filter.py
@@ -21,12 +21,12 @@ class FilterBranchInvoker(BaseMirControllerInvoker):
                                      ],
                                      mir_root=self._repo_root)
 
-    # check father filter task
+    # check parent filter task
     def check_filter_task_work_dir(self):
-        father_task_type = backend_pb2.TaskType.Name(backend_pb2.TaskType.TaskTypeFilter)
-        father_work_dir = os.path.join(self._sandbox_root, "work_dir", father_task_type, self._request.dst_task_id)
-        if os.path.exists(father_work_dir):
-            return father_work_dir
+        parent_task_type = backend_pb2.TaskType.Name(backend_pb2.TaskType.TaskTypeFilter)
+        parent_work_dir = os.path.join(self._sandbox_root, "work_dir", parent_task_type, self._request.dst_task_id)
+        if os.path.exists(parent_work_dir):
+            return parent_work_dir
         else:
             return False
 
@@ -46,9 +46,9 @@ class FilterBranchInvoker(BaseMirControllerInvoker):
         if self._request.ex_class_ids:
             filter_command += " -P '{}'".format(';'.join(label_handler.get_main_labels_by_ids(self._request.ex_class_ids)))
 
-        father_work_dir = self.check_filter_task_work_dir()
-        if father_work_dir:
-            filter_command += f" -w {father_work_dir}"
+        parent_work_dir = self.check_filter_task_work_dir()
+        if parent_work_dir:
+            filter_command += f" -w {parent_work_dir}"
 
         return utils.run_command(filter_command)
 
