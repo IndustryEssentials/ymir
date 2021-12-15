cd3f53e9c96a4868c025e3a4029cc4e2d902f4d4
elloitli <li.shanlue@intellif.com>
2021-12-15 10:12:34 +0800

    mirror list; update icon; cn desc;

---
+++ ymir/web/src/locales/modules/mirror.ts
@@ -0,0 +1,22 @@
+const mirror = {
+  "mirrors.title": { en: "Mirorr List", cn: "镜像列表", },
+  "mirror.title": { en: "Mirorr Detail", cn: "镜像详情", },
+  "mirror.type.mining": { en: "Mining Mirror", cn: "挖掘镜像", },
+  "mirror.type.train": { en: "Training Mirror", cn: "训练镜像", },
+  "mirror.column.type": { en: "Mirror Type", cn: "镜像类型", },
+  "mirror.action.link": { en: "Link Mirror", cn: "关联镜像", },
+  "mirror.action.share": { en: "Share Mirror", cn: "共享镜像", },
+  "mirror.action.edit": { en: "Edit Mirror", cn: "编辑镜像", },
+  "mirror.action.del": { en: "Delete Mirror", cn: "删除镜像", },
+  "mirror.action.detail": { en: "Mirror Detail", cn: "镜像详情", },
+  "mirror.tab.my.title": { en: "Mirror Detail", cn: "我的镜像", },
+  "mirror.tab.public.title": { en: "Mirror Detail", cn: "公共镜像", },
+  "mirror.new.label": { en: "Mirror Detail", cn: "新增镜像", },
+  "mirror.list.item.type": { en: "Mirror Detail", cn: "镜像类型：", },
+  "mirror.list.item.remote": { en: "Mirror Detail", cn: "镜像地址：", },
+  "mirror.list.item.desc": { en: "Mirror Detail", cn: "描述：", },
+  "mirror.list.item.related": { en: "Mirror Detail", cn: "关联镜像：", },
+  "mirror.list.train.btn": { en: "Mirror Detail", cn: "去训练", },
+}
+
+export default mirror
--- ymir/web/src/pages/mirror/index.js
+++ ymir/web/src/pages/mirror/index.js
@@ -2,53 +2,39 @@ import React, { useEffect, useRef, useState } from "react"
 import { connect } from 'dva'
 import styles from "./index.less"
 import { Link, useHistory, useParams } from "umi"
-import { Form, Button, Input, Select, Table, Menu, Dropdown, Space, Modal, ConfigProvider, Row, Col, Radio, Tooltip, } from "antd"
-import {
-  PlusOutlined,
-  SearchOutlined,
-  SyncOutlined,
-} from "@ant-design/icons"
-import moment from "moment"
+import { Form, List, Input, Menu, Modal, Radio, Card, Skeleton, Space, Button, Pagination, Col, Row, } from "antd"
 
-import { numFormat } from "@/utils/number"
-import { format, getUnixTimeStamp } from "@/utils/date"
 import t from "@/utils/t"
-import { getTimes, getModelImportTypes } from '@/constants/query'
+import { getMirrorTypes } from '@/constants/query'
 import Breadcrumbs from "@/components/common/breadcrumb"
-import EmptyState from '@/components/empty/model'
-import EditBox from "../../components/form/editBox"
-import { ImportIcon, ShieldIcon, VectorIcon, TipsIcon, More1Icon, TreeIcon, EditIcon, DeleteIcon, FileDownloadIcon } from "../../components/common/icons"
-import Actions from "../../components/table/actions"
-import TypeTag from "../../components/task/typeTag"
+import { ImportIcon, ShieldIcon, VectorIcon, TipsIcon, More1Icon, TreeIcon, EditIcon, DeleteIcon, FileDownloadIcon, AddIcon, SearchIcon, MoreIcon, UserSettingsIcon, ShareIcon, LinkIcon } from "../../components/common/icons"
 
 const { confirm } = Modal
 const { useForm } = Form
 
+const tabsTitle = [
+  { tab: t('mirror.tab.my.title'), key: 'my', },
+  { tab: t('mirror.tab.public.title'), key: 'public', },
+]
+
 const initQuery = {
   name: "",
   type: "",
-  time: 0,
   offset: 0,
   limit: 20,
 }
 
-function Keyword({ getModels, delModel, updateModel }) {
+function Mirror({ getMirrors, delMirror, updateMirror }) {
   const { keyword } = useParams()
   const history = useHistory()
-  const [models, setModels] = useState([])
+  const [mirrors, setMirrors] = useState([])
   const [total, setTotal] = useState(0)
   const [form] = useForm()
+  const [active, setActive] = useState(tabsTitle[0].key)
   const [query, setQuery] = useState(initQuery)
-  const [current, setCurrent] = useState({})
-  const [showAdd, setSowAdd] = useState(false)
-  let [init, setInit] = useState(Boolean(keyword))
 
   /** use effect must put on the top */
   useEffect(() => {
-    if (init) {
-      setInit(false)
-      return
-    }
     getData()
   }, [query])
 
@@ -59,80 +45,8 @@ function Keyword({ getModels, delModel, updateModel }) {
     }
   }, [keyword])
 
-  const types = getModelImportTypes()
+  const types = getMirrorTypes()
 
-  const times = getTimes()
-
-  const renderSource = (type, record) => {
-    const target = types.find((t) => t.value === type)
-    if (!target) {
-      return type
-    }
-
-    if (target.value === 1) {
-      // train
-      return (
-        <Link to={`/home/task/detail/${record.task_id}`}>{t('model.type.train')}: {record.task_name}</Link>
-      )
-    } else {
-      return target.label
-    }
-  }
-
-  const columns = [
-    {
-      title: showTitle("model.column.name"),
-      key: "name",
-      dataIndex: "name",
-      className: styles[`column_name`],
-      render: (name, { id }) => (
-        <Link to={`/home/model/detail/${id}`}>{name}</Link>
-      ),
-      ellipsis: true,
-    },
-    {
-      title: showTitle("model.column.source"),
-      dataIndex: "source",
-      render: (type, { task_id, task_name }) => <TypeTag types={getModelImportTypes()} type={type} id={task_id} name={task_name} />,
-      ellipsis: true,
-    },
-    {
-      title: showTitle("model.column.target"),
-      dataIndex: "keywords",
-      render: (keywords) => {
-        const label = t('dataset.column.keyword.label', { keywords: keywords.join(', '), total: keywords.length })
-        return <Tooltip placement='left' title={label}
-          color='white' overlayInnerStyle={{ color: 'rgba(0,0,0,0.45)', fontSize: 12 }}
-          mouseEnterDelay={0.5}
-        >{label}</Tooltip>
-      },
-      ellipsis: {
-        showTitle: false,
-      },
-    },
-    {
-      title: showTitle("model.column.map"),
-      dataIndex: "map",
-      align: 'center',
-    },
-    {
-      title: showTitle("model.column.create_time"),
-      key: "create_datetime",
-      dataIndex: "create_datetime",
-      render: (datetime) => format(datetime),
-      width: 200,
-      align: 'center',
-    },
-    {
-      title: showTitle("model.column.action"),
-      key: "action",
-      dataIndex: "action",
-      render: (text, record) => <Actions menus={actionMenus(record)} />,
-      className: styles.tab_actions,
-      align: "center",
-      width: "280px",
-    },
-  ]
 
   const pageChange = ({ current, pageSize }) => {
     const limit = pageSize
@@ -140,75 +54,82 @@ function Keyword({ getModels, delModel, updateModel }) {
     setQuery((old) => ({ ...old, limit, offset }))
   }
 
-  function showTitle(str) {
-    return <strong>{t(str)}</strong>
-  }
-
   async function getData() {
     let params = {
       offset: query.offset,
       limit: query.limit,
-    }
-    if (query.source !== "") {
-      params.source = query.source
-    }
-    if (query.time) {
-      const now = moment()
-      const today = moment([now.year(), now.month(), now.date()])
-      const startTime = today.subtract(query.time - 1, 'd')
-      params.end_time = getUnixTimeStamp(now)
-      params.start_time = getUnixTimeStamp(startTime)
+      type: query.type,
     }
     if (query.name) {
       params.name = query.name
     }
-    const { items, total } = await getModels(params)
-    if (items) {
-      setModels(() => items)
+    // const result = await getMirrors(params)
+    // mock data
+    const result = {
+      items: [
+        { id: 0, name: 'mirror 0', type: 1, remote: 'docker hub name', desc: 'mirror desc', relative: [2, 3, 4, 5], state: 0, },
+        { id: 1, name: 'mirror 1', type: 1, remote: 'docker hub name', desc: 'mirror desc', relative: [2, 3, 4, 5], state: 1, },
+        { id: 2, name: 'mirror 2', type: 2, remote: 'docker hub name', desc: 'mirror desc', relative: [2, 3, 4, 5], state: 1, },
+        { id: 3, name: 'mirror 3', type: 2, remote: 'docker hub name', desc: 'mirror desc', relative: [2, 3, 4, 5], state: 1, },
+        { id: 4, name: 'mirror 4', type: 2, remote: 'docker hub name', desc: 'mirror desc', relative: [2, 3, 4, 5], state: 2, },
+        { id: 5, name: 'mirror 5', type: 2, remote: 'docker hub name', desc: 'mirror desc', relative: [2, 3, 4, 5], state: 3, },
+        { id: 6, name: 'mirror 6', type: 1, remote: 'docker hub name', desc: 'mirror desc', relative: [2, 3, 4, 5], state: 4, },
+        { id: 7, name: 'mirror 7', type: 2, remote: 'docker hub name', desc: 'mirror desc', relative: [2, 3, 4, 5], state: 0, },
+        { id: 8, name: 'mirror 8', type: 1, remote: 'docker hub name', desc: 'mirror desc', relative: [2, 3, 4, 5], state: 0, },
+        { id: 9, name: 'mirror 9', type: 1, remote: 'docker hub name', desc: 'mirror desc', relative: [2, 3, 4, 5], state: 0, },
+        { id: 10, name: 'mirror 10', type: 2, remote: 'docker hub name', desc: 'mirror desc', relative: [2, 3, 4, 5], state: 3, },
+        { id: 11, name: 'mirror 11', type: 1, remote: 'docker hub name', desc: 'mirror desc', relative: [2, 3, 4, 5], state: 3, },
+        { id: 12, name: 'mirror 12', type: 1, remote: 'docker hub name', desc: 'mirror desc', relative: [2, 3, 4, 5], state: 3, },
+        { id: 13, name: 'mirror 13', type: 2, remote: 'docker hub name', desc: 'mirror desc', relative: [2, 3, 4, 5], state: 3, },
+        { id: 14, name: 'mirror 14', type: 1, remote: 'docker hub name', desc: 'mirror desc', relative: [2, 3, 4, 5], state: 3, },
+        { id: 15, name: 'mirror 15', type: 1, remote: 'docker hub name', desc: 'mirror desc', relative: [2, 3, 4, 5], state: 3, },
+        { id: 16, name: 'mirror 16', type: 1, remote: 'docker hub name', desc: 'mirror desc', relative: [2, 3, 4, 5], state: 3, },
+        { id: 17, name: 'mirror 17', type: 1, remote: 'docker hub name', desc: 'mirror desc', relative: [2, 3, 4, 5], state: 3, },
+        { id: 18, name: 'mirror 18', type: 1, remote: 'docker hub name', desc: 'mirror desc', relative: [2, 3, 4, 5], state: 3, },
+        { id: 19, name: 'mirror 19', type: 1, remote: 'docker hub name', desc: 'mirror desc', relative: [2, 3, 4, 5], state: 3, },
+      ],
+      total: 56,
+    }
+    if (result) {
+      const { items, total } = result
+      setMirrors(() => items)
       setTotal(total)
     }
   }
 
-  const actionMenus = (record) => {
-    const { id, name, url } = record
+  const moreList = (record) => {
+    const { id, name, type, remote } = record
     return [
       {
-        key: "verify",
-        label: t("model.action.verify"),
-        onclick: () => history.push(`/home/model/verify/${id}`),
-        icon: <ShieldIcon />,
+        key: "link",
+        label: t("mirror.action.link"),
+        onclick: () => history.push(`/home/mirror/detail/${id}`),
+        icon: <LinkIcon />,
       },
       {
-        key: "download",
-        label: t("model.action.download"),
-        link: url,
-        target: '_blank',
-        icon: <FileDownloadIcon />,
-      },
-      {
-        key: "mining",
-        label: t("dataset.action.mining"),
-        onclick: () => history.push(`/home/task/mining?mid=${id}`),
-        icon: <VectorIcon />,
-      },
-      {
-        key: "history",
-        label: t("dataset.action.history"),
-        onclick: () => history.push(`/home/history/model/${id}`),
-        icon: <TreeIcon />,
+        key: "share",
+        label: t("mirror.action.share"),
+        onclick: () => history.push('/home/mirror/share'),
+        icon: <ShareIcon />,
       },
       {
         key: "edit",
-        label: t("dataset.action.edit"),
-        onclick: () => edit(record),
+        label: t("mirror.action.edit"),
+        onclick: () => history.push(`/home/mirror/detail/${id}`),
         icon: <EditIcon />,
       },
       {
         key: "del",
-        label: t("dataset.action.del"),
+        label: t("mirror.action.del"),
         onclick: () => del(id, name),
         className: styles.action_del,
         icon: <DeleteIcon />,
+      },
+      {
+        key: "detail",
+        label: t("mirror.action.detail"),
+        onclick: () => history.push(`/home/model/verify/${id}`),
+        icon: <MoreIcon />,
       },
     ]
   }
@@ -223,9 +144,9 @@ function Keyword({ getModels, delModel, updateModel }) {
       icon: <TipsIcon style={{ color: 'rgb(242, 99, 123)' }} />,
       content: t("model.action.del.confirm.content", { name }),
       onOk: async () => {
-        const result = await delModel(id)
+        const result = await delMirror(id)
         if (result) {
-          setModels(models.filter((model) => model.id !== id))
+          setMirrors(mirrors.filter((model) => model.id !== id))
           setTotal(old => old - 1)
           getData()
         }
@@ -237,10 +158,10 @@ function Keyword({ getModels, delModel, updateModel }) {
 
 
   const saveName = async (record, name) => {
-    const result = await updateModel(record.id, name)
+    const result = await updateMirror(record.id, name)
     if (result) {
-      setModels((models) =>
-        models.map((model) => {
+      setMirrors((mirrors) =>
+        mirrors.map((model) => {
           if (model.id === record.id) {
             model.name = name
           }
@@ -280,109 +201,99 @@ function Keyword({ getModels, delModel, updateModel }) {
     form.resetFields()
   }
 
-  const moreActions = (record) => {
+  const more = (item) => {
+    console.log('item: ', item, moreList(item))
     return (
-      <Menu>
-        {moreActionsList(record).map((action) => (
-          <Menu.Item
+      <Space>
+        {moreList(item).map((action) => (
+          <a
+            type='link'
             className={action.className}
             key={action.key}
             onClick={action.onclick}
+            title={action.label}
           >
-            {action.label}
-          </Menu.Item>
+            {action.icon}
+          </a>
         ))}
-      </Menu>
+      </Space>
     )
   }
 
-  // const addBtn = (
-  //   <Button type="primary" onClick={add}>
-  //     <PlusOutlined /> {t("model.import.label")}
-  //   </Button>
-  // )
+  const addBtn = (
+    <div className={styles.addBtn}><AddIcon />{t('mirror.new.label')}</div>
+  )
+
+  const renderItem = (item) => {
+    const title = <Row wrap={false}>
+      <Col flex={1}>{item.name}</Col>
+      <Col>{more(item)}</Col>
+    </Row>
+    const desc = <Row><Col className={styles.desc} flex={1}>
+      <Space className={styles.info}>
+        <span className={styles.infoItem}><span className={styles.infoLabel}>{t('mirror.list.item.type')}</span>{item.type}</span>
+        <span className={styles.infoItem}><span className={styles.infoLabel}>{t('mirror.list.item.remote')}</span>{item.remote}</span>
+        <span className={styles.infoItem}><span className={styles.infoLabel}>{t('mirror.list.item.desc')}</span>{item.desc}</span>
+      </Space>
+      <div className={styles.related}>{t('mirror.list.item.related')}{item.relative.join(',')}</div>
+    </Col>
+      <Col><Button key='train' onClick={() => history.push(`/home/task/train?mirror=${item.id}`)}>{t('mirror.list.train.btn')}</Button></Col>
+    </Row>
+    return <List.Item className={item.state ? 'success' : 'failure'}>
+      <Skeleton active loading={item.loading}>
+        <List.Item.Meta title={title} description={desc}>
+        </List.Item.Meta>
+      </Skeleton>
+    </List.Item>
+  }
+
+  const myMirror = (
+    <div className={styles.my}>
+      {addBtn}
+      <List
+        className={styles.list}
+        dataSource={mirrors}
+        renderItem={renderItem}
+      />
+      <Pagination onChange={() => getData()} defaultCurrent={1} defaultPageSize={query.limit} total={total} showQuickJumper showSizeChanger></Pagination>
+    </div>
+  )
+
+  const publicMirror = ('')
+
+  const contents = {
+    my: myMirror,
+    public: publicMirror,
+  }
+
+  const searchPanel = (
+    <Form
+      name='queryForm'
+      form={form}
+      layout="inline"
+      initialValues={{ name: keyword || "" }}
+      onValuesChange={search}
+      size='large'
+      colon={false}
+    >
+      <Form.Item
+        name="type"
+        label={t("mirror.column.type")}
+      >
+        <Radio.Group options={types} optionType="button"></Radio.Group>
+      </Form.Item>
+      <Form.Item name="name" label={t('model.query.name')}>
+        <Input placeholder={t("model.query.name.placeholder")} allowClear suffix={<SearchIcon />} />
+      </Form.Item>
+    </Form>
+  )
 
   return (
-    <div className={styles.model}>
+    <div className={styles.mirror}>
       <Breadcrumbs />
-      <div className={styles.list}>
-        <div className={styles.search}>
-          <Form
-            name='queryForm'
-            form={form}
-            // layout="inline"
-            labelCol={{ flex: '100px' }}
-            initialValues={{ source: "", time: 0, name: keyword || "" }}
-            onValuesChange={search}
-            size='large'
-            colon={false}
-          >
-            <Row>
-              <Col className={styles.queryColumn} span={12}>
-                <Form.Item name="name" label={t('model.query.name')}>
-                  <Input placeholder={t("model.query.name.placeholder")} style={{ width: '80%' }} allowClear suffix={<SearchOutlined />} />
-                </Form.Item>
-              </Col>
-              <Col className={styles.queryColumn} span={12}>
-                <Form.Item
-                  name="time"
-                  label={t("model.column.create_time")}
-                >
-                  <Radio.Group options={times} optionType="button"></Radio.Group>
-                </Form.Item></Col>
-            </Row>
-          </Form>
-        </div>
-
-        {/* <Space hidden={true} className={styles.actions}>
-        {addBtn}
-      </Space> */}
-        <Row className={styles.refresh}>
-          <Col flex={1}>
-          </Col>
-          <Col>
-            <Button
-              type='text'
-              icon={<SyncOutlined style={{ color: 'rgba(0, 0, 0, 0.45)' }} />}
-              title={'Refresh'}
-              onClick={() => getData()}
-            ></Button>
-          </Col>
-        </Row>
-        <div className={styles.table}>
-          <ConfigProvider renderEmpty={() => <EmptyState />}>
-            <Table
-              dataSource={models}
-              onChange={({ current, pageSize }) =>
-                pageChange({ current, pageSize })
-              }
-              rowKey={(record) => record.id}
-              pagination={{
-                showQuickJumper: true,
-                showSizeChanger: true,
-                total: total,
-                // total: 500,
-                defaultPageSize: query.limit,
-                showTotal: (total) => t("model.pager.total.label", { total }),
-                defaultCurrent: 1,
-              }}
-              columns={columns}
-            ></Table>
-          </ConfigProvider>
-        </div>
-      </div>
-      <EditBox record={current} action={saveName}>
-        {current.source ? <Form.Item label={t('model.column.source')}>
-          <TypeTag types={types} type={current.source} id={current.id} name={current.task_name} />
-        </Form.Item> : null}
-        {current.keywords ? <Form.Item label={t('model.column.target')}>
-          {t('dataset.column.keyword.label', { keywords: current.keywords.join(', '), total: current.keywords.length })}
-        </Form.Item> : null}
-        <Form.Item label={t('model.column.map')}>
-          {current.map}
-        </Form.Item>
-      </EditBox>
-      {/* <Add visible={showAdd} type='local' cancel={() => setSowAdd(false)} ok={() => { resetQuery(); getData() }} /> */}
+      <Card tabList={tabsTitle} activeTabKey={active} onTabChange={(key) => setActive(key)} tabBarExtraContent={searchPanel}>
+        {contents[active]}
+      </Card>
     </div>
   )
 }
@@ -395,25 +306,25 @@ const props = (state) => {
 
 const actions = (dispatch) => {
   return {
-    getModels: (payload) => {
+    getMirrors: (payload) => {
       return dispatch({
-        type: 'model/getModels',
+        type: 'mirror/getMirrors',
         payload,
       })
     },
-    delModel: (payload) => {
+    delMirror: (payload) => {
       return dispatch({
-        type: 'model/delModel',
+        type: 'mirror/delMirror',
         payload,
       })
     },
-    updateModel: (id, name) => {
+    updateMirror: (id, name) => {
       return dispatch({
-        type: 'model/updateModel',
+        type: 'mirror/updateMirror',
         payload: { id, name },
       })
     },
   }
 }
 
-export default connect(props, actions)(Keyword)
+export default connect(props, actions)(Mirror)
