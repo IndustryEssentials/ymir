26bf3baca658ec00c1fd471c74ecd191bbefea0c
Kelvin <lc_kelvin@163.com>
2021-12-17 11:26:17 +0800

    add pull image

--- ymir/backend/src/pymir-controller/controller/invoker/invoker_cmd_pull_image.py
+++ ymir/backend/src/pymir-controller/controller/invoker/invoker_cmd_pull_image.py
@@ -1,11 +1,14 @@
+import json
+from typing import Optional
+
+import sentry_sdk
+import yaml
+
+from controller.config import IMAGE_INFER_CONFIG_PATH, IMAGE_MINING_CONFIG_PATH, IMAGE_TRAINING_CONFIG_PATH
 from controller.invoker.invoker_cmd_base import BaseMirControllerInvoker
 from controller.utils import checker, utils, code
-from proto import backend_pb2
-import yaml
-import json
-from controller.config import IMAGE_INFER_CONFIG_PATH, IMAGE_MINING_CONFIG_PATH, IMAGE_TRAINING_CONFIG_PATH
 from controller.utils.app_logger import logger
-import sentry_sdk
+from proto import backend_pb2
 
 
 class ImageHandler(BaseMirControllerInvoker):
@@ -13,7 +16,7 @@ class ImageHandler(BaseMirControllerInvoker):
         return checker.check_request(request=self._request, prerequisites=[checker.Prerequisites.CHECK_USER_ID],)
 
     @staticmethod
-    def get_image_config(raw_image_config):
+    def get_image_config(raw_image_config: str) -> Optional[str]:
         image_config = yaml.safe_load(raw_image_config)
         if not isinstance(image_config, dict):
             error_message = f"raw image config error: {raw_image_config}"
@@ -27,15 +30,29 @@ class ImageHandler(BaseMirControllerInvoker):
         if self._request.req_type != backend_pb2.CMD_PULL_IMAGE:
             raise RuntimeError("Mismatched req_type")
 
-        command = f"docker run --rm {self._request.singleton_op} cat {IMAGE_TRAINING_CONFIG_PATH}"
-
-        response = utils.run_command(command)
-
-        image_config = self.get_image_config(response.message)
-        if not image_config:
-            return utils.make_general_response(code.ResCode.CTR_ERROR_UNKNOWN, response.message)
+        if self._request.operate_task_type == backend_pb2.TaskType.TaskTypeTraining:
+            image_config_path = IMAGE_TRAINING_CONFIG_PATH
+        elif self._request.operate_task_type == backend_pb2.TaskType.TaskTypeMining:
+            image_config_path = IMAGE_MINING_CONFIG_PATH
+        elif self._request.operate_task_type == backend_pb2.TaskType.TaskTypeInfer:
+            image_config_path = IMAGE_INFER_CONFIG_PATH
         else:
-            return utils.make_general_response(code.ResCode.CTR_OK, image_config)
+            raise ValueError(f"operate_task_type error:{self._request.operate_task_type}")
+
+        config_command = f"docker run --rm {self._request.singleton_op} cat {image_config_path}"
+        config_response = utils.run_command(config_command)
+
+        image_config = self.get_image_config(config_response.message)
+        if image_config:
+            hash_command = f"docker images {self._request.singleton_op} --format {'{{.ID}}'} --no-trunc"
+            hash_response = utils.run_command(hash_command)
+
+            response = utils.make_general_response(code.ResCode.CTR_OK, config_response.message)
+            response.hash_id = hash_response.message
+        else:
+            response = utils.make_general_response(code.ResCode.CTR_INVALID_SERVICE_REQ, config_response.message)
+
+        return response
 
     def _repr(self) -> str:
-        return f"cmd_kill: user: {self._request.user_id}, kill executor_instance: {self._request.executor_instance}"
+        return f"image_pull: user: {self._request.user_id}, image_name: {self._request.singleton_op}"
