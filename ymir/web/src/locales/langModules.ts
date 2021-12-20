c64d64d1240d1c6d9579135c152aa3db1c3c4006
elloitli <li.shanlue@intellif.com>
2021-12-20 14:10:27 +0800

    remove repeated code

---
+++ ymir/web/src/locales/langModules.ts
@@ -0,0 +1,32 @@
+import trans from './trans'
+import common from "./modules/common"
+import dataset from "./modules/dataset"
+import errors from "./modules/errors"
+import login from "./modules/login"
+import model from "./modules/model"
+import routeTitle from "./modules/routeTitle"
+import signup from "./modules/signup"
+import task from "./modules/task"
+import breadcrumbs from "./modules/breadcrumbs"
+import portal from "./modules/portal"
+import keyword from './modules/keyword'
+import user from './modules/user'
+import mirror from './modules/mirror'
+import tip from './modules/tip'
+
+export default {
+  ...common,
+  ...routeTitle,
+  ...breadcrumbs,
+  ...portal,
+  ...login,
+  ...signup,
+  ...dataset,
+  ...model,
+  ...task,
+  ...errors,
+  ...keyword,
+  ...user,
+  ...mirror,
+  ...tip,
+}
