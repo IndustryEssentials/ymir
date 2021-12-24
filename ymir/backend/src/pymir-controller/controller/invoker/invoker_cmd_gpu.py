8a570299af79e1c0719c4680ee19ab9d954f4c45
Kelvin <lc_kelvin@163.com>
2021-12-24 15:53:32 +0800

    update

--- ymir/backend/src/pymir-controller/controller/invoker/invoker_cmd_gpu.py
+++ ymir/backend/src/pymir-controller/controller/invoker/invoker_cmd_gpu.py
@@ -1,19 +1,23 @@
 from controller.invoker.invoker_cmd_base import BaseMirControllerInvoker
-from controller.utils import checker, utils, gpu_utils, code
+from controller.utils import checker, gpu_utils, code
 from proto import backend_pb2
 
 
-class GPUInvoker(BaseMirControllerInvoker):
+class GPUInfoInvoker(BaseMirControllerInvoker):
     def pre_invoke(self) -> backend_pb2.GeneralResp:
         return checker.check_request(request=self._request, prerequisites=[checker.Prerequisites.CHECK_USER_ID],)
 
     def invoke(self) -> backend_pb2.GeneralResp:
-        if self._request.req_type != backend_pb2.CMD_GPU_GET:
-            raise RuntimeError("Mismatched req_type")
+        if self._request.req_type != backend_pb2.CMD_GPU_INFO_GET:
+            raise RuntimeError(f"Mismatched req_type {self._request.req_type}")
 
-        free_gpus = gpu_utils.GPUInfo.get_available_gpus()
+        available_gpus = gpu_utils.GPUInfo.get_available_gpus()
 
-        return utils.make_general_response(code.ResCode.CTR_OK, str(len(free_gpus)))
+        response = backend_pb2.GeneralResp()
+        response.code = code.ResCode.CTR_OK
+        response.available_gpu_counts = len(available_gpus)
+
+        return response
 
     def _repr(self) -> str:
         return f"gpu: user: {self._request.user_id}"
