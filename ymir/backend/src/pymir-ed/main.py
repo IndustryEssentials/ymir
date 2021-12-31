a54474a08cbdc59c31081e3a7c30e8ffd301a300
ymir <ymir@intellif.com>
2021-12-31 15:13:11 +0800

    wip

--- ymir/backend/src/pymir-ed/main.py
+++ ymir/backend/src/pymir-ed/main.py
@@ -1,10 +1,15 @@
 """ emtry point for pymir-ed (pymir events dispatcher) service """
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class TaskStatesEvent(BaseModel):
+    pass
 
 
 app_ed = FastAPI()
 
 @app_ed.post('/events/taskstate')
-async def root() -> str:
+async def root(tse: TaskStatesEvent) -> str:
     return 'hello, world'
