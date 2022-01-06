1358c0992519b91e0f8a7139aa6d8d67baa71397
elliotmessi <90443217+elliotmessi@users.noreply.github.com>
2022-01-06 15:59:41 +0800

    [web] 030 train model (#109)
    * temp save
    
    * temp save
    
    * save temp
    
    * temp save
    
    * temp save
    
    * verify: select image
    temp save
    
    * temp save
    
    * support cpu mining
    
    * temp save
    
    * temp save
    
    * model in train task
    
    * 504 error handle
    
    * tranlation in en
    
    * update translation

---
+++ ymir/web/src/pages/task/components/modelSelect.js
@@ -0,0 +1,82 @@
+import { Col, Row, Select } from 'antd'
+import { connect } from 'dva'
+import { useEffect, useState } from 'react'
+
+import t from '@/utils/t'
+
+
+const ModelSelect = ({ value, keywords = [], onChange = () => { }, getModels, ...resProps }) => {
+  const [options, setOptions] = useState([])
+  const [models, setModels] = useState([])
+
+  useEffect(() => {
+    fetchModels()
+  }, [])
+
+  useEffect(() => {
+    if (options.length === 1) {
+      value = options[0].value
+    }
+  }, [options])
+
+  useEffect(() => {
+    generateOptions()
+  }, [models])
+
+  useEffect(() => {
+    const same = models.filter(model => model.keywords.toString() === keywords.toString()) || []
+    const inter = models.filter(model => {
+      const kws = model.keywords
+      return kws.toString() !== keywords.toString() && kws.some(kw => keywords.includes(kw))
+    })
+    const diff = models.filter(model => model.keywords.every(kw => !keywords.includes(kw))) || []
+    setModels([...same, ...inter, ...diff])
+  }, [keywords])
+
+  useEffect(() => {
+    const opt = options.find(opt => opt.value === value)
+    onChange(value, opt?.model)
+  }, [value, options])
+
+  async function fetchModels() {
+    const params = {
+      offset: 0,
+      limit: 100000,
+    }
+    const result = await getModels(params)
+    if (result?.items) {
+      setModels(result.items)
+    }
+  }
+
+  function generateOptions() {
+    const opts = models.map(model => {
+      return {
+        label: <Row gutter={10} wrap={false}>
+          <Col flex={1}>{model.name}</Col>
+          <Col>mAP: <strong>{model.map}</strong></Col>
+          <Col>{t('model.column.target')}: {model.keywords.join(',')}</Col>
+        </Row>,
+        model,
+        value: model.id,
+      }
+    })
+    setOptions(opts)
+  }
+
+  return (
+    <Select value={value} {...resProps} onChange={(value, option) => onChange(value, option?.model)} options={options} allowClear></Select>
+  )
+}
+
+const actions = (dispatch) => {
+  return {
+    getModels(payload) {
+      return dispatch({
+        type: 'model/getModels',
+        payload,
+      })
+    }
+  }
+}
+export default connect(null, actions)(ModelSelect)
