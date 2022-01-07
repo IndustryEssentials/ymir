de926938df179dbc7a9cdfbd1942a6e2e48cffd4
Phoenix <89957974+phoenix-xhuang@users.noreply.github.com>
2022-01-07 16:42:23 +0800

    Update deploy script (#121)
    * update local config to run server.py
    
    * ignore
    
    * fix python version issue
    
    * Readability update
    
    * Readability update
    
    * update docker-compose
    
    * update exportter pattern
    
    * add ls format
    
    * add export annotation field
    
    * update field
    
    * update deploy scripts
    
    * fix dockerfile
    
    * update source

--- ymir.sh
+++ ymir.sh
@@ -16,7 +16,6 @@ docker-compose down
 pre_start() {
 docker pull ${EXECUTOR_TRAINING}
 docker pull ${EXECUTOR_MINING}
-docker-compose pull
 stop
 }
 
@@ -50,6 +49,22 @@ done
 start() {
 check_permission
 pre_start
+
+if [[ $1 -eq 'dev' ]]; then
+    echo '\nin dev mode, building images.\n'
+    docker build \
+        -t industryessentials/ymir-backend \
+        --build-arg PIP_SOURCE='https://mirrors.aliyun.com/pypi/simple' \
+        --build-arg SERVER_MODE='dev' \
+        git@github.com:IndustryEssentials/ymir.git#dev:/ymir/backend
+    docker build \
+        -t industryessentials/ymir-web \
+        --build-arg NPM_REGISTRY='https://registry.npmmirror.com' \
+        git@github.com:IndustryEssentials/ymir.git#dev:/ymir/web
+else
+    echo '\nin prod mode, pulling images.\n'
+    docker-compose pull
+fi
 docker-compose up -d
 }
 
@@ -62,7 +77,7 @@ main() {
     if [[ $# -eq 0 ]]; then
         print_help
     else
-        $1
+        "$@"
     fi
 }
 
