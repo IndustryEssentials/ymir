26da8849a89db41079f01c89be0cb86386e08562
ymir <ymir@intellif.com>
2022-01-13 23:40:15 +0800

    fix mypy errors

--- ymir/backend/src/ymir-postman/postman/event_dispatcher.py
+++ ymir/backend/src/ymir-postman/postman/event_dispatcher.py
@@ -1,7 +1,7 @@
 """ event dispatcher """
 
 import logging
-from typing import Any, Callable, Optional, Set
+from typing import Any, Callable, Set
 
 import redis
 
@@ -14,7 +14,7 @@ class EventDispatcher:
         if not event_name:
             raise ValueError('empty event name')
 
-        self._event_handler: Optional[Callable] = None
+        self._event_handler: Any = None
         self._event_name = event_name
         self._group_name = f"group:{event_name}"
         self._redis_connect: redis.Redis = self.get_redis_connect()
@@ -85,6 +85,10 @@ class EventDispatcher:
             self._handle_msgs_and_remove(kvs)
 
     def _handle_msgs_and_remove(self, kvs: Any) -> None:
+        if not self._event_handler:
+            logging.warning('EventDispatcher: handler not set')
+            return
+
         for _, stream_msgs in kvs:
             if not stream_msgs:
                 continue
