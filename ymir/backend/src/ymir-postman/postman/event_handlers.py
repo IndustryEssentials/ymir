e2a03be5c2a2937822dda60568349a37a7baf0fe
ymir <ymir@intellif.com>
2022-01-14 18:51:17 +0800

    code review update

--- ymir/backend/src/ymir-postman/postman/event_handlers.py
+++ ymir/backend/src/ymir-postman/postman/event_handlers.py
@@ -21,7 +21,6 @@ def on_task_state(ed: event_dispatcher.EventDispatcher, mid_and_msgs: list, **kw
     """
     _, msgs = zip(*mid_and_msgs)
     tid_to_taskstates_latest = _aggregate_msgs(msgs)
-    logging.debug(f"latest: {tid_to_taskstates_latest}")
 
     # update db, if error occured, write back
     failed_tids = None
@@ -43,7 +42,6 @@ def _aggregate_msgs(msgs: List[Dict[str, str]]) -> Dict[str, entities.TaskState]
     for all redis stream msgs, deserialize them to entities, select the latest for each tid
     """
     tid_to_taskstates_latest: Dict[str, entities.TaskState] = _load_failed()
-    logging.debug(f"_aggregate_msgs: previous failed: {list(tid_to_taskstates_latest.keys())}")
     for msg in msgs:
         msg_topic = msg['topic']
         if msg_topic != 'raw' and msg_topic != 'selected':
@@ -80,7 +78,6 @@ def _load_failed() -> Dict[str, entities.TaskState]:
     """
     # pass
     json_str = redis_connect.get(name=settings.RETRY_CACHE_KEY)
-    logging.debug(f"_load_failed: {json_str}")
     if not json_str:
         return {}
     failed_tid_to_taskstates = parse_raw_as(Dict[str, entities.TaskState], json_str)
@@ -119,25 +116,25 @@ def _update_db_single_task(tid: str, task: entities.TaskState, custom_headers: d
         Tuple[str, str, bool]: tid, error_message, need_to_retry
     """
     url = f"http://{settings.APP_API_HOST}/api/v1/tasks/status"
+    # try:
+    # task_data: see api: /api/v1/tasks/status
+    task_data = {
+        'hash': tid,
+        'timestamp': task.percent_result.timestamp,
+        'state': tasks_util.task_state_str_to_code(task.percent_result.state),
+        'percent': task.percent_result.percent,
+        'state_message': task.percent_result.state_message
+    }
+
     try:
-        # task_data: see api: /api/v1/tasks/status
-        task_data = {
-            'hash': tid,
-            'timestamp': task.percent_result.timestamp,
-            'state': tasks_util.task_state_str_to_code(task.percent_result.state),
-            'percent': task.percent_result.percent,
-            'state_message': task.percent_result.state_message
-        }
-
-        logging.info(f"_update_db_single_task: request: {task_data}")
         response = requests.post(url=url, headers=custom_headers, json=task_data)
-        logging.info(f"_update_db_single_task: response: {response.text}")
-
-        response_obj = json.loads(response.text)
-        return_code = int(response_obj['code'])
-        return_msg = response_obj.get('message', '')
-
-        return (tid, return_msg, return_code != 0)
     except requests.exceptions.RequestException as e:
         logging.exception(msg='_update_db_single_task error')
         return (tid, f"{type(e).__name__}: {e}", True)
+
+    response_obj = json.loads(response.text)
+    return_code = int(response_obj['code'])
+    return_msg = response_obj.get('message', '')
+
+    return (tid, return_msg, return_code != 0)
+
