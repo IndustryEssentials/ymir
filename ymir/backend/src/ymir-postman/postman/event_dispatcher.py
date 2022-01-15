edaccf95339b280b757669948f7467911ba0bdaa
Fenrir <90444968+fenrir-z@users.noreply.github.com>
2022-01-15 14:41:04 +0800

    [backend] event push (#142)
    Co-authored-by: under-chaos <90446262+under-chaos@users.noreply.github.com>
    Co-authored-by: ymir <ymir@intellif.com>

---
+++ ymir/backend/src/ymir-postman/postman/event_dispatcher.py
@@ -0,0 +1,84 @@
+""" event dispatcher """
+
+import logging
+from typing import Any, Callable
+
+import redis
+
+from postman.settings import settings
+
+
+# public: class EventDispatcher
+class EventDispatcher:
+    def __init__(self, event_name: str) -> None:
+        if not event_name:
+            raise ValueError('empty event name')
+
+        self._event_handler: Any = None
+        self._event_name = event_name
+        self._group_name = f"group:{event_name}"
+        self._redis_connect: redis.Redis = self.get_redis_connect()
+        self._config_stream_and_group()
+
+    # public: general
+    def start(self) -> None:
+        """ start to listen """
+        logging.debug(f"ed start. event: {self._event_name}, group: {self._group_name}")
+        self._read_redis_stream_msgs()
+
+    @classmethod
+    def get_redis_connect(cls) -> redis.Redis:
+        return redis.StrictRedis.from_url(settings.PM_REDIS_URI, encoding="utf8", decode_responses=True)
+
+    def register_handler(self, handler: Callable) -> None:
+        if self._event_handler:
+            raise RuntimeError('already have handler')
+        self._event_handler = handler
+
+    @classmethod
+    def add_event(cls, event_name: str, event_topic: str, event_body: str) -> None:
+        cls.get_redis_connect().xadd(name=event_name,
+                                     fields={
+                                         'topic': event_topic,
+                                         'body': event_body
+                                     },
+                                     maxlen=settings.MAX_REDIS_STREAM_LENGTH,
+                                     approximate=True)
+
+    # private: redis stream and consumer group
+    def _config_stream_and_group(self) -> None:
+        # creates stream
+        self.add_event(event_name=self._event_name, event_topic=settings.EVENT_TOPIC_INNER, event_body='')
+        # creates stream consumer group
+        try:
+            self._redis_connect.xgroup_create(name=self._event_name, groupname=self._group_name, id='$')
+        except redis.ResponseError as e:
+            logging.debug(f"xgroup_create: {e}")
+
+    def _read_redis_stream_msgs(self) -> None:
+        kvs = self._redis_connect.xreadgroup(groupname=self._group_name,
+                                             consumername='default',
+                                             streams={self._event_name: '0'})
+        self._handle_msgs_and_remove(kvs)
+
+        while True:
+            kvs = self._redis_connect.xreadgroup(groupname=self._group_name,
+                                                 consumername='default',
+                                                 streams={self._event_name: '>'},
+                                                 block=0)
+            self._handle_msgs_and_remove(kvs)
+
+    def _handle_msgs_and_remove(self, kvs: Any) -> None:
+        if not self._event_handler:
+            logging.warning('EventDispatcher: handler not set')
+            return
+
+        for _, stream_msgs in kvs:
+            if not stream_msgs:
+                continue
+
+            self._event_handler(ed=self, mid_and_msgs=stream_msgs)
+
+            msg_ids, *_ = zip(*stream_msgs)
+            self._redis_connect.xack(self._event_name, self._group_name, *msg_ids)
+            self._redis_connect.xdel(self._event_name, *msg_ids)
---
+++ ymir/backend/src/ymir-postman/postman/event_handlers.py
@@ -0,0 +1,128 @@
+import json
+import logging
+import requests
+import time
+from typing import Any, List, Set, Dict, Tuple
+
+from fastapi.encoders import jsonable_encoder
+from pydantic import parse_raw_as
+
+from postman import entities, event_dispatcher  # type: ignore
+from postman.settings import settings
+
+redis_connect = event_dispatcher.EventDispatcher.get_redis_connect()
+
+
+def on_task_state(ed: event_dispatcher.EventDispatcher, mid_and_msgs: list, **kwargs: Any) -> None:
+    _, msgs = zip(*mid_and_msgs)
+    tid_to_taskstates_latest = _aggregate_msgs(msgs)
+    if not tid_to_taskstates_latest:
+        return
+
+    # update db, save failed
+    logging.debug(f"about to update db: {tid_to_taskstates_latest}")
+    failed_tids = _update_db(tid_to_tasks=tid_to_taskstates_latest)
+    logging.debug(f"failed tids: {failed_tids}")
+    if failed_tids:
+        time.sleep(5)
+        _save_failed(failed_tids=failed_tids, tid_to_taskstates_latest=tid_to_taskstates_latest)
+
+
+def _aggregate_msgs(msgs: List[Dict[str, str]]) -> entities.TaskStateDict:
+    """
+    for all redis stream msgs, deserialize them to entities, select the latest for each tid
+    """
+    tid_to_taskstates_latest: entities.TaskStateDict = _load_failed()
+    for msg in msgs:
+        msg_topic = msg['topic']
+        if msg_topic != settings.EVENT_TOPIC_RAW:
+            continue
+
+        tid_to_taskstates = parse_raw_as(entities.TaskStateDict, msg['body'])
+        for tid, taskstate in tid_to_taskstates.items():
+            if (tid not in tid_to_taskstates_latest
+                    or tid_to_taskstates_latest[tid].percent_result.timestamp < taskstate.percent_result.timestamp):
+                tid_to_taskstates_latest[tid] = taskstate
+    return tid_to_taskstates_latest
+
+
+def _save_failed(failed_tids: Set[str], tid_to_taskstates_latest: entities.TaskStateDict) -> None:
+    """
+    save failed taskstates to redis cache
+
+    Args:
+        failed_tids (Set[str])
+        tid_to_taskstates_latest (entities.TaskStateDict)
+    """
+    failed_tid_to_tasks = {tid: tid_to_taskstates_latest[tid] for tid in failed_tids}
+    json_str = json.dumps(jsonable_encoder(failed_tid_to_tasks))
+    redis_connect.set(name=settings.RETRY_CACHE_KEY, value=json_str)
+
+
+def _load_failed() -> entities.TaskStateDict:
+    """
+    load failed taskstates from redis cache
+
+    Returns:
+        entities.TaskStateDict
+    """
+    json_str = redis_connect.get(name=settings.RETRY_CACHE_KEY)
+    if not json_str:
+        return {}
+
+    return parse_raw_as(entities.TaskStateDict, json_str) or {}
+
+
+def _update_db(tid_to_tasks: entities.TaskStateDict) -> Set[str]:
+    """
+    update db for all tasks in tid_to_tasks
+
+    Args:
+        tid_to_tasks (entities.TaskStateDict): key: tid, value: TaskState
+
+    Returns:
+        Set[str]: failed tids
+    """
+    failed_tids: Set[str] = set()
+    custom_headers = {'api-key': settings.APP_API_KEY}
+    for tid, task in tid_to_tasks.items():
+        *_, need_retry = _update_db_single_task(tid, task, custom_headers)
+        if need_retry:
+            failed_tids.add(tid)
+    return failed_tids
+
+
+def _update_db_single_task(tid: str, task: entities.TaskState, custom_headers: dict) -> Tuple[str, str, bool]:
+    """
+    update db for single task
+
+    Args:
+        tid (str): task id
+        task (entities.TaskState): task state
+        custom_headers (dict)
+
+    Returns:
+        Tuple[str, str, bool]: tid, error_message, need_to_retry
+    """
+    url = f"http://{settings.APP_API_HOST}/api/v1/tasks/status"
+    # try:
+    # task_data: see api: /api/v1/tasks/status
+    task_data = {
+        'hash': tid,
+        'timestamp': task.percent_result.timestamp,
+        'state': task.percent_result.state,
+        'percent': task.percent_result.percent,
+        'state_message': task.percent_result.state_message
+    }
+
+    try:
+        response = requests.post(url=url, headers=custom_headers, json=task_data)
+    except requests.exceptions.RequestException as e:
+        logging.exception(msg='_update_db_single_task error')
+        return (tid, f"{type(e).__name__}: {e}", True)
+
+    response_obj = json.loads(response.text)
+    return_code = int(response_obj['code'])
+    return_msg = response_obj.get('message', '')
+
+    return (tid, return_msg, return_code != 0)
