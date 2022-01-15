b604a3e27b37809ab56253cc005cfc880741455d
IJtLJZ8Rm4Yr <90443055+IJtLJZ8Rm4Yr@users.noreply.github.com>
2022-01-15 10:54:57 +0800

    [backend] update stats endpoint to use clickhouse backend (#145)
    * [backend] update stats endpoint to use clickhouse backend
    
    * fix typo
    
    * add clickhouse initial script
    
    * use int as task type

--- ymir/backend/src/ymir-app/app/config.py
+++ ymir/backend/src/ymir-app/app/config.py
@@ -10,6 +10,7 @@ class Settings(BaseSettings):
     NGINX_PREFIX: str = ""
     API_V1_STR: str = "/api/v1"
     DATABASE_URI: str = "sqlite:///app.db"
+    CLICKHOUSE_URI: str = "clickhouse"
     TOKEN_URL: str = "/auth/token"
     GRPC_CHANNEL: str = "controller:50066"
     DEFAULT_LIMIT: int = 20
