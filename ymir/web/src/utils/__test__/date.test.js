c47a702286448b167f19940e47bfa908d87515c6
elloitli <li.shanlue@intellif.com>
2022-01-15 14:23:31 +0800

    temp save

--- ymir/web/src/utils/__test__/date.test.js
+++ ymir/web/src/utils/__test__/date.test.js
@@ -1,8 +1,30 @@
-import { format } from "../date"
+import { format, getDateFromTimestamp } from "../date"
 import moment from 'moment'
 
 describe("utils: date", () => {
   it("function: format. transfer utc to local", () => {
+    const time = "2021-07-05T01:37:28.242Z" // utc format
+    const expected = moment(time).local().format('YYYY-MM-DD HH:mm:ss')
+    expect(format(time)).toBe(expected) // local date and time
+  })
+  it("function: getDateFromTimestamp. transfer utc to local", () => {
+    const timestamp = "1642226485" // utc format
+    const expected = moment(timestamp).local().format('YYYY-MM-DD HH:mm:ss')
+    expect(getDateFromTimestamp(timestamp)).toBe(expected) // local date and time
+
+    expect(getDateFromTimestamp())
+  })
+  it("function: getUnixTimeStamp. transfer utc to local", () => {
+    const time = "2021-07-05T01:37:28.242Z" // utc format
+    const expected = moment(time).local().format('YYYY-MM-DD HH:mm:ss')
+    expect(format(time)).toBe(expected) // local date and time
+  })
+  it("function: calTimeLeft. transfer utc to local", () => {
+    const time = "2021-07-05T01:37:28.242Z" // utc format
+    const expected = moment(time).local().format('YYYY-MM-DD HH:mm:ss')
+    expect(format(time)).toBe(expected) // local date and time
+  })
+  it("function: calDuration. transfer utc to local", () => {
     const time = "2021-07-05T01:37:28.242Z" // utc format
     const expected = moment(time).local().format('YYYY-MM-DD HH:mm:ss')
     expect(format(time)).toBe(expected) // local date and time
