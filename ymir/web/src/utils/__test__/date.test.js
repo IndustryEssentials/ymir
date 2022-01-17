bf541c53ee08576eb4d43cbe930d8f2c2a41dbaa
elloitli <li.shanlue@intellif.com>
2022-01-17 11:18:36 +0800

    temp save

--- ymir/web/src/utils/__test__/date.test.js
+++ ymir/web/src/utils/__test__/date.test.js
@@ -1,4 +1,4 @@
-import { format, getDateFromTimestamp } from "../date"
+import { format, getDateFromTimestamp,getUnixTimeStamp, calTimeLeft, calDuration, } from "../date"
 import moment from 'moment'
 
 describe("utils: date", () => {
@@ -9,7 +9,7 @@ describe("utils: date", () => {
   })
   it("function: getDateFromTimestamp. transfer utc to local", () => {
     const timestamp = "1642226485" // utc format
-    const expected = moment(timestamp).local().format('YYYY-MM-DD HH:mm:ss')
+    const expected = moment(timestamp * 1000).local().format('YYYY-MM-DD HH:mm:ss')
     expect(getDateFromTimestamp(timestamp)).toBe(expected) // local date and time
 
     expect(getDateFromTimestamp())
