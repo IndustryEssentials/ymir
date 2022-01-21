3f1585f61c1249ea486e3d018fbcf06132757e7d
elliotmessi <90443217+elliotmessi@users.noreply.github.com>
2022-01-21 10:06:22 +0800

    [web] 040 unit test and other fixed (#226)
    unit test to ^90%
    update version

--- ymir/web/src/services/__test__/user.test.js
+++ ymir/web/src/services/__test__/user.test.js
@@ -1,43 +1,17 @@
-import { login, loginout, getMeInfo, signup, resetPwd, modifyPwd, forgetPwd } from "../user"
-import request from '@/utils/request'
+import { login, loginout, getMeInfo, signup, resetPwd, modifyPwd, forgetPwd, getUsers, setUserState } from "../user"
 
-jest.mock('@/utils/request', () => {
-  const req = jest.fn()
-  req.get = jest.fn()
-  req.post = jest.fn()
-  return req
-})
+import { products, requestExample } from './func'
 
 describe("service: user", () => {
   it("login -> success", () => {
     const params = { username: "idol", password: "1q2w3e" }
     const expected = "token"
-    request.mockImplementationOnce(() => {
-      return Promise.resolve({
-        code: 0,
-        result: {
-          access_token: expected,
-        },
-      })
-    })
-
-    login(params).then(({ result }) => {
-      expect(result.access_token).toBe(expected)
-    })
+    requestExample(login, params, expected)
   })
   it("loginout -> success", () => {
     const params = {}
     const expected = "ok"
-    request.post.mockImplementationOnce(() => {
-      return Promise.resolve({
-        code: 0,
-        result: expected,
-      })
-    })
-
-    loginout(params).then(({ result }) => {
-      expect(result).toBe(expected)
-    })
+    requestExample(loginout, params, expected, 'post')
   })
   it("getMeInfo -> success", () => {
     const expected = {
@@ -46,79 +20,42 @@ describe("service: user", () => {
       phone: "13245678908",
       id: 1345,
     }
-    request.get.mockImplementationOnce(() => {
-      return Promise.resolve({
-        code: 0,
-        result: expected,
-      })
-    })
-
-    getMeInfo().then(({ code, result }) => {
-      expect(result).toEqual(expected)
-    })
+    requestExample(getMeInfo, null, expected, 'get')
   })
 
   it('signup -> success', () => {
     const params = { username: "idol", password: "password", email: 'test@test.com' }
     const expected = { access_token: 'itisaaccesstoken' }
-    request.post.mockImplementationOnce(() => {
-      return Promise.resolve({
-        code: 0,
-        result: expected,
-      })
-    })
-
-    signup(params).then(({ code, result }) => {
-      expect(code).toBe(0)
-      expect(result).toEqual(expected)
-    })
+    requestExample(signup, params, expected, 'post')
   })
   it('forgetPwd -> success', () => {
     
     const params = { email: 'test@test.com' }
     const expected = "ok"
-    request.post.mockImplementationOnce(() => {
-      return Promise.resolve({
-        code: 0,
-        result: expected,
-      })
-    })
-
-    forgetPwd(params).then(({ code, result }) => {
-      expect(code).toBe(0)
-      expect(result).toEqual(expected)
-    })
+    requestExample(forgetPwd, params, expected, 'post')
   })
   it('modifyPwd -> success', () => {
     
     const password = 'newpassword'
     const expected = "ok"
-    request.mockImplementationOnce(() => {
-      return Promise.resolve({
-        code: 0,
-        result: expected,
-      })
-    })
-
-    modifyPwd(password).then(({ code, result }) => {
-      expect(code).toBe(0)
-      expect(result).toEqual(expected)
-    })
+    requestExample(modifyPwd, password, expected)
   })
   it('resetPwd -> success', () => {
     
     const params = { old: 'oldpassword', newPassword: 'newpassword' }
     const expected = "ok"
-    request.post.mockImplementationOnce(() => {
-      return Promise.resolve({
-        code: 0,
-        result: expected,
-      })
-    })
-
-    resetPwd(params).then(({ code, result }) => {
-      expect(code).toBe(0)
-      expect(result).toEqual(expected)
-    })
+    requestExample(resetPwd, params, expected, 'post')
   })
 })
+  it('getUsers -> success', () => {
+    
+    const params = { state: 2 }
+    const expected = products(19)
+    requestExample(getUsers, params, expected, 'get')
+  })
+  it('setUserState -> success', () => {
+    
+    const params = { username: 'new rosa' }
+    const expected = "ok"
+    requestExample(setUserState, params, expected)
+  })
