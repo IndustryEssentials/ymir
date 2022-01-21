be923932b339a58b94fca15a3e4e7a5287a75e39
Fenrir <90444968+fenrir-z@users.noreply.github.com>
2022-01-21 09:24:39 +0800

    [backend/postman] fix socketio (#217)
    * wip
    
    * sio happened after update db success
    
    * finished
    
    * tiny changed
    
    * bug fix
    
    * fix mypy error
    
    * revert gitignore
    
    * postman url get from settings, not hard coded
    
    * changed log
    
    * update
    
    * update
    
    * update
    
    * update
    
    * tiny changed
    
    * removed TaskStateEventPayloadData
    
    Co-authored-by: ymir <ymir@intellif.com>
    Co-authored-by: Phoenix <89957974+phoenix-xhuang@users.noreply.github.com>

--- ymir/backend/src/ymir-postman/postman/event_handlers.py
+++ ymir/backend/src/ymir-postman/postman/event_handlers.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+from enum import IntEnum
 import json
 import logging
 import requests
@@ -10,6 +12,32 @@ from pydantic import parse_raw_as
 from postman import entities, event_dispatcher  # type: ignore
 from postman.settings import constants, settings
 
+
+class _UpdateDbConclusion(IntEnum):
+    SUCCESS = 0  # update success
+    RETRY = 1  # update failed, need retry
+    DROP = 2  # update failed, need drop
+
+
+def _conclusion_from_return_code(return_code: int) -> _UpdateDbConclusion:
+    if return_code == constants.RC_OK:
+        return _UpdateDbConclusion.SUCCESS
+    elif return_code == constants.RC_FAILED_TO_UPDATE_TASK_STATUS:
+        return _UpdateDbConclusion.RETRY
+    else:
+        return _UpdateDbConclusion.DROP
+
+
+class _UpdateDbResult:
+    def __init__(self) -> None:
+        self.success_tids: Set[str] = set()
+        self.retry_tids: Set[str] = set()
+        self.drop_tids: Set[str] = set()
+
+    def __repr__(self) -> str:
+        return f"success: {self.success_tids}, retry: {self.retry_tids}, drop: {self.drop_tids}"
+
+
 redis_connect = event_dispatcher.EventDispatcher.get_redis_connect()
 
 
@@ -20,19 +48,19 @@ def on_task_state(ed: event_dispatcher.EventDispatcher, mid_and_msgs: list, **kw
         return
 
     # update db, save failed
-    logging.debug(f"about to update db: {tid_to_taskstates_latest}")
-    failed_tids = _update_db(tid_to_tasks=tid_to_taskstates_latest)
-    logging.debug(f"failed tids: {failed_tids}")
-    if failed_tids:
+    update_db_result = _update_db(tid_to_tasks=tid_to_taskstates_latest)
+    logging.info(f"update db result: {update_db_result}")
+    _update_sio(tids=update_db_result.success_tids, tid_to_taskstates=tid_to_taskstates_latest)
+    if update_db_result.retry_tids:
         time.sleep(5)
-        _save_failed(failed_tids=failed_tids, tid_to_taskstates_latest=tid_to_taskstates_latest)
+    _update_retry(retry_tids=update_db_result.retry_tids, tid_to_taskstates_latest=tid_to_taskstates_latest)
 
 
 def _aggregate_msgs(msgs: List[Dict[str, str]]) -> entities.TaskStateDict:
     """
     for all redis stream msgs, deserialize them to entities, select the latest for each tid
     """
-    tid_to_taskstates_latest: entities.TaskStateDict = _load_failed()
+    tid_to_taskstates_latest: entities.TaskStateDict = _load_retry()
     for msg in msgs:
         msg_topic = msg['topic']
         if msg_topic != constants.EVENT_TOPIC_RAW:
@@ -46,20 +74,21 @@ def _aggregate_msgs(msgs: List[Dict[str, str]]) -> entities.TaskStateDict:
     return tid_to_taskstates_latest
 
 
-def _save_failed(failed_tids: Set[str], tid_to_taskstates_latest: entities.TaskStateDict) -> None:
+# private: update db
+def _update_retry(retry_tids: Set[str], tid_to_taskstates_latest: entities.TaskStateDict) -> None:
     """
     save failed taskstates to redis cache
 
     Args:
-        failed_tids (Set[str])
+        retry_tids (Set[str])
         tid_to_taskstates_latest (entities.TaskStateDict)
     """
-    failed_tid_to_tasks = {tid: tid_to_taskstates_latest[tid] for tid in failed_tids}
-    json_str = json.dumps(jsonable_encoder(failed_tid_to_tasks))
+    retry_tid_to_tasks = {tid: tid_to_taskstates_latest[tid] for tid in retry_tids if tid in tid_to_taskstates_latest}
+    json_str = json.dumps(jsonable_encoder(retry_tid_to_tasks))
     redis_connect.set(name=settings.RETRY_CACHE_KEY, value=json_str)
 
 
-def _load_failed() -> entities.TaskStateDict:
+def _load_retry() -> entities.TaskStateDict:
     """
     load failed taskstates from redis cache
 
@@ -73,7 +102,7 @@ def _load_failed() -> entities.TaskStateDict:
     return parse_raw_as(entities.TaskStateDict, json_str) or {}
 
 
-def _update_db(tid_to_tasks: entities.TaskStateDict) -> Set[str]:
+def _update_db(tid_to_tasks: entities.TaskStateDict) -> _UpdateDbResult:
     """
     update db for all tasks in tid_to_tasks
 
@@ -81,18 +110,23 @@ def _update_db(tid_to_tasks: entities.TaskStateDict) -> Set[str]:
         tid_to_tasks (entities.TaskStateDict): key: tid, value: TaskState
 
     Returns:
-        Set[str]: failed tids
+        _UpdateDbResult: update db result (success, retry and drop tids)
     """
-    failed_tids: Set[str] = set()
+    update_db_result = _UpdateDbResult()
     custom_headers = {'api-key': settings.APP_API_KEY}
     for tid, task in tid_to_tasks.items():
-        *_, need_retry = _update_db_single_task(tid, task, custom_headers)
-        if need_retry:
-            failed_tids.add(tid)
-    return failed_tids
+        *_, code = _update_db_single_task(tid, task, custom_headers)
+        if code == _UpdateDbConclusion.SUCCESS:
+            update_db_result.success_tids.add(tid)
+        elif code == _UpdateDbConclusion.RETRY:
+            update_db_result.retry_tids.add(tid)
+        else:
+            update_db_result.drop_tids.add(tid)
+
+    return update_db_result
 
 
-def _update_db_single_task(tid: str, task: entities.TaskState, custom_headers: dict) -> Tuple[str, str, bool]:
+def _update_db_single_task(tid: str, task: entities.TaskState, custom_headers: dict) -> Tuple[str, _UpdateDbConclusion]:
     """
     update db for single task
 
@@ -102,10 +136,10 @@ def _update_db_single_task(tid: str, task: entities.TaskState, custom_headers: d
         custom_headers (dict)
 
     Returns:
-        Tuple[str, str, bool]: tid, error_message, need_to_retry
+        Tuple[str, _UpdateDbConclusion]: error_message, result conclusion (success, retry or drop)
     """
     url = f"http://{settings.APP_API_HOST}/api/v1/tasks/status"
-    # try:
+
     # task_data: see api: /api/v1/tasks/status
     task_data = {
         'hash': tid,
@@ -115,14 +149,44 @@ def _update_db_single_task(tid: str, task: entities.TaskState, custom_headers: d
         'state_message': task.percent_result.state_message
     }
 
+    logging.debug(f"update db single task request: {task_data}")
     try:
         response = requests.post(url=url, headers=custom_headers, json=task_data)
     except requests.exceptions.RequestException as e:
-        logging.exception(msg='_update_db_single_task error')
-        return (tid, f"{type(e).__name__}: {e}", True)
+        logging.exception(msg=f"update db single task error ignored: {tid}, {e}")
+        return (f"{type(e).__name__}: {e}", _UpdateDbConclusion.RETRY)
 
     response_obj = json.loads(response.text)
     return_code = int(response_obj['code'])
     return_msg = response_obj.get('message', '')
 
-    return (tid, return_msg, return_code == constants.RC_FAILED_TO_UPDATE_TASK_STATUS)
+    return (return_msg, _conclusion_from_return_code(return_code))
+
+
+# private: socketio
+def _update_sio(tids: Set[str], tid_to_taskstates: entities.TaskStateDict) -> None:
+    if not tids:
+        return
+
+    event_payloads = _get_event_payloads({tid: tid_to_taskstates[tid] for tid in tids if tid in tid_to_taskstates})
+
+    url = f"{settings.PM_URL}/events/push"
+    try:
+        requests.post(url=url, json=jsonable_encoder(event_payloads))
+    except requests.exceptions.RequestException:
+        logging.exception('update sio error ignored')
+
+
+def _get_event_payloads(tid_to_taskstates: entities.TaskStateDict) -> entities.EventPayloadList:
+    # sort by user
+    uid_to_taskdatas: Dict[str, Dict[str, entities.TaskStatePercent]] = defaultdict(dict)
+    for tid, taskstate in tid_to_taskstates.items():
+        uid = taskstate.task_extra_info.user_id
+        uid_to_taskdatas[uid][tid] = taskstate.percent_result
+
+    # get event payloads
+    event_payloads = []
+    for uid, tid_to_taskdatas in uid_to_taskdatas.items():
+        event_payloads.append(
+            entities.EventPayload(event='update_taskstate', namespace=f"/{uid}", data=tid_to_taskdatas))
+    return event_payloads
