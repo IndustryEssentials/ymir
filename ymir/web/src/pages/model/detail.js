cdca2f8a45825df399cc2f8364abc68730e3c1c2
elliotmessi <90443217+elliotmessi@users.noreply.github.com>
2022-01-24 11:16:53 +0800

    [web] longer task, dataset, model name and remove unused code (#234)
    * 1. max lenth of task name and dataset name
    2. remove part of unused code
    3. set relative path to comfirm mark path
    
    * keep relative path
    
    Co-authored-by: Phoenix <89957974+phoenix-xhuang@users.noreply.github.com>

--- ymir/web/src/pages/model/detail.js
+++ ymir/web/src/pages/model/detail.js
@@ -4,7 +4,7 @@ import { connect } from 'dva'
 import { useParams, Link, useHistory } from "umi"
 
 import t from "@/utils/t"
-import Breadcrumbs from "../../components/common/breadcrumb"
+import Breadcrumbs from "@/components/common/breadcrumb"
 import TripleRates from "@/components/form/tripleRates"
 import styles from "./detail.less"
 
