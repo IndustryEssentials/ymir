e4e5e4f9b3c10741d74eb39d8d235ad535a72492
Phoenix <89957974+phoenix-xhuang@users.noreply.github.com>
2022-01-24 11:07:40 +0800

    [backend] fix bugs (#232)
    * fix bug
    
    * fix bug
    
    * comments
    
    * fix script bug
    
    * fix service when empty uuid
    
    * fix bug
    
    * update
    
    * udpate
    
    * format
    
    * formatter
    
    * update
    
    * update
    
    * update
    
    * update
    
    * fix bugs
    
    Co-authored-by: under-chaos <90446262+under-chaos@users.noreply.github.com>

--- ymir/backend/src/ymir-controller/controller/config/gpu_task.py
+++ ymir/backend/src/ymir-controller/controller/config/gpu_task.py
@@ -6,4 +6,4 @@ GPU_LOCKING_SET = "gpu_locking_set"
 # gpu usage for dispach
 GPU_USAGE_THRESHOLD = float(os.environ.get("GPU_USAGE_THRESHOLD", 0.8))
 # gpu lock time
-GPU_LOCK_MINUTES = int(os.environ.get("GPU_LOCK_MINUTES", 1))
+GPU_LOCK_MINUTES = int(os.environ.get("GPU_LOCK_MINUTES", 3))
