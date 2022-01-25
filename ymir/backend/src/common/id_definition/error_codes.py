afa70c78e2abebbee10ef3a09d206d6c9323e8a4
Kelvin <lc_kelvin@163.com>
2022-01-25 10:46:29 +0800

    update

--- ymir/backend/src/common/id_definition/error_codes.py
+++ ymir/backend/src/common/id_definition/error_codes.py
@@ -1,10 +1,13 @@
-class VizErrorCode:
+from enum import IntEnum
+
+
+class VizErrorCode(IntEnum):
     general_error = 140400
     branch_not_exists = 140401
     internal_error = 140500
 
 
-class MonitorErrorCode:
+class MonitorErrorCode(IntEnum):
     general_error = 150400
     duplicate_task_id = 150401
     percent_log_error = 150402
