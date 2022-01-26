7e8ce5a22d034cb0e5a827bc6e18e12f41ded117
IJtLJZ8Rm4Yr <90443055+IJtLJZ8Rm4Yr@users.noreply.github.com>
2022-01-26 18:11:30 +0800

    [backend] refactor handle task result process (#242)
    * [backend] refactor task result handler
    
    * [backend] refactor handle task result process
    
    Co-authored-by: under-chaos <90446262+under-chaos@users.noreply.github.com>

--- ymir/backend/src/ymir-app/app/models/task.py
+++ ymir/backend/src/ymir-app/app/models/task.py
@@ -18,10 +18,11 @@ from app.db.base_class import Base
 class Task(Base):
     __tablename__ = "task"
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    name = Column(String(settings.NAME_LEN_LIMIT), index=True)
-    hash = Column(String(settings.HASH_LEN_LIMIT), index=True)
+    name = Column(String(settings.NAME_LEN_LIMIT), index=True, nullable=False)
+    hash = Column(String(settings.HASH_LEN_LIMIT), index=True, nullable=False)
     type = Column(Integer, index=True)
     state = Column(Integer, index=True)
+    error_code = Column(String(settings.DEFAULT_LIMIT))
     progress = Column(SmallInteger)
     parameters = Column(Text(settings.PARA_LEN_LIMIT))
     config = Column(Text(settings.PARA_LEN_LIMIT))
