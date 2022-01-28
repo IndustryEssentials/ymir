a2c3eb083ed89a8fb1da68f6b1e9ca02eb9c8428
Phoenix <89957974+phoenix-xhuang@users.noreply.github.com>
2022-01-28 12:06:13 +0800

    update

--- ymir/backend/src/ymir-controller/tests/unit/test_invoker_task_mining.py
+++ ymir/backend/src/ymir-controller/tests/unit/test_invoker_task_mining.py
@@ -110,7 +110,7 @@ class TestInvokerTaskMining(unittest.TestCase):
         working_dir_1 = os.path.join(working_dir_root, 'sub_task', self._sub_task_id_1)
         os.makedirs(working_dir_1, exist_ok=True)
 
-        expected_cmd_merge = ("cd {0} && mir merge --dst-rev {1}@{2} -s host -w {3} "
+        expected_cmd_merge = ("cd '{0}' && mir merge --dst-rev {1}@{2} -s host -w '{3}' "
                               "--src-revs '{4}@{4};{5}' --ex-src-revs '{6}'".format(self._mir_repo_root, self._task_id,
                                                                                     self._sub_task_id_1, working_dir_1,
                                                                                     self._guest_id1, self._guest_id2,
@@ -138,8 +138,8 @@ class TestInvokerTaskMining(unittest.TestCase):
         self.assertDictEqual(mining_config, config)
 
         asset_cache_dir = os.path.join(self._user_root, 'mining_assset_cache')
-        mining_cmd = ("cd {0} && mir mining --dst-rev {1}@{1} -w {2} --model-location {3} --media-location {3} "
-                      "--model-hash {5} --src-revs {1}@{6} --cache {9} --config-file {7} --executor {8} "
+        mining_cmd = ("cd '{0}' && mir mining --dst-rev {1}@{1} -w '{2}' --model-location '{3}' --media-location '{3}' "
+                      "--model-hash {5} --src-revs {1}@{6} --cache '{9}' --config-file '{7}' --executor {8} "
                       "--executor-instance {10} --topk {4}".format(self._mir_repo_root, self._task_id, working_dir_0,
                                                                    self._storage_root, top_k, model_hash,
                                                                    self._sub_task_id_1, output_config, 'mining_image',
