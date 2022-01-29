c2b2bc9a63ad65ff6ea1d410208a53d88a7d90e6
Kelvin <lc_kelvin@163.com>
2022-01-29 10:56:29 +0800

    update

--- ymir/backend/src/ymir-monitor/tests/conftest.py
+++ ymir/backend/src/ymir-monitor/tests/conftest.py
@@ -1,21 +1,15 @@
-import json
-from typing import Dict, Generator
-from unittest.mock import Mock
+from typing import Generator
 
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy.orm import Session
 
-from app.api import deps
-from app.config import settings
-from app.db.session import SessionLocal
 from monitor.main import app
-
-
 
 
 @pytest.fixture(scope="module")
 def client() -> Generator:
     with TestClient(app) as c:
         yield c
+
+
 
