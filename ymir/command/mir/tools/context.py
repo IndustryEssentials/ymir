7d8b9a82802c46c6542fbf071a4b90c1c5903c2c
ymir <ymir@intellif.com>
2022-02-28 16:48:51 +0800

    add doc

--- ymir/command/mir/tools/context.py
+++ ymir/command/mir/tools/context.py
@@ -31,12 +31,26 @@ def save(mir_root: str, project_class_ids: List[int]) -> None:
 
 # general
 def check_class_names(mir_root: str, current_class_names: List[str]) -> bool:
+    """
+    check `current_class_names` matches mir repo's project class ids settings
+
+    if mir repo has project class ids settings, this function returns True if they are equal
+
+    if mir repo has no project class ids settings, this function always returns True, meaning they are always matched
+    """
     class_id_manager = class_ids.ClassIdManager(mir_root)
     current_class_ids = class_id_manager.id_for_names(current_class_names)
     return check_class_ids(mir_root=mir_root, current_class_ids=current_class_ids)
 
 
 def check_class_ids(mir_root: str, current_class_ids: List[int]) -> bool:
+    """
+    check `current_class_ids` matches mir repo's project class ids settings
+
+    if mir repo has project class ids settings, this function returns True if they are equal
+
+    if mir repo has no project class ids settings, this function always returns True, meaning they are always matched
+    """
     project_class_ids = load(mir_root=mir_root)
     if not project_class_ids:
         # if this mir repo not binded to project, treat as equal
