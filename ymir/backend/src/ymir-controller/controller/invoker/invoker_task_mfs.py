2bec696598cd1f1539a05dc8222001b933f7dc2f
ymir <ymir@intellif.com>
2022-03-01 14:41:47 +0800

    wip

--- ymir/backend/src/ymir-controller/controller/invoker/invoker_task_mfs.py
+++ ymir/backend/src/ymir-controller/controller/invoker/invoker_task_mfs.py
@@ -1,5 +1,6 @@
 from typing import Dict
 
+from controller.invoker.invoker_cmd_merge import MergeInvoker
 from controller.invoker.invoker_task_base import TaskBaseInvoker
 from controller.utils import invoker_call, revs, utils
 from id_definition.error_codes import CTLResponseCode
@@ -8,21 +9,8 @@ from proto import backend_pb2
 
 class TaskMfsInvoker(TaskBaseInvoker):
     def task_pre_invoke(self, sandbox_root: str, request: backend_pb2.GeneralReq) -> backend_pb2.GeneralResp:
-        # train_request = request.req_create_task.training
-        # if not train_request.in_dataset_types:
-        #     return utils.make_general_response(CTLResponseCode.ARG_VALIDATION_FAILED, "invalid dataset_types")
-
-        # # store executor config in task_0 work_dir
-        # subtask_work_dir_0 = self.subtask_work_dir(self._work_dir, utils.sub_task_id(self._task_id, 0))
-        # output_config_file = self.gen_executor_config_path(subtask_work_dir_0)
-        # gpu_lock_ret = self.gen_executor_config_lock_gpus(
-        #     repo_root=self._repo_root,
-        #     req_executor_config=request.docker_image_config,
-        #     in_class_ids=train_request.in_class_ids,
-        #     output_config_file=output_config_file,
-        # )
-        # if not gpu_lock_ret:
-        #     return utils.make_general_response(CTLResponseCode.LOCK_GPU_ERROR, "Not enough GPU available")
+        if not request.req_create_task.process_data.merge_in_dataset_ids:
+            return utils.make_general_response(CTLResponseCode.ARG_VALIDATION_FAILED, 'empty merge_in_dataset_ids')
 
         return utils.make_general_response(CTLResponseCode.CTR_OK, "")
 
@@ -34,13 +22,30 @@ class TaskMfsInvoker(TaskBaseInvoker):
     def subtask_invoke_2(cls, sandbox_root: str, repo_root: str, assets_config: Dict[str, str],
                          request: backend_pb2.GeneralReq, subtask_id: str, subtask_workdir: str,
                          subtask_id_dict: Dict[int, str]) -> backend_pb2.GeneralResp:
-        pass
+        """ merge """
+        process_data_req = request.req_create_task.process_data
+        merge_response = invoker_call.make_invoker_cmd_call(
+            invoker=MergeInvoker,
+            sandbox_root=sandbox_root,
+            req_type=backend_pb2.CMD_MERGE,
+            user_id=request.user_id,
+            repo_id=request.repo_id,
+            task_id=subtask_id,
+            his_task_id=process_data_req.merge_in_dataset_ids[0],
+            dst_task_id=request.task_id,
+            in_dataset_ids=process_data_req.merge_in_dataset_ids,
+            ex_dataset_ids=process_data_req.merge_ex_dataset_ids,
+            merge_strategy=process_data_req.merge_strategy,
+            work_dir=subtask_workdir,
+        )
+        return merge_response
 
     @classmethod
     def subtask_invoke_1(cls, sandbox_root: str, repo_root: str, assets_config: Dict[str, str],
                          request: backend_pb2.GeneralReq, subtask_id: str, subtask_workdir: str,
                          subtask_id_dict: Dict[int, str]) -> backend_pb2.GeneralResp:
-        pass
+        process_data_req = request.req_create_task.process_data
+        
 
     @classmethod
     def subtask_invoke_0(cls, sandbox_root: str, repo_root: str, assets_config: Dict[str, str],
