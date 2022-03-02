15f1f996298535d2b4da82ff6a944dd44de36d8b
Kelvin <lc_kelvin@163.com>
2022-03-02 16:39:21 +0800

    update

--- ymir/backend/src/ymir-app/app/utils/ymir_controller.py
+++ ymir/backend/src/ymir-app/app/utils/ymir_controller.py
@@ -93,7 +93,6 @@ class ControllerRequest:
     ) -> mirsvrpb.GeneralReq:
         # project training target labels
         request.private_labels[:] = args["labels"]
-        request.dst_task_id = args["initial_dataset_id"]
         request.req_type = mirsvrpb.REPO_CREATE
         return request
 
