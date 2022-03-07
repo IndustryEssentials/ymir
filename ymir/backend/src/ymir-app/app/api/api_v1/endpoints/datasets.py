68d184b8f28f0fed39f50fe64d7dbbb2ebcd8c28
Kelvin <lc_kelvin@163.com>
2022-03-07 11:45:35 +0800

    update

--- ymir/backend/src/ymir-app/app/api/api_v1/endpoints/datasets.py
+++ ymir/backend/src/ymir-app/app/api/api_v1/endpoints/datasets.py
@@ -8,7 +8,7 @@ from fastapi.encoders import jsonable_encoder
 from fastapi import APIRouter, BackgroundTasks, Depends, Path, Query
 from fastapi.logger import logger
 from sqlalchemy.orm import Session
-from app.utils.class_ids import get_keyword_name_to_id_mapping
+from app.utils.class_ids import convert_keywords_to_classes
 from app import crud, models, schemas
 from app.api import deps
 from app.api.errors.errors import (
@@ -474,14 +474,14 @@ def create_dataset_fusion(
         "[create task] create dataset fusion with payload: %s",
         jsonable_encoder(task_in),
     )
-    keyword_name_to_id = get_keyword_name_to_id_mapping(labels)
     task_id = gen_task_hash(current_user.id, task_in.project_id)
+
     parameters = dict(
         include_datasets=[task_in.main_dataset_id] + task_in.include_datasets,
         include_strategy=task_in.include_strategy,
         exclude_datasets=task_in.exclude_datasets,
-        include_labels=[keyword_name_to_id[label] for label in task_in.include_labels],
-        exclude_labels=[keyword_name_to_id[label] for label in task_in.exclude_labels],
+        include_labels=convert_keywords_to_classes(labels, task_in.include_labels),
+        exclude_labels=convert_keywords_to_classes(labels, task_in.exclude_labels),
         sampling_count=task_in.sampling_count,
     )
     try:
