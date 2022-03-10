35bd205f60fe8a04e84fd5378b4c9942decc6643
ymir <ymir@intellif.com>
2022-03-10 16:23:00 +0800

    update

--- ymir/backend/src/ymir-controller/controller/utils/labels.py
+++ ymir/backend/src/ymir-controller/controller/utils/labels.py
@@ -1,8 +1,6 @@
-import csv
-import logging
-import os
 from collections import Counter
 from pathlib import Path
+import os
 import time
 from typing import Dict, List, Iterable, Set
 
@@ -20,9 +18,6 @@ kLabelCreated = 'created'
 kLabelModified = 'modified'
 
 
-logging.basicConfig(level=logging.INFO)  # for test
-
-
 def labels_file_name() -> str:
     return 'labels.yaml'
 
@@ -34,7 +29,6 @@ def labels_file_path(mir_root: str) -> str:
 
 
 class LabelFileHandler:
-    # csv file: type_id, reserve, main_label, alias_label_1, xxx
     def __init__(self, mir_root: str) -> None:
         self._label_file = labels_file_path(mir_root=mir_root)
 
@@ -109,12 +103,10 @@ class LabelFileHandler:
         return result_labels
 
     def merge_labels(self, candidate_labels: List[str], check_only: bool = False) -> List[List[str]]:
-        logging.info(f"wants to merge: {candidate_labels}")  # for test
         # check `candidate_labels` has no duplicate
-        candidate_labels_list = [x.lower().split(",") for x in candidate_labels]
+        candidate_labels_list = [x.strip().lower().split(",") for x in candidate_labels]
         candidates_list = [x for row in candidate_labels_list for x in row]
         if len(candidates_list) != len(set(candidates_list)):
-            logging.info(f"conflict candidate labels: {candidate_labels_list}")
             return candidate_labels_list
 
         current_timestamp = time.time()
@@ -158,8 +150,6 @@ class LabelFileHandler:
         conflict_labels = []
         for candidate_list in candidate_labels_list_new:
             candidate_set = set(candidate_list)
-            logging.info(f"candidate set: {candidate_set}")  # for test
-            logging.info(f"existed labels set: {existed_labels_set}")  # for test
             if set.intersection(candidate_set, existed_labels_set):  # at least one label exist.
                 conflict_labels.append(candidate_list)
                 continue
@@ -171,27 +161,12 @@ class LabelFileHandler:
                 kLabelCreated: current_timestamp,
                 kLabelModified: current_timestamp,
             })
-            logging.info(f"candidate list added: {candidate_list}")  # for test
-            logging.info(f"existed labels 175: {existed_labels}")
             existed_labels_set.update(candidate_set)
-        logging.info(f"conflict labels: {conflict_labels}")
 
         if not (check_only or conflict_labels):
             self._write_label_file(existed_labels)
         return conflict_labels
-        pass
 
     def get_main_labels_by_ids(self, type_ids: Iterable) -> List[str]:
-        with open(self._label_file, newline='') as f:
-            reader = csv.reader(f)
-            all_main_names = [one_row[2] for one_row in reader]
-
-        main_names = []
-        for type_id in type_ids:
-            type_id = int(type_id)
-            if type_id >= len(all_main_names):
-                raise ValueError(f"get_main_labels_by_ids input type_ids error: {type_ids}")
-            else:
-                main_names.append(all_main_names[type_id])
-
-        return main_names
+        all_labels = self.get_all_labels()
+        return [all_labels[int(idx)][kLabelName] for idx in type_ids]
