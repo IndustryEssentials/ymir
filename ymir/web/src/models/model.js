8eb4966cda34ff2a35d4b8df28ae07af6d628798
elliotmessi <90443217+elliotmessi@users.noreply.github.com>
2022-03-12 09:29:10 +0800

    [web] 050 bugs for fusion, mining, etc. (#374)

--- ymir/web/src/models/model.js
+++ ymir/web/src/models/model.js
@@ -72,7 +72,7 @@ export default {
     },
     *queryAllModels({ payload }, { select, call, put }) {
       const pid = payload
-      const dss = yield put.resolve({ type: 'queryModels', payload: { project_id: pid, state: states.VILID, limit: 10000 }})
+      const dss = yield put.resolve({ type: 'queryModels', payload: { project_id: pid, state: states.VALID, limit: 10000 }})
       if (dss) {
         yield put({
           type: "UPDATE_ALL_MODELS",
