ecdea16a55e0d26dc0cfac21dd75e3444166d0bc
IJtLJZ8Rm4Yr <90443055+IJtLJZ8Rm4Yr@users.noreply.github.com>
2022-03-16 21:48:29 +0800

    [backend] add iteration apis (#401)

--- ymir/backend/src/ymir_app/app/api/api_v1/api.py
+++ ymir/backend/src/ymir_app/app/api/api_v1/api.py
@@ -15,6 +15,7 @@ from app.api.api_v1.endpoints import (
     upload,
     users,
     projects,
+    iterations,
     dataset_groups,
     model_groups,
 )
@@ -22,18 +23,19 @@ from app.api.api_v1.endpoints import (
 api_router = APIRouter()
 
 api_router.include_router(login.router, tags=["auth"])
-api_router.include_router(upload.router, tags=["upload"])
-api_router.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(stats.router, prefix="/stats", tags=["stats"])
+api_router.include_router(projects.router, prefix="/projects", tags=["projects"])
+api_router.include_router(iterations.router, prefix="/iterations", tags=["iterations"])
+api_router.include_router(dataset_groups.router, prefix="/dataset_groups", tags=["dataset_groups"])
 api_router.include_router(datasets.router, prefix="/datasets", tags=["datasets"])
+api_router.include_router(model_groups.router, prefix="/model_groups", tags=["model_groups"])
 api_router.include_router(models.router, prefix="/models", tags=["models"])
+api_router.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
 api_router.include_router(keywords.router, prefix="/keywords", tags=["keywords"])
 api_router.include_router(graphs.router, prefix="/graphs", tags=["graphs"])
 api_router.include_router(images.router, prefix="/images", tags=["docker_images"])
 api_router.include_router(inferences.router, prefix="/inferences", tags=["inference"])
 api_router.include_router(roles.router, prefix="/roles", tags=["roles"])
 api_router.include_router(info.router, prefix="/sys_info", tags=["sys"])
-api_router.include_router(projects.router, prefix="/projects", tags=["projects"])
-api_router.include_router(dataset_groups.router, prefix="/dataset_groups", tags=["dataset_groups"])
-api_router.include_router(model_groups.router, prefix="/model_groups", tags=["model_groups"])
+api_router.include_router(upload.router, tags=["upload"])
+api_router.include_router(stats.router, prefix="/stats", tags=["stats"])
