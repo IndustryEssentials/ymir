1c9d370b1275032544276c586d27f6473e272148
ymir <ymir@intellif.com>
2022-03-22 18:06:45 +0800

    update

--- ymir/command/mir/tools/settings.py
+++ ymir/command/mir/tools/settings.py
@@ -3,5 +3,3 @@ PRODUCER_NAME = 'ymir'
 EXECUTOR_CONFIG_KEY = 'executor_config'
 TASK_CONTEXT_KEY = 'task_context'
 TASK_CONTEXT_PARAMETERS_KEY = 'task_parameters'
-
-MIR_ASSOCIATED_FILES = ['.git', '.mir', '.gitignore', '.mir_lock']
