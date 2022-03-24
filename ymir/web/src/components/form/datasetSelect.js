7fe0baa255e129b256fd4e27a800aaf412dd80ca
elloitli <li.shanlue@intellif.com>
2022-03-24 19:40:34 +0800

    add dataset select component

---
+++ ymir/web/src/components/form/datasetSelect.js
@@ -0,0 +1,53 @@
+import { Select } from 'antd'
+import { connect } from 'umi'
+import { useEffect, useState } from 'react'
+import t from '@/utils/t'
+
+const DatasetSelect = ({ pid, filter = [], value, datasets = [], onChange = () => { }, getDatasets, ...resProps }) => {
+
+  useEffect(() => {
+    fetchDatasets()
+  }, [])
+
+  useEffect(() => {
+  console.log('filter:', filter)
+  }, [filter])
+
+  function fetchDatasets() {
+    getDatasets(pid)
+  }
+
+  return (
+    <Select
+      placeholder={t('task.train.form.training.datasets.placeholder')}
+      filterOption={(input, option) => option.key.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+      onChange={onChange}
+      showArrow
+      {...resProps}
+    >
+      {datasets.filter(ds => !filter.includes(ds.id)).map(item =>
+        <Option value={item.id} key={item.name}>
+          {item.name} {item.versionName}(assets: {item.assetCount})
+        </Option>
+      )}
+    </Select>
+  )
+}
+
+const props = (state) => {
+  return {
+    datasets: state.dataset.allDatasets,
+  }
+}
+const actions = (dispatch) => {
+  return {
+    getDatasets(pid, force) {
+      console.log('pid:', pid)
+      return dispatch({
+        type: 'dataset/queryAllDatasets',
+        payload: {pid, force},
+      })
+    }
+  }
+}
+export default connect(props, actions)(DatasetSelect)
