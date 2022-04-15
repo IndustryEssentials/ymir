68cc6414361a5a257b973335fe979d8d5210664a
elloitli <li.shanlue@intellif.com>
2022-04-15 16:03:21 +0800

    remove unused intl

--- ymir/web/src/locales/modules/portal.ts
+++ ymir/web/src/locales/modules/portal.ts
@@ -1,23 +1,13 @@
 const portal = {
-  'portal.btn.more': { cn: '管理', en: 'More', },
   'portal.project.static.title': { cn: '项目统计', en: 'Project Stastics', },
   'portal.project': { cn: '项目', en: 'Project', },
   'portal.model.best.title': { cn: '最优模型', en: 'Best Model', },
-  'portal.model.my.title': { cn: '我的模型', en: 'My Model', },
   'portal.dataset.origin.title': { cn: '公共数据集', en: 'Public Dataset', },
-  'portal.dataset.origin.copy.title': { cn: '复制公共数据集到自己的空间', en: 'Copy public dataset to your own workspace', },
-  'portal.dataset.my.title': { cn: '我的数据集', en: 'My Dataset', },
   'portal.project.my.title': { cn: '我的项目', en: 'My Project', },
-  'portal.model.keywords': { cn: '训练目标', en: 'Train Classes', },
   'portal.dataset.asset.count': { cn: '图片数', en: 'Assets', },
   'portal.dataset.keyword.count': { cn: '标签数', en: 'Keywords', },
   'portal.dataset.keyword': { cn: '标签', en: 'Keywords', },
-  'portal.dataset.origin.copy': { cn: '复制', en: 'Copy', },
   'portal.action.new.project': { cn: '新建一个项目', en: 'Create Project', },
-  'portal.action.new.task': { cn: '新建一个任务', en: 'New Task', },
-  'portal.action.train': { cn: '训练一个模型', en: 'Train A Model', },
-  'portal.action.dataset.import': { cn: '导入一个数据集', en: 'Import A Dataset', },
-  'portal.cited': { cn: '引用数', en: 'Cited', },
 }
 
 export default portal
