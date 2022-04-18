e1246397f3d933d0be0d116a424e375b9959f977
ymir <ymir@intellif.com>
2022-04-18 18:45:34 +0800

    add some doc

--- docker_executor/sample_executor/tests/__main__.py
+++ docker_executor/sample_executor/tests/__main__.py
@@ -7,6 +7,8 @@ from typing import List
 def main(args: List[str]) -> int:
     module_root = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
 
+    # note that env.DEFAULT_ENV_FILE_PATH will change when test
+    # so there should be only ONE process / thread when test
     cmd = [f"PYTHONPATH=$PYTHONPATH:{module_root}", 'pytest', '-vv', '-x', '--durations=0']
     cmd.extend(args)
 
