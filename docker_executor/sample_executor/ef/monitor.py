e276f35e921c80926a4b5845ea75bc24c8a294be
ymir <ymir@intellif.com>
2022-04-18 17:02:07 +0800

    update

--- docker_executor/sample_executor/ef/monitor.py
+++ docker_executor/sample_executor/ef/monitor.py
@@ -15,19 +15,15 @@ def write_logger(info: str, percent: float = None, exception: Exception = None) 
     logging.info(info)
 
     if percent is not None or exception is not None:
-        _write_monitor_file(info, percent, exception)
+        env_config = env.get_current_env()
+        with open(env_config.output.monitor_file, 'w') as f:
+            if not exception:
+                state = _TaskState.RUNNING.value
+                tb = ''
+            else:
+                state = _TaskState.ERROR.value
+                percent = 1.0
+                tb = ''.join(traceback.format_stack()[:-2])  # ignore the last 2 items: write_logger and _write_monitor_file
 
-
-def _write_monitor_file(info: str, percent: float = None, exception: Exception = None) -> None:
-    env_config = env.get_current_env()
-    with open(env_config.output.monitor_file, 'w') as f:
-        if not exception:
-            state = _TaskState.RUNNING.value
-            tb = ''
-        else:
-            state = _TaskState.ERROR.value
-            percent = 1.0
-            tb = ''.join(traceback.format_stack()[:-2])  # ignore the last 2 items: write_logger and _write_monitor_file
-
-        f.write(f"{env_config.task_id}\t{time.time()}\t{percent:.2f}\t{state}\t{info}\n")
-        f.write(f"{tb}")
+            f.write(f"{env_config.task_id}\t{time.time()}\t{percent:.2f}\t{state}\t{info}\n")
+            f.write(f"{tb}")
