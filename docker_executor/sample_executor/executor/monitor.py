b7a02d4a8c4b6d456d50a9aed5ff1dc96900356e
ymir <ymir@intellif.com>
2022-04-18 18:07:32 +0800

    update

--- docker_executor/sample_executor/executor/monitor.py
+++ docker_executor/sample_executor/executor/monitor.py
@@ -16,16 +16,16 @@ def write_monitor_logger(info: str, percent: float = None, error_occured: bool =
         raise ValueError('percent and error_occured should not be both none')
 
     logging.info(info)
+    if not error_occured:
+        state = _TaskState.RUNNING.value
+        tb = ''
+    else:
+        state = _TaskState.ERROR.value
+        percent = 1.0
+        # ignore the last 2 items: write_logger and _write_monitor_file
+        tb = ''.join(traceback.format_stack()[:-2])
+
     env_config = env.get_current_env()
     with open(env_config.output.monitor_file, 'w') as f:
-        if not error_occured:
-            state = _TaskState.RUNNING.value
-            tb = ''
-        else:
-            state = _TaskState.ERROR.value
-            percent = 1.0
-            # ignore the last 2 items: write_logger and _write_monitor_file
-            tb = ''.join(traceback.format_stack()[:-2])
-
         f.write(f"{env_config.task_id}\t{time.time()}\t{percent:.2f}\t{state}\t{info}\n")
         f.write(f"{tb}")
