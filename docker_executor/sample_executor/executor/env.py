8b251fd6df544933b9dd2f16d4080a8fb6898532
ymir <ymir@intellif.com>
2022-04-18 18:40:23 +0800

    add some doc

--- docker_executor/sample_executor/executor/env.py
+++ docker_executor/sample_executor/executor/env.py
@@ -51,6 +51,9 @@ from pydantic import BaseModel
 import yaml
 
 
+# default location of env yaml file
+# if your app is running inside docker image, env file is always at /in/env.yaml
+# you can change the location of this file IF AND ONLY IF you are doing test
 DEFAULT_ENV_FILE_PATH = '/in/env.yaml'
 
 
