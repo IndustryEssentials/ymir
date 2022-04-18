06469e83e23a4f06a1ae8eb3e843bcb8b3a41ec7
ymir <ymir@intellif.com>
2022-04-18 14:42:31 +0800

    put writer_xxx_result from nonitor to result_writer

--- docker_executor/sample_executor/app/start.py
+++ docker_executor/sample_executor/app/start.py
@@ -2,7 +2,7 @@ import os
 import sys
 from typing import List
 
-from ef import env, monitor
+from ef import env, monitor, result_writer as rw
 
 
 def start() -> int:
@@ -47,8 +47,8 @@ def _run_training(env_config: env.EnvConfig) -> None:
     with open(os.path.join(env_config.output.models_dir, 'model-symbols.json'), 'w') as f:
         f.write('fake json')
 
-    #! use `monitor.write_training_result` to save training result
-    monitor.write_training_result(model_names=['model-0000.params', 'model-symbols.json'],
+    #! use `rw.write_training_result` to save training result
+    rw.write_training_result(model_names=['model-0000.params', 'model-symbols.json'],
                                   mAP=0.8,
                                   classAPs={'person': 0.8, 'cat': 0.82})
 
@@ -74,7 +74,7 @@ def _run_mining(env_config: env.EnvConfig) -> None:
 
     #! write mining result
     mining_result = [(asset_path, 0.9) for asset_path in asset_paths]
-    monitor.write_mining_result(mining_result=mining_result)
+    rw.write_mining_result(mining_result=mining_result)
 
     #! if task done, write 100% percent log
     monitor.write_logger(info='mining done', percent=1.0)
@@ -97,9 +97,9 @@ def _run_infer(env_config: env.EnvConfig) -> None:
     monitor.write_logger(info=f"sample 50%% log, assets count: {len(asset_paths)}", percent=0.5)
 
     #! write infer result
-    fake_annotation = monitor.Annotation(class_name='cat', score=0.9, box=monitor.Box(x=50, y=50, w=150, h=150))
+    fake_annotation = rw.Annotation(class_name='cat', score=0.9, box=rw.Box(x=50, y=50, w=150, h=150))
     infer_result = {asset_path: [fake_annotation] for asset_path in asset_paths}
-    monitor.write_infer_result(infer_result=infer_result)
+    rw.write_infer_result(infer_result=infer_result)
 
     #! if task done, write 100% percent log
     monitor.write_logger(info='infer done', percent=1.0)
--- docker_executor/sample_executor/tests/test_monitor.py
+++ docker_executor/sample_executor/tests/test_monitor.py
@@ -1,7 +1,5 @@
-import json
 import os
 import shutil
-from typing import List, Dict, Tuple
 import unittest
 
 import yaml
@@ -72,28 +70,6 @@ class TestMonitor(unittest.TestCase):
             self.assertEqual(2, int(state_str))
             self.assertEqual(len(lines), 1)
 
-    def _check_training_result(self, model_names: List[str], mAP: float, classAPs: Dict[str, float], **kwargs) -> None:
-        with open(self._training_result_file, 'r') as f:
-            result_obj = yaml.safe_load(f)
-            self.assertEqual(result_obj['model'], model_names)
-            self.assertEqual(result_obj['map'], mAP)
-            self.assertEqual(result_obj['class_aps'], classAPs)
-            for k, v in kwargs.items():
-                self.assertEqual(result_obj[k], v)
-
-    def _check_mining_result(self, mining_result: List[Tuple[str, float]]) -> None:
-        with open(self._mining_result_file, 'r') as f:
-            lines = f.read().splitlines()
-            self.assertEqual(len(lines), len(mining_result))
-            self.assertEqual(lines[0], 'b\t0.3')
-            self.assertEqual(lines[1], 'c\t0.2')
-            self.assertEqual(lines[2], 'a\t0.1')
-
-    def _check_infer_result(self, infer_result: Dict[str, List[monitor.Annotation]]) -> None:
-        with open(self._infer_result_file, 'r') as f:
-            infer_result_obj = json.loads(f.read())
-            self.assertEqual(set(infer_result_obj['detection'].keys()), set(infer_result.keys()))
-
     # public: test cases
     def test_write_monitor(self) -> None:
         monitor.write_logger(info='a fake log info', percent=0.2)
@@ -104,27 +80,3 @@ class TestMonitor(unittest.TestCase):
                              percent=0.4,
                              exception=exception)
         self._check_monitor(percent=1.0, exception=exception)
-
-    def test_write_training_result(self) -> None:
-        model_names = ['model-symbols.json', 'model-0000.params']
-        mAP = 0.86
-        classAPs = {'cat': 0.86, 'person': 0.86}
-        monitor.write_training_result(model_names=model_names, mAP=mAP, classAPs=classAPs, author='fake author')
-        self._check_training_result(model_names=model_names, mAP=mAP, classAPs=classAPs, author='fake author')
-
-    def test_write_mining_result(self) -> None:
-        mining_result = [('a', '0.1'), ('b', '0.3'), ('c', '0.2')]
-        monitor.write_mining_result(mining_result=mining_result)
-        self._check_mining_result(mining_result=mining_result)
-
-    def test_write_infer_result(self) -> None:
-        infer_result = {
-            'a': [
-                monitor.Annotation(box=monitor.Box(x=0, y=0, w=50, h=50), class_name='cat', score=0.2),
-                monitor.Annotation(box=monitor.Box(x=150, y=0, w=50, h=50), class_name='person', score=0.3)
-            ],
-            'b': [monitor.Annotation(box=monitor.Box(x=0, y=0, w=50, h=150), class_name='person', score=0.2)],
-            'c': [],
-        }
-        monitor.write_infer_result(infer_result=infer_result)
-        self._check_infer_result(infer_result=infer_result)
