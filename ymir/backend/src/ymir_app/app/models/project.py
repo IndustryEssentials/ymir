e24106fe6637a3c7992ee1425f56a64a6aadb5ec
under-chaos <90446262+under-chaos@users.noreply.github.com>
2022-04-19 12:33:06 +0800

    [backend] add is_example column for project  (#552)
    * update
    
    * update
    
    * update
    
    Co-authored-by: Kelvin <lc_kelvin@163.com>

--- ymir/backend/src/ymir_app/app/models/project.py
+++ ymir/backend/src/ymir_app/app/models/project.py
@@ -89,6 +89,7 @@ class Project(Base):
         viewonly=True,
     )
 
+    is_example = Column(Boolean, default=False)
     is_deleted = Column(Boolean, default=False, nullable=False)
     create_datetime = Column(DateTime, default=datetime.utcnow, nullable=False)
     update_datetime = Column(
