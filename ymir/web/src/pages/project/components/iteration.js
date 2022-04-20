ab0731190181aba31856beb0316d0442097774d4
elloitli <li.shanlue@intellif.com>
2022-04-20 18:35:33 +0800

    init train set version on first iteration

--- ymir/web/src/pages/project/components/iteration.js
+++ ymir/web/src/pages/project/components/iteration.js
@@ -11,7 +11,6 @@ function Iteration({ project, fresh = () => { }, ...func }) {
   const [iteration, setIteration] = useState({})
   const [stages, setStages] = useState([])
   const [prevIteration, setPrevIteration] = useState({})
-  const [firstTrainSet, setFirstTrainSet] = useState({})
 
   useEffect(() => {
     initStages()
@@ -25,15 +24,12 @@ function Iteration({ project, fresh = () => { }, ...func }) {
   useEffect(() => {
     if (iteration.prevIteration) {
       fetchPrevIteration()
-    } else {
-      fetchFirstTrainSet()
     }
-
   }, [iteration])
 
   useEffect(() => {
     iteration.id && rerenderStages()
-  }, [iteration, firstTrainSet, prevIteration])
+  }, [iteration, prevIteration])
 
   const callback = useCallback(iterationHandle, [iteration])
 
@@ -70,7 +66,7 @@ function Iteration({ project, fresh = () => { }, ...func }) {
         s1d: iteration.miningSet,
         s1m: prevIteration.model || project.model,
         s2d: iteration.miningResult,
-        s3d: prevIteration.trainUpdateSet || firstTrainSet.id,
+        s3d: prevIteration.trainUpdateSet || project.trainSetVersion,
         s3m: iteration.labelSet,
         s4d: iteration.trainUpdateSet,
         s4t: project?.testSet?.id,
@@ -106,14 +102,6 @@ function Iteration({ project, fresh = () => { }, ...func }) {
     const result = await func.getIteration(project.id, iteration.prevIteration)
     if (result) {
       setPrevIteration(result)
-    }
-  }
-
-  async function fetchFirstTrainSet() {
-    const result = await func.queryFirstTrainDataset(project.trainSet.id)
-    if (result) {
-      const { items: [ds] } = result
-      ds && setFirstTrainSet(ds)
     }
   }
 
