2bb217135dccac8bd798e754a1d3c0f2806c17be
elloitli <li.shanlue@intellif.com>
2022-04-22 14:53:45 +0800

    terminate btn hidden when task pending

--- ymir/web/src/components/dataset/list.js
+++ ymir/web/src/components/dataset/list.js
@@ -7,7 +7,7 @@ import { Form, Button, Input, Table, Space, Modal, Row, Col, Tooltip, Pagination
 import t from "@/utils/t"
 import { humanize } from "@/utils/number"
 import { diffTime } from '@/utils/date'
-import { getTaskTypeLabel } from '@/constants/task'
+import { getTaskTypeLabel, TASKSTATES } from '@/constants/task'
 import { states } from '@/constants/dataset'
 
 import StateTag from "@/components/task/stateTag"
@@ -195,7 +195,7 @@ function Datasets({ pid, project = {}, datasetList, query, versions, ...func }) 
   ]}
 
   const actionMenus = (record) => {
-    const { id, name, state, versionName, isProtected, task: { is_terminated } } = record
+    const { id, state, taskState, task } = record
     const menus = [
       {
         key: "fusion",
@@ -243,7 +243,7 @@ function Datasets({ pid, project = {}, datasetList, query, versions, ...func }) 
         key: "stop",
         label: t("task.action.terminate"),
         onclick: () => stop(record),
-        hidden: () => !isRunning(state) || is_terminated,
+        hidden: () => taskState === TASKSTATES.PENDING || !isRunning(state) || task.is_terminated,
         icon: <StopIcon />,
       },
       // {
