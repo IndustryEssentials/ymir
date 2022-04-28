72f3e0062aed8d17bdd4d1bd7ff159e41084d0ac
elliotmessi <90443217+elliotmessi@users.noreply.github.com>
2022-04-28 12:18:27 +0800

    restore get keywords code (#596)

--- ymir/web/src/pages/task/label/index.js
+++ ymir/web/src/pages/task/label/index.js
@@ -180,7 +180,7 @@ function Label({ datasets, keywords, ...func }) {
                 <Space size={20}>
                   <Form.Item name='submitBtn' noStyle>
                     <Button type="primary" size="large" htmlType="submit">
-                      {t('common.action.label')}
+                      {t('task.create')}
                     </Button>
                   </Form.Item>
                   <Form.Item name='backBtn' noStyle>
