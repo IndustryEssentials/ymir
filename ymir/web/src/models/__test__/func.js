dc74cd17bed13f49cfb74ead5c0c1909037aedf8
elloitli <li.shanlue@intellif.com>
2022-05-13 17:04:31 +0800

    update model -> iteration unit test

--- ymir/web/src/models/__test__/func.js
+++ ymir/web/src/models/__test__/func.js
@@ -37,3 +37,13 @@ export function normalReducer(module, func, payload, expected, field, initState)
 
 export const product = (id) => ({ id })
 export const products = (n) => Array.from({ length: n }, (item, index) => product(index + 1))
+
+export const generatorCreator = (module) => (func, payload) => {
+  const saga = module['effects'][func]
+  const creator = {
+    type: func,
+    payload,
+  }
+
+  return saga(creator, { put, call, select })
+}
