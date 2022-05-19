4300a105a37e3c7ccc3abc034bb73ea2b72fd5f9
elloitli <li.shanlue@intellif.com>
2022-05-19 19:56:43 +0800

    update prepare -> structure

---
+++ ymir/web/src/hooks/useUpdateProject.js
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import { useDispatch } from 'umi'
+
+const useUpdateProject = () => {
+  const dispatch = useDispatch()
+  const updateProject = payload => dispatch({
+    type: 'project/updateProject',
+    payload,
+  })
+  const [updated, setUpdated] = useState({})
+
+  const update = async (params = {}) => {
+    const result = await updateProject(ids)
+    if (result) {
+      setUpdated(result)
+    }
+  }
+
+  return [updated, update]
+}
+
+export default useUpdateProject
--- ymir/web/src/pages/project/components/prepare.js
+++ ymir/web/src/pages/project/components/prepare.js
@@ -1,17 +1,33 @@
 import { useCallback, useEffect, useState } from "react"
-import { Row, Col } from "antd"
+import { Row, Col, Form, Button } from "antd"
 import { connect } from "dva"
 
 import { states } from '@/constants/dataset'
-import { Stages, StageList } from '@/constants/project'
-import Stage from './stage'
+import useUpdateProject from '@/hooks/useUpdateProject'
+import DatasetSelect from '@/components/form/DatasetSelect'
+import ModelSelect from '@/components/form/ModelSelect'
+
 import s from "./iteration.less"
 
-function Prepare({ project = {}, fresh = () => { }, ...func }) {
-  const [stages, setStages] = useState([])
+const SettingsSelection = (Select) => {
+  const Selection = ({ field, callback = () => { }, ...props }) => {
+    const [newProject, updateProject] = useUpdateProject()
+    const onChange = async (value) => {
+      await updateProject({ [field]: value })
+      callback(newProject)
+    }
+
+    return <Select {...props} onChange={onChange} />
+  }
+  return Selection
+}
+
+const Stage = ({ project, stage }) => {
+  const [valid, setValid] = useState(false)
+  const Selection = SettingsSelection(stage.type ? ModelSelect : DatasetSelect)
 
   useEffect(() => {
-    project.id && initStages()
+    project && setValid(project[stage.field])
   }, [project])
 
   const validNext = () => project.miningSet && project.testSet && project.model
@@ -45,6 +61,40 @@ function Prepare({ project = {}, fresh = () => { }, ...func }) {
     })
     setStages(ss)
   }
+  const renderIcon = () => {
+    return valid ? <YesIcon /> : 'wait'
+  }
+  const renderState = () => {
+    return valid ? '已完成' : '待选择'
+  }
+  return <Col flex={1}>
+    <Row>
+      <Col flex={'60px'}>
+        <div className={s.state}>{renderIcon()}</div>
+        <div className={s.state}>{renderState()}</div>
+      </Col>
+      <Col flex={1}>
+        <Form.Item label={stage.label}></Form.Item>
+        <Selection field={stage.field} callback={stage.callback} />
+      </Col>
+    </Row>
+  </Col>
+}
+
+function Prepare({ project = {}, fresh = () => { }, ...func }) {
+  const [validPrepare, setValidPrepare] = useState(false)
+
+  const stages = [
+    { field: 'trainSet', },
+    { field: 'testSet', },
+    { field: 'miningSet', },
+    { field: 'model', type: 1 },
+  ]
+
+  useEffect(() => {
+    const valid = stages.reduce((prev, curr) => prev && project[curr[field]], false)
+    setValidPrepare(valid)
+  }, [project])
 
   async function createIteration() {
     const params = {
@@ -63,11 +113,12 @@ function Prepare({ project = {}, fresh = () => { }, ...func }) {
     <div className={s.iteration}>
       <Row style={{ justifyContent: 'flex-end' }}>
         {stages.map((stage, index) => (
-          <Col key={stage.value} flex={index >= stages.length - 1 ? null : 1}>
-            <Stage pid={project.id} stage={stage} end={index >= stages.length - 1} callback={stage.callback} />
+          <Col key={stage.field} flex={1}>
+            <Stage stage={{ ...stage, callback: fresh }} project={project} />
           </Col>
         ))}
       </Row>
+      <div className={s.createBtn}><Button type='primary' disabled={validPrepare} onClick={createIteration}>使用迭代功能提升模型效果</Button></div>
     </div>
   )
 }
