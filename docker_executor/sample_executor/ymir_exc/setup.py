30f26a637db1fd5f1fc572ae09df4269b438ba53
Phoenix <89957974+phoenix-xhuang@users.noreply.github.com>
2022-05-23 13:43:40 +0800

    [exc] fix setup.py (#685)
    * fix pypi installation
    
    * update
    
    * update

--- docker_executor/sample_executor/ymir_exc/setup.py
+++ docker_executor/sample_executor/ymir_exc/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-__version__ = '1.1.0'
+__version__ = '1.1.0.0523'
 
 requirements = []
 with open('requirements.txt') as f:
@@ -9,12 +9,13 @@ with open('requirements.txt') as f:
         requirements.append(line)
 
 setup(
-    name='ymir_exc',
+    name='ymir-exc',
     version=__version__,
     python_requires=">=3.8.10",
     install_requires=requirements,
     author_email="contact.viesc@gmail.com",
     description="ymir executor SDK: SDK for develop ymir training, mining and infer docker images",
     url="https://github.com/IndustryEssentials/ymir",
-    packages=find_packages(),
+    packages=find_packages(exclude=["*tests*"]),
+    include_package_data=True,
 )
