4fe05150333afb5747a4c508f1994253b3746373
yance <1415940604@qq.com>
2022-05-25 16:49:39 +0800

    [plugin] update pytest coveragerc

--- plugin/fiftyone/tests/__main__.py
+++ plugin/fiftyone/tests/__main__.py
@@ -3,12 +3,15 @@ import sys
 import subprocess
 
 
-def main():
+def main(args):
+    params = " ".join(sys.argv[1:])
     repo_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     # --cov-report term-missing
     cmd = (
-        "PYTHONPATH=$PYTHONPATH:{repo_root} pytest -vv -xs --durations=0 "
-        "--cov=..".format(repo_root=repo_root)
+        "PYTHONPATH=$PYTHONPATH:{repo_root} pytest -vv -xs --durations=0 -n=4 "
+        "--cov=app --cov-config=.coveragerc {params}".format(
+            repo_root=repo_root, params=params
+        )
     )
     subprocess.check_call(cmd, shell=True)
 
