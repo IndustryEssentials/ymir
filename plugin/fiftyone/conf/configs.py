5047aa4cc5791c0e6088aa5dc067119d969109e3
yance <1415940604@qq.com>
2022-05-25 10:51:38 +0800

    change logger and update ci
    change logger and update ci

--- plugin/fiftyone/conf/configs.py
+++ plugin/fiftyone/conf/configs.py
@@ -6,10 +6,10 @@ env = Env()
 
 
 class Config:
-    mongo_uri: str = env.str("MONGO_URI", "mongodb://localhost:27017/")
-    redis_host: str = env.str("REDIS_HOST", "localhost")
-    redis_port: int = env.int("REDIS_PORT", 6379)
-    redis_db: int = env.int("REDIS_DB", 0)
+    mongo_uri: str = env.str("FIFTYONE_DATABASE_URI", "mongodb://localhost:27017/")
+    redis_host: str = env.str("FIFTYONE_REDIS_HOST", "localhost")
+    redis_port: int = env.int("FIFTYONE_REDIS_PORT", 6379)
+    redis_db: int = env.int("FIFTYONE_REDIS_DB", 0)
     allowed_hosts: List[str] = ["*"]
 
 
