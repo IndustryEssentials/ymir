a87a44c17d9776beed78defd7557bc9222e6c57c
yance <1415940604@qq.com>
2022-05-25 16:23:08 +0800

    [plugin] update pytest-cov

--- plugin/fiftyone/tests/__main__.py
+++ plugin/fiftyone/tests/__main__.py
@@ -10,9 +10,7 @@ def main(args):
     # --cov-report term-missing
     cmd = (
         "PYTHONPATH=$PYTHONPATH:{repo_root} pytest -vv -xs --durations=0 -n=4 "
-        "--cov=fiftyone --cov-config=.coveragerc {params}".format(
-            repo_root=repo_root, params=params
-        )
+        "--cov=fiftyone".format(repo_root=repo_root, params=params)
     )
     subprocess.check_call(cmd, shell=True)
 
