df1d4c13b3e9b4c8ac60807effc4ad00773631f4
ymir <ymir@intellif.com>
2022-05-29 17:18:46 +0800

    update

--- ymir/command/mir/tools/data_exporter.py
+++ ymir/command/mir/tools/data_exporter.py
@@ -83,3 +83,53 @@ def export(mir_root: str,
     dw.close()
 
     return True
+
+
+def export_lmdb(mir_root: str,
+                assets_location: str,
+                class_type_ids: Dict[int, int],
+                asset_ids: Set[str],
+                base_branch: str,
+                base_task_id: str,
+                format_type: data_writer.ExportFormat,
+                lmdb_dir: str,
+                index_file_path: str = '') -> bool:
+    """
+    export assets and annotations and pack them to lmdb format
+
+    Args:
+        mir_root (str): path to mir repo root directory
+        assets_location (str): path to assets storage directory
+        class_type_ids (Dict[int, int]): class ids (and it's mapping value)
+            all objects within this dict keys will be exported, if None, export everything;
+        asset_ids (Set[str]): export asset ids
+        base_branch (str): data branch
+        format_type (ExportFormat): format type, NONE means exports no annotations
+        lmdb_dir: lmdb file directory
+        index_file_path (str): path to index file, if None, generates no index file
+
+    Raises:
+        MirRuntimeError
+
+    Returns:
+        bool: returns True if success
+    """
+    rev_tid = revs_parser.TypRevTid()
+    rev_tid.rev = base_branch
+    rev_tid.tid = base_task_id
+
+    dr = data_reader.MirDataReader(mir_root=mir_root,
+                                   typ_rev_tid=rev_tid,
+                                   asset_ids=asset_ids,
+                                   class_ids=set(class_type_ids.keys()))
+    dw = data_writer.LmdbDataWriter(mir_root=mir_root,
+                                    assets_location=assets_location,
+                                    lmdb_dir=lmdb_dir,
+                                    class_ids_mapping=class_type_ids,
+                                    format_type=format_type,
+                                    index_file_path=index_file_path)
+    for asset_id, attrs, annotations in dr.read():
+        dw.write(asset_id=asset_id, attrs=attrs, annotations=annotations)
+    dw.close()
+
+    return True
