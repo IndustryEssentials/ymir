9b4a3ead6f58e3ddaae954080d500bc30bc45e73
liuzz07 <45936183+liuzz07@users.noreply.github.com>
2022-06-09 15:17:49 +0800

    [plugin] add support for rotate annotation (#722)
    * [plugin] add support for rotate annotations
    
    * [plugin] fix python style
    
    * [plugin] update celery config
    
    * [plugin] update rotate annotations
    
    * [plugin] fix python style
    
    * [plugin] update rotate annotation
    
    * [plugin] remove unused imports
    
    * [plugin] update rotate annotation
    
    * [plugin] add confidence and ck
    
    * [plugin] update rotate annotation
    
    * [plugin] update rotate annotation
    
    * [plugin] fix ck and tag in polyline
    
    Co-authored-by: liuzongze <liu.zongze@intellif.com>

--- plugin/fiftyone/app/worker.py
+++ plugin/fiftyone/app/worker.py
@@ -1,13 +1,13 @@
 import csv
+import math
 from pathlib import Path
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 import xmltodict
 from celery import current_app as current_celery_app
 from celery import shared_task
-from fiftyone import Detection, Dataset, Sample, Detections
+from fiftyone import Dataset, Sample, Polyline, Polylines
 from fiftyone.core.metadata import ImageMetadata
-from fiftyone.utils.voc import VOCBoundingBox
 
 from app.models.schemas import Task
 from conf.configs import conf
@@ -24,7 +24,7 @@ def create_celery() -> current_celery_app:
         f"redis://{conf.redis_host}:{conf.redis_port}/{conf.redis_db}"
     )
     celery_app.conf.result_backend = (
-        f"redis://{conf.redis_host}:{conf.redis_port}/{conf.redis_db}"
+        conf.mongo_uri + "/" + conf.fiftyone_database_name
     )
 
     celery_app.conf.task_serializer = "pickle"
@@ -65,6 +65,8 @@ def load_task_data(task: Task) -> None:
                     sample = sample_pool[img_path.name]
                 else:
                     sample = Sample(filepath=base_path / img_path)
+                    for k, v in annotation.get("ck", {}).items():
+                        sample[k] = v
                     sample_pool[img_path.name] = sample
                     _set_metadata(annotation, sample)
                 # if object is empty, skip
@@ -141,24 +143,54 @@ def _add_detections(
         voc_objects = annotation["object"]
     else:
         raise ValueError(f"Invalid object type: {type(annotation['object'])}")
-    detections = _build_detections(
-        voc_objects, sample["metadata"]["width"], sample["metadata"]["height"]
-    )
 
-    sample[ymir_data_name] = Detections(detections=detections)
+    polylines = _build_polylines(voc_objects, sample["metadata"]["width"], sample["metadata"]["height"])
+    sample[ymir_data_name] = Polylines(polylines=polylines)
     return sample
 
 
-def _build_detections(voc_objects: list, width: int, height: int) -> List[Detection]:
-    detections = []
+def _build_polylines(voc_objects: list, width: int, height: int) -> List[Polyline]:
+    polylines = []
     for obj in voc_objects:
         label = obj["name"]
-        bndbox = VOCBoundingBox.from_bndbox_dict(obj["bndbox"]).to_detection_format(
-            frame_size=(width, height)
-        )
-        item = Detection(
+        points = _get_points_from_bndbox(obj["bndbox"], width, height)
+        polyline = Polyline(
             label=label,
-            bounding_box=bndbox,
+            points=[points],
+            confidence=obj.get("confidence"),
+            closed=True
         )
-        detections.append(item)
-    return detections
+        for k, v in obj.get("tag", {}).items():
+            polyline[k] = v
+        polylines.append(polyline)
+    return polylines
+
+
+def _get_points_from_bndbox(bndbox: Dict, width: int, height: int) -> list:
+    # raise error if parameter not exist
+    xmin, ymin = float(bndbox["xmin"]), float(bndbox["ymin"])
+    xmax, ymax = float(bndbox["xmax"]), float(bndbox["ymax"])
+    angle = float(bndbox.get("rotate_angle", 0)) * math.pi
+
+    cx, cy = (xmin + xmax) / 2, (ymin + ymax) / 2
+    half_w, half_h = (xmax - xmin) / 2, (ymax - ymin) / 2
+
+    p0x, p0y = _rotate_point(cx, cy, cx - half_w, cy - half_h, -angle, width, height)
+    p1x, p1y = _rotate_point(cx, cy, cx + half_w, cy - half_h, -angle, width, height)
+    p2x, p2y = _rotate_point(cx, cy, cx + half_w, cy + half_h, -angle, width, height)
+    p3x, p3y = _rotate_point(cx, cy, cx - half_w, cy + half_h, -angle, width, height)
+
+    return [(p0x, p0y), (p1x, p1y), (p2x, p2y), (p3x, p3y)]
+
+
+def _rotate_point(cx: float, cy: float, xp: float, yp: float, theta: float, width: int,
+                  height: int) -> Tuple[float, float]:
+    xoff = xp - cx
+    yoff = yp - cy
+
+    cos_theta = math.cos(theta)
+    sin_theta = math.sin(theta)
+    resx = cos_theta * xoff + sin_theta * yoff
+    resy = - sin_theta * xoff + cos_theta * yoff
+
+    return (cx + resx) / width, (cy + resy) / height
