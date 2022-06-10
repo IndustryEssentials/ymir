264e84068f440f3f82f50f9613b44911e5f3a0d8
youdaoyzbx <youdaoyzbx@163.com>
2022-06-10 12:09:34 +0800

    use class to avoid multiple tensorboard log files

--- docker_executor/sample_executor/ymir_exc/ymir_exc/monitor.py
+++ docker_executor/sample_executor/ymir_exc/ymir_exc/monitor.py
@@ -1,4 +1,3 @@
-import os
 import time
 
 from tensorboardX import SummaryWriter
@@ -14,27 +13,28 @@ def write_monitor_logger(percent: float) -> None:
         f.write(f"{env_config.task_id}\t{time.time()}\t{percent:.2f}\t{TASK_STATE_RUNNING}\n")
 
 
-def write_tensorboard_text(text: str, tb_log_file: str = 'tensorboard_text.log',
-                           tag: str = "", global_step: int = -1) -> None:
-    """
-    donot call this function too often, it may overwrite history log text with the same `tag` and `global_step`
-    """
-    env_config = env.get_current_env()
-    tb_log_file = os.path.join(env_config.output.tensorboard_dir, tb_log_file)
-    writer = SummaryWriter(tb_log_file)
+class YmirTensorboardLog:
+    def __init__(self) -> None:
+        env_config = env.get_current_env()
+        self.writer = SummaryWriter(env_config.output.tensorboard_dir)
 
-    now = int(round(time.time() * 1000))
-    tag = tag if tag else str(now)
-    global_step = global_step if global_step >= 0 else now
-    writer.add_text(tag=tag, text_string=text, global_step=global_step)
-    writer.close()
+    def __del__(self) -> None:
+        self.writer.close()
 
+    def write_tensorboard_text(self, text: str, tag: str = "") -> None:
+        """
+        donot call this function too often, tensorboard may
+        overwrite history log text with the same `tag` and `global_step`
+        """
+        now = int(round(time.time() * 1000))
+        tag = tag if tag else str(now)
+        self.writer.add_text(tag=tag, text_string=text, global_step=now)
 
-def write_final_executor_log(tb_log_file: str = 'tensorboard_text.log',
-                             tag: str = "", global_step: int = -1) -> None:
-    env_config = env.get_current_env()
-    exe_log_file = env_config.output.executor_log_file
-    with open(exe_log_file) as f:
-        # use the raw text format instead of markdown
-        text = "```\n" + f.read() + "\n```"
-        write_tensorboard_text(text, tb_log_file=tb_log_file, tag=tag, global_step=global_step)
+    def write_final_executor_log(self,
+                                 tag: str = "") -> None:
+        env_config = env.get_current_env()
+        exe_log_file = env_config.output.executor_log_file
+        with open(exe_log_file) as f:
+            # use the raw text format instead of markdown
+            text = "```\n" + f.read() + "\n```"
+            self.write_tensorboard_text(text, tag=tag)
