5e3280a732793fe8488a6c7ff6fdd9bfd1584096
youdaoyzbx <youdaoyzbx@163.com>
2022-06-10 16:27:30 +0800

    simple is better

--- docker_executor/sample_executor/ymir_exc/ymir_exc/monitor.py
+++ docker_executor/sample_executor/ymir_exc/ymir_exc/monitor.py
@@ -1,5 +1,4 @@
 import time
-from typing import Callable, Dict
 
 from tensorboardX import SummaryWriter
 
@@ -8,47 +7,29 @@ from ymir_exc import env
 TASK_STATE_RUNNING = 2
 
 
-def singleton(cls: Callable) -> object:
-    _instance = {}
-
-    def inner(*args: tuple, **kwargs: Dict[str, dict]) -> object:
-        if cls not in _instance:
-            _instance[cls] = cls(*args, **kwargs)
-        return _instance[cls]
-
-    return inner
-
-
 def write_monitor_logger(percent: float) -> None:
     env_config = env.get_current_env()
     with open(env_config.output.monitor_file, 'w') as f:
         f.write(f"{env_config.task_id}\t{time.time()}\t{percent:.2f}\t{TASK_STATE_RUNNING}\n")
 
 
-@singleton
-class YmirTensorboardLog:
-    def __init__(self) -> None:
-        env_config = env.get_current_env()
-        self.writer = SummaryWriter(env_config.output.tensorboard_dir)
+def write_tensorboard_text(text: str, tag: str = "") -> None:
+    """
+    donot call this function too often, tensorboard may
+    overwrite history log text with the same `tag` and `global_step`
+    """
+    env_config = env.get_current_env()
+    now = int(round(time.time() * 1000))
+    tag = tag if tag else "default"
 
-    def __del__(self) -> None:
-        self.writer.close()
+    # show the raw text format instead of markdown
+    text = f"```\n {text} \n```"
+    with SummaryWriter(env_config.output.tensorboard_dir) as f:
+        f.add_text(tag=tag, text_string=text, global_step=now)
 
-    def write_tensorboard_text(self, text: str, tag: str = "") -> None:
-        """
-        donot call this function too often, tensorboard may
-        overwrite history log text with the same `tag` and `global_step`
-        """
-        now = int(round(time.time() * 1000))
-        tag = tag if tag else "default"
 
-        # show the raw text format instead of markdown
-        text = "```\n" + text + "\n```"
-        self.writer.add_text(tag=tag, text_string=text, global_step=now)
-
-    def write_final_executor_log(self,
-                                 tag: str = "") -> None:
-        env_config = env.get_current_env()
-        exe_log_file = env_config.output.executor_log_file
-        with open(exe_log_file) as f:
-            self.write_tensorboard_text(f.read(), tag=tag)
+def write_final_executor_log(tag: str = "") -> None:
+    env_config = env.get_current_env()
+    exe_log_file = env_config.output.executor_log_file
+    with open(exe_log_file) as f:
+        write_tensorboard_text(f.read(), tag=tag)
