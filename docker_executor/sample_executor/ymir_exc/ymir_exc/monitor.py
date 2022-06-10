44ec6934a06ae0513d4381a74132068c591c0326
youdaoyzbx <youdaoyzbx@163.com>
2022-06-10 10:34:23 +0800

    remove global variable

--- docker_executor/sample_executor/ymir_exc/ymir_exc/monitor.py
+++ docker_executor/sample_executor/ymir_exc/ymir_exc/monitor.py
@@ -1,13 +1,11 @@
-import os.path as osp
+import os
 import time
 
-from pygtail import Pygtail
 from tensorboardX import SummaryWriter
 
 from ymir_exc import env
 
 TASK_STATE_RUNNING = 2
-_TENSORBOARD_GLOBAL_STEP = 0
 
 
 def write_monitor_logger(percent: float) -> None:
@@ -16,15 +14,27 @@ def write_monitor_logger(percent: float) -> None:
         f.write(f"{env_config.task_id}\t{time.time()}\t{percent:.2f}\t{TASK_STATE_RUNNING}\n")
 
 
-def write_tensorboard_text() -> None:
+def write_tensorboard_text(text: str, tb_log_file: str = 'tensorboard_text.log',
+                           tag: str = "", global_step: int = -1) -> None:
+    """
+    donot call this function too often, it may overwrite history log text with the same `tag` and `global_step`
+    """
     env_config = env.get_current_env()
-    tb_log_file = osp.join(env_config.output.tensorboard_dir, 'tensorboard_text.log')
-    executor_log_file = env_config.output.executor_log_file
+    tb_log_file = os.path.join(env_config.output.tensorboard_dir, tb_log_file)
     writer = SummaryWriter(tb_log_file)
 
-    # Pygtail always return the new lines
-    for line in Pygtail(executor_log_file):
-        writer.add_text(tag='ymir-executor', text_string=line, global_step=_TENSORBOARD_GLOBAL_STEP)
-        _TENSORBOARD_GLOBAL_STEP += 1
-
+    now = int(round(time.time() * 1000))
+    tag = tag if tag else str(now)
+    global_step = global_step if global_step >= 0 else now
+    writer.add_text(tag=tag, text_string=text, global_step=global_step)
     writer.close()
+
+
+def write_final_executor_log(tb_log_file: str = 'tensorboard_text.log',
+                             tag: str = "", global_step: int = -1) -> None:
+    env_config = env.get_current_env()
+    exe_log_file = env_config.output.executor_log_file
+    with open(exe_log_file) as f:
+        # use the raw text format instead of markdown
+        text = "```\n" + f.read() + "\n```"
+        write_tensorboard_text(text, tb_log_file=tb_log_file, tag=tag, global_step=global_step)
