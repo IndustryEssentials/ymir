0c4894be6cba3fae71b1cbc7489d3b88263c8dfa
Phoenix <89957974+phoenix-xhuang@users.noreply.github.com>
2022-06-11 13:36:54 +0800

    update

--- ymir/backend/src/ymir_controller/tests/unit/test_invoker_task_training.py
+++ ymir/backend/src/ymir_controller/tests/unit/test_invoker_task_training.py
@@ -119,6 +119,8 @@ class TestInvokerTaskTraining(unittest.TestCase):
             'modelsuploadlocation': self._storage_root,
             'assetskvlocation': self._storage_root,
             'tensorboard_root': self._tensorboard_root,
+            'openpai_host': '',
+            'openpai_token': '',
         }
 
         working_dir_root = os.path.join(self._sandbox_root, "work_dir",
