e6e24d300d5003c75f34dc51cd76ef86481adafd
IJtLJZ8Rm4Yr <90443055+IJtLJZ8Rm4Yr@users.noreply.github.com>
2022-06-16 15:08:26 +0800

    [backend] minor fix: replace ModelStageBase (#757)
    [backend] replace ModelStageBase

--- ymir/backend/src/ymir_app/app/crud/crud_model_stage.py
+++ ymir/backend/src/ymir_app/app/crud/crud_model_stage.py
@@ -1,10 +1,10 @@
 from app.crud.base import CRUDBase
-from app.models import Model
+from app.models import ModelStage
 from app.schemas.model_stage import ModelStageCreate, ModelStageUpdate
 
 
-class CRUDModelStage(CRUDBase[Model, ModelStageCreate, ModelStageUpdate]):
+class CRUDModelStage(CRUDBase[ModelStage, ModelStageCreate, ModelStageUpdate]):
     pass
 
 
-model_stage = CRUDModelStage(Model)
+model_stage = CRUDModelStage(ModelStage)
