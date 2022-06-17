cc1228a8867dd3f7df3bd0f24cd3e3cf3a61e189
jingyiliu <liu.jingyi@intellif.com>
2022-06-17 11:35:13 +0800

    [backend] check if model stage name is valid beforehand

--- ymir/backend/src/ymir_app/app/crud/crud_model_stage.py
+++ ymir/backend/src/ymir_app/app/crud/crud_model_stage.py
@@ -1,10 +1,23 @@
+from typing import Any, Optional
+from sqlalchemy.orm import Session
+
 from app.crud.base import CRUDBase
 from app.models import ModelStage
 from app.schemas.model_stage import ModelStageCreate, ModelStageUpdate
 
 
 class CRUDModelStage(CRUDBase[ModelStage, ModelStageCreate, ModelStageUpdate]):
-    pass
+    def get(self, db: Session, id: Any) -> Optional[ModelStage]:
+        stage = db.query(self.model).filter(self.model.id == id).first()
+        if stage and not is_valid_model_stage_name(stage.name):
+            raise ValueError("Invalid Model Stage Name")
+        return stage
+
+
+def is_valid_model_stage_name(name: Optional[str]) -> bool:
+    if not name:
+        return False
+    return name.isidentifier()
 
 
 model_stage = CRUDModelStage(ModelStage)
