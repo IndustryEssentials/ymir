3b598d12d79ff75d3f420ff2f0105d81054d034b
liule1613 <102013374+liule1613@users.noreply.github.com>
2022-06-18 11:53:24 +0800

    [web] PA add data analysis (#766)
    * [web] PA add data analysis
    
    * [web] PA resolve conversation
    
    * [web] PA modify data analysis colunm name

--- ymir/web/src/interface/dataset.ts
+++ ymir/web/src/interface/dataset.ts
@@ -1,5 +1,5 @@
 
-import { Result } from "@/interface/common"
+import { Result, BackendData } from "@/interface/common"
 type Keywords = {
   [key: string]: number,
 }
@@ -19,4 +19,23 @@ export interface Dataset extends Result {
   projectNagetiveCount?: number,
   assetCount: number,
   ignoredKeywords: Array<string>,
+}
+
+export interface DatasetAnalysis {
+  name: string,
+  version: number,
+  versionName: string,
+  assetCount: number,
+  totalAssetMbytes: number,
+  annosCnt: number,
+  aveAnnosCnt: number,
+  positiveAssetCnt: number,
+  negativeAssetCnt: number,
+  assetBytes: Array<BackendData>,
+  assetHWRatio: Array<BackendData>,
+  assetArea: Array<BackendData>,
+  assetQuality: Array<BackendData>,
+  annoAreaRatio: Array<BackendData>,
+  annoQuality: Array<BackendData>,
+  classNamesCount: BackendData,
 }
