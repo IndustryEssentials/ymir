47ea5b799393b5aa476f00950203813024ea02cf
ymir <ymir@intellif.com>
2022-06-18 22:23:32 +0800

    update viz

--- ymir/backend/src/ymir_viz/src/controllers/model_controller.py
+++ ymir/backend/src/ymir_viz/src/controllers/model_controller.py
@@ -35,6 +35,8 @@ def get_model_info(user_id: str, repo_id: str, branch_id: str) -> ModelResult:
             model_mAP=model_info["mean_average_precision"],
             task_parameters=model_info["task_parameters"],
             executor_config=model_info["executor_config"],
+            best_stage_name=model_info['best_stage_name'],
+            model_stages=model_info['stages'],
         )
     })
     logging.info(f"get_model_info: {resp}")
