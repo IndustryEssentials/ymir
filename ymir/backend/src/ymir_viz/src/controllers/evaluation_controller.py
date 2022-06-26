8464ef5d8808458432b4035cb51bf9c78fb2a305
ymir <ymir@intellif.com>
2022-06-26 11:44:22 +0800

    ymir_viz: dataset_fast_evaluate: add need_pr_curve

--- ymir/backend/src/ymir_viz/src/controllers/evaluation_controller.py
+++ ymir/backend/src/ymir_viz/src/controllers/evaluation_controller.py
@@ -39,8 +39,8 @@ def get_dataset_evaluations(user_id: str, repo_id: str, branch_id: str) -> Datas
     return DatasetEvaluationResult(**resp)
 
 
-def dataset_fast_evaluation(user_id: str, repo_id: str, branch_id: str, conf_thr: float,
-                            iou_thr: float) -> DatasetEvaluationResult:
+def dataset_fast_evaluation(user_id: str, repo_id: str, branch_id: str, conf_thr: float, iou_thr: float,
+                            need_pr_curve: bool) -> DatasetEvaluationResult:
     rev_tid = revs_parser.parse_single_arg_rev(branch_id, need_tid=False)
     mir_root = os.path.join(viz_settings.BACKEND_SANDBOX_ROOT, user_id, repo_id)
 
@@ -48,7 +48,7 @@ def dataset_fast_evaluation(user_id: str, repo_id: str, branch_id: str, conf_thr
                                        rev_tid=rev_tid,
                                        conf_thr=conf_thr,
                                        iou_thrs=str(iou_thr),
-                                       need_pr_curve=True)
+                                       need_pr_curve=need_pr_curve)
 
     return _get_dataset_evaluation_result(evaluation)
 
