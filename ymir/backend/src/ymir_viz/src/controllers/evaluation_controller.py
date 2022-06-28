95f91b4ae52b5200c473c44a45b8eb33e7c7680a
ymir <ymir@intellif.com>
2022-06-28 11:16:33 +0800

    update

--- ymir/backend/src/ymir_viz/src/controllers/evaluation_controller.py
+++ ymir/backend/src/ymir_viz/src/controllers/evaluation_controller.py
@@ -3,11 +3,9 @@ import os
 
 from google.protobuf import json_format
 from mir.tools import det_eval, revs_parser
-from mir.tools.code import MirCode
-from mir.tools.errors import MirRuntimeError
 
 from src.config import viz_settings
-from src.libs import utils, exceptions
+from src.libs import utils
 from src.swagger_models import DatasetEvaluationResult
 from src.viz_models import pb_reader
 
@@ -40,38 +38,22 @@ def get_dataset_evaluations(user_id: str, repo_id: str, branch_id: str) -> Datas
     return DatasetEvaluationResult(**resp)
 
 
-@exceptions.catch_viz_exceptions
 def dataset_fast_evaluation(user_id: str, repo_id: str, branch_id: str, conf_thr: float, iou_thr: float,
                             need_pr_curve: bool) -> DatasetEvaluationResult:
     rev_tid = revs_parser.parse_single_arg_rev(branch_id, need_tid=False)
     mir_root = os.path.join(viz_settings.BACKEND_SANDBOX_ROOT, user_id, repo_id)
 
-    code: int
-    message: str
-    result: dict
+    evaluation = det_eval.det_evaluate(mir_root=mir_root,
+                                       rev_tid=rev_tid,
+                                       conf_thr=conf_thr,
+                                       iou_thrs=str(iou_thr),
+                                       need_pr_curve=need_pr_curve)
 
-    try:
-        evaluation = det_eval.det_evaluate(mir_root=mir_root,
-                                           rev_tid=rev_tid,
-                                           conf_thr=conf_thr,
-                                           iou_thrs=str(iou_thr),
-                                           need_pr_curve=need_pr_curve)
-        logging.info(f"successfully dataset_fast_evaluation from branch {branch_id}")
+    logging.info(f"successfully dataset_fast_evaluation from branch {branch_id}")
 
-        code = 0
-        message = 'operation successful'
-        result = json_format.MessageToDict(evaluation,
-                                           including_default_value_fields=True,
-                                           preserving_proto_field_name=True,
-                                           use_integers_for_enums=True)['dataset_evaluations']
-    except MirRuntimeError as e:
-        code = e.error_code
-        message = e.error_message
-        result = {}
-    except Exception as e:
-        code = MirCode.RC_CMD_ERROR_UNKNOWN
-        message = str(e)
-        result = {}
-
-    resp = utils.suss_resp(code=code, message=message, result=result)
+    resp = utils.suss_resp()
+    resp["result"] = json_format.MessageToDict(evaluation,
+                                               including_default_value_fields=True,
+                                               preserving_proto_field_name=True,
+                                               use_integers_for_enums=True)['dataset_evaluations']
     return DatasetEvaluationResult(**resp)
