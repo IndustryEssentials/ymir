5d1e5851cef63b3940aa26fc1cd17b1fe36591fd
ymir <ymir@intellif.com>
2022-06-29 11:55:18 +0800

    bug fix

--- ymir/command/mir/commands/mining.py
+++ ymir/command/mir/commands/mining.py
@@ -202,6 +202,9 @@ def _process_results(mir_root: str, export_out: str, dst_typ_rev_tid: revs_parse
                      task: mirpb.Task) -> int:
     # step 1: build topk results:
     #   read old
+    mir_metadatas: mirpb.MirMetadatas
+    mir_annotations: mirpb.MirAnnotations
+
     [mir_metadatas, mir_annotations] = mir_storage_ops.MirStorageOps.load_multiple_storages(
         mir_root=mir_root,
         mir_branch=src_typ_rev_tid.rev,
@@ -249,7 +252,7 @@ def _process_results(mir_root: str, export_out: str, dst_typ_rev_tid: revs_parse
         for asset_id in pred_asset_ids:
             prediction.image_annotations[asset_id].CopyFrom(mir_annotations.prediction.image_annotations[asset_id])
 
-    gt_asset_ids = set(ground_truth.image_annotations.keys()) & asset_ids_set
+    gt_asset_ids = set(mir_annotations.ground_truth.image_annotations.keys()) & asset_ids_set
     for asset_id in gt_asset_ids:
         ground_truth.image_annotations[asset_id].CopyFrom(mir_annotations.ground_truth.image_annotations[asset_id])
 
