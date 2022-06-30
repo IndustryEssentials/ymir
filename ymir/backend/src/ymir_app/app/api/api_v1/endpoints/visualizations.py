a19a78fc90d5b953ac02b3571b0525fcb1ebfd60
liuzz07 <zzl1993@126.com>
2022-06-30 11:53:19 +0800

    [backend] improved

--- ymir/backend/src/ymir_app/app/api/api_v1/endpoints/visualizations.py
+++ ymir/backend/src/ymir_app/app/api/api_v1/endpoints/visualizations.py
@@ -67,8 +67,8 @@ def create_visualization(
     if not tasks:
         raise TaskNotFound()
 
-    visualization = crud.visualization.create_visualization(db, user_id=current_user.id, conf_thr=obj_in.conf_thr,
-                                                            iou_thr=obj_in.iou_thr, project_id=obj_in.project_id)
+    visualization = crud.visualization.create_visualization(db, user_id=current_user.id, project_id=obj_in.project_id,
+                                                            conf_thr=obj_in.conf_thr, iou_thr=obj_in.iou_thr, )
 
     for task in tasks:
         crud.task_visual_relationship.create_relationship(db, task_id=task.id, visualization_id=visualization.id)
--- ymir/backend/src/ymir_app/tests/utils/visualizations.py
+++ ymir/backend/src/ymir_app/tests/utils/visualizations.py
@@ -14,7 +14,7 @@ def create_visualization_record(
     project_id: Optional[int] = None,
 ):
     user_id = user_id or randint(1, 20)
-    record = crud.visualization.create_visualization(db, user_id=user_id, conf_thr=0.0005, iou_thr=0.5,
-                                                     project_id=project_id)
+    record = crud.visualization.create_visualization(db, user_id=user_id, project_id=project_id, conf_thr=0.0005,
+                                                     iou_thr=0.5)
     crud.task_visual_relationship.create_relationship(db, task_id=task.id, visualization_id=record.id)
     return record
