358ebd9dad8bc30d9a345737fe46ba56911dae96
Phoenix <89957974+phoenix-xhuang@users.noreply.github.com>
2022-06-29 19:46:49 -0700

    fix ci

--- ymir/command/mir/tools/det_eval.py
+++ ymir/command/mir/tools/det_eval.py
@@ -626,19 +626,10 @@ class CocoDetEval:
             return mean_s
 
         def _summarizeDets() -> np.ndarray:
-            stats = np.zeros((12, ))
+            stats = np.zeros((3, ))
             stats[0] = _summarize(1)
-            stats[1] = _summarize(1, iouThr=.5, maxDets=self.params.maxDets[2])
-            stats[2] = _summarize(1, iouThr=.75, maxDets=self.params.maxDets[2])
-            stats[3] = _summarize(1, areaRng='small', maxDets=self.params.maxDets[2])
-            stats[4] = _summarize(1, areaRng='medium', maxDets=self.params.maxDets[2])
-            stats[5] = _summarize(1, areaRng='large', maxDets=self.params.maxDets[2])
-            stats[6] = _summarize(0, maxDets=self.params.maxDets[0])
-            stats[7] = _summarize(0, maxDets=self.params.maxDets[1])
-            stats[8] = _summarize(0, maxDets=self.params.maxDets[2])
-            stats[9] = _summarize(0, areaRng='small', maxDets=self.params.maxDets[2])
-            stats[10] = _summarize(0, areaRng='medium', maxDets=self.params.maxDets[2])
-            stats[11] = _summarize(0, areaRng='large', maxDets=self.params.maxDets[2])
+            stats[1] = _summarize(1, iouThr=.5, maxDets=self.params.maxDets[-1])
+            stats[2] = _summarize(1, iouThr=.75, maxDets=self.params.maxDets[-1])
             return stats
 
         if not self.eval:
