4645c5d9de7f7b055fbb5750926787c2bdc7f935
Fenrir <90444968+fenrir-z@users.noreply.github.com>
2022-07-05 14:41:53 +0800

    [command] preprocess (#877)
    * add dpp
    
    * add dpp to mir train
    
    * WIP
    
    * update
    
    * update
    
    * remove unused code
    
    * update
    
    * optimize raw data writer process
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * bug fix
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * remove unused code
    
    * update
    
    * update
    
    * remove default value of return_bytes in func prep_img
    
    Co-authored-by: ymir <ymir@intellif.com>
    Co-authored-by: IJtLJZ8Rm4Yr <90443055+IJtLJZ8Rm4Yr@users.noreply.github.com>
    Co-authored-by: Phoenix <89957974+phoenix-xhuang@users.noreply.github.com>

--- ymir/command/mir/commands/training.py
+++ ymir/command/mir/commands/training.py
@@ -10,7 +10,8 @@ import yaml
 
 from mir.commands import base
 from mir.protos import mir_command_pb2 as mirpb
-from mir.tools import checker, class_ids, context, data_reader, data_writer, mir_storage_ops, revs_parser
+from mir.tools import checker, class_ids, context, data_reader, data_writer
+from mir.tools import mir_storage_ops, revs_parser
 from mir.tools import settings as mir_settings, utils as mir_utils
 from mir.tools.command_run_in_out import command_run_in_out
 from mir.tools.code import MirCode
@@ -286,6 +287,7 @@ class CmdTrain(base.BaseCommand):
 
         dw_train: data_writer.BaseDataWriter
         dw_val: data_writer.BaseDataWriter
+        prep_args = config.get(mir_settings.TASK_CONTEXT_KEY, {}).get(mir_settings.TASK_CONTEXT_PREPROCESS_KEY, {})
         if asset_format == data_writer.AssetFormat.ASSET_FORMAT_RAW:
             dw_train = data_writer.RawDataWriter(
                 mir_root=mir_root,
@@ -303,6 +305,7 @@ class CmdTrain(base.BaseCommand):
                 index_assets_prefix='/in/assets',
                 index_annotations_prefix='/in/annotations',
                 index_gt_prefix='/in/groundtruth',
+                prep_args=prep_args,
             )
             dw_val = data_writer.RawDataWriter(
                 mir_root=mir_root,
@@ -320,6 +323,7 @@ class CmdTrain(base.BaseCommand):
                 index_assets_prefix='/in/assets',
                 index_annotations_prefix='/in/annotations',
                 index_gt_prefix='/in/groundtruth',
+                prep_args=prep_args,
             )
         elif asset_format == data_writer.AssetFormat.ASSET_FORMAT_LMDB:
             asset_dir = os.path.join(work_dir_in, 'lmdb')
@@ -327,37 +331,45 @@ class CmdTrain(base.BaseCommand):
 
             # export train set
             train_lmdb_dir = os.path.join(asset_dir, 'train')
+            dw_train = data_writer.LmdbDataWriter(
+                mir_root=mir_root,
+                assets_location=media_location,
+                lmdb_dir=train_lmdb_dir,
+                class_ids_mapping=type_id_idx_mapping,
+                format_type=export_format,
+                index_file_path=os.path.join(work_dir_in, 'train-index.tsv'),
+                prep_args=prep_args,
+            )
             if asset_cache_dir:
                 orig_lmdb_dir = os.path.join(asset_cache_dir, 'tr', src_revs)
+                if dw_train.signature:
+                    orig_lmdb_dir = os.path.join(orig_lmdb_dir, dw_train.signature)
                 os.makedirs(orig_lmdb_dir, exist_ok=True)
 
                 os.symlink(orig_lmdb_dir, train_lmdb_dir)
             else:
                 os.makedirs(train_lmdb_dir, exist_ok=True)
 
-            dw_train = data_writer.LmdbDataWriter(mir_root=mir_root,
-                                                  assets_location=media_location,
-                                                  lmdb_dir=train_lmdb_dir,
-                                                  class_ids_mapping=type_id_idx_mapping,
-                                                  format_type=export_format,
-                                                  index_file_path=os.path.join(work_dir_in, 'train-index.tsv'))
-
             # export validation set
             val_lmdb_dir = os.path.join(asset_dir, 'val')
+            dw_val = data_writer.LmdbDataWriter(
+                mir_root=mir_root,
+                assets_location=media_location,
+                lmdb_dir=val_lmdb_dir,
+                class_ids_mapping=type_id_idx_mapping,
+                format_type=export_format,
+                index_file_path=os.path.join(work_dir_in, 'val-index.tsv'),
+                prep_args=prep_args,
+            )
             if asset_cache_dir:
                 orig_lmdb_dir = os.path.join(asset_cache_dir, 'va', src_revs)
+                if dw_val.signature:
+                    orig_lmdb_dir = os.path.join(orig_lmdb_dir, dw_val.signature)
                 os.makedirs(orig_lmdb_dir, exist_ok=True)
 
                 os.symlink(orig_lmdb_dir, val_lmdb_dir)
             else:
                 os.makedirs(val_lmdb_dir, exist_ok=True)
-
-            dw_val = data_writer.LmdbDataWriter(mir_root=mir_root,
-                                                assets_location=media_location,
-                                                lmdb_dir=val_lmdb_dir,
-                                                class_ids_mapping=type_id_idx_mapping,
-                                                format_type=export_format,
-                                                index_file_path=os.path.join(work_dir_in, 'val-index.tsv'))
         else:
             raise MirRuntimeError(error_code=MirCode.RC_CMD_INVALID_ARGS,
                                   error_message=f"training unsupported asset format: {asset_format}")
