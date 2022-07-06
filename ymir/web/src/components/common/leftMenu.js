b5721406c15bf04aaf9b8d96fdb0990a33138c84
elloitli <li.shanlue@intellif.com>
2022-07-06 12:15:16 +0800

    hide permission on left menu

--- ymir/web/src/components/common/leftMenu.js
+++ ymir/web/src/components/common/leftMenu.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react"
 import { Menu, Layout } from "antd"
-import { useHistory, useLocation, withRouter } from "umi"
+import { useHistory, useLocation, withRouter, connect } from "umi"
 import t from '@/utils/t'
+import { isSuperAdmin } from '@/constants/user'
 import { BarchartIcon, FlagIcon, GithubIcon, FileHistoryIcon, MymodelIcon, NavDatasetIcon, UserIcon, UserSettingsIcon } from '@/components/common/icons'
 import { EditIcon, EyeOffIcon } from "./icons"
 
@@ -13,7 +14,8 @@ const getItem = (label, key, icon, children, type = '') => ({
 
 const getGroupItem = (label, key, children) => getItem(label, key, undefined, children, 'group')
 
-function LeftMenu() {
+function LeftMenu({ role }) {
+  console.log('role:', role)
   const history = useHistory()
   const { pathname } = useLocation()
   const [defaultKeys, setDefaultKeys] = useState(null)
@@ -47,7 +49,7 @@ function LeftMenu() {
       ]),
       getGroupItem(t('common.top.menu.configure'), 'settings', [
         getItem(t('common.top.menu.image'), '/home/image', <FileHistoryIcon />,),
-        getItem(t('common.top.menu.permission'), '/home/permission', <UserSettingsIcon />,),
+        isSuperAdmin(role) ? getItem(t('common.top.menu.permission'), '/home/permission', <UserSettingsIcon />,) : null,
       ]),
       { type: 'divider' },
       getItem(t('user.settings'), '/home/user', <UserIcon />,),
@@ -67,5 +69,9 @@ function LeftMenu() {
     </Sider>
   ) : null
 }
-
-export default withRouter(LeftMenu)
+const props = state => {
+  return {
+    role: state.user.role,
+  }
+}
+export default withRouter(connect(props)(LeftMenu))
