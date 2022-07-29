9ba9cf715e95de1a857feb629d65a83f419f86f2
Elliot <li.shanlue@intellif.com>
2022-07-29 19:20:28 +0800

    model empty add train link

--- ymir/web/src/components/empty/model.js
+++ ymir/web/src/components/empty/model.js
@@ -4,18 +4,23 @@ import { useHistory, useParams } from 'umi'
 
 import t from '@/utils/t'
 import styles from './empty.less'
-import { NoXlmxIcon, TrainIcon, } from '@/components/common/icons'
+import { NoXlmxIcon, TrainIcon, ImportIcon } from '@/components/common/icons'
 
-export default ({ style={} }) => {
+export default ({ style = {} }) => {
   const { id } = useParams()
   const history = useHistory()
   return (
     <Space className={styles.empty} style={style} direction="vertical">
       <NoXlmxIcon className={styles.primaryIcon} style={{ fontSize: 80 }} />
       <h3>{t("model.empty.label")}</h3>
-      <Button type="primary" style={{ pointerEvents: 'auto' }} onClick={() => history.push(`/home/project/${id}/model/import`)}>
-        <TrainIcon /> {t('model.empty.btn.label')}
-      </Button>
+      <Space>
+        <Button type="primary" style={{ pointerEvents: 'auto' }} onClick={() => history.push(`/home/project/${id}/train`)}>
+          <TrainIcon /> {t('common.action.train')}
+        </Button>
+        <Button type="primary" style={{ pointerEvents: 'auto' }} onClick={() => history.push(`/home/project/${id}/model/import`)}>
+          <ImportIcon /> {t('model.empty.btn.label')}
+        </Button>
+      </Space>
     </Space>
   )
 }
