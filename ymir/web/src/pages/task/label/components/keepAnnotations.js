5a07ca47949c4e43554f0e0691cad2bfe79a7afe
elliotmessi <90443217+elliotmessi@users.noreply.github.com>
2022-08-08 12:25:32 +0800

    [web] gt/pred optional for labelling (#997)
    * gt/pred optional for labelling
    
    * update api
    
    Co-authored-by: Phoenix <89957974+phoenix-xhuang@users.noreply.github.com>

---
+++ ymir/web/src/pages/task/label/components/keepAnnotations.js
@@ -0,0 +1,22 @@
+import { Form, Radio } from "antd"
+import t from "@/utils/t"
+
+const options = [
+  { value: 0, label: 'gt' },
+  { value: 1, label: 'pred' },
+  { value: undefined, label: 'none' },
+]
+
+const KeepAnnotations = ({ initialValue, ...rest }) => {
+  const prefix = 'task.label.form.keep_anno.'
+  return <Form.Item name='keepAnnotations'
+    required
+    label={t(`${prefix}label`)}
+    initialValue={initialValue}
+    {...rest}
+  >
+    <Radio.Group options={options.map(opt => ({ ...opt, label: t(prefix + opt.label) }))} />
+  </Form.Item>
+}
+
+export default KeepAnnotations
