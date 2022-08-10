9edfbe8ecdd1a2c3815a7eeb53b74632973558fe
Phoenix <89957974+phoenix-xhuang@users.noreply.github.com>
2022-08-10 16:31:01 -0700

    add ci framework

--- ymir/backend/src/ymir_viewer/services/handler.go
+++ ymir/backend/src/ymir_viewer/services/handler.go
@@ -6,21 +6,23 @@ import (
 	"time"
 
 	"github.com/IndustryEssentials/ymir-viewer/common/constants"
-	"github.com/IndustryEssentials/ymir-viewer/common/pbreader"
+	"github.com/IndustryEssentials/ymir-viewer/common/loader"
 	"github.com/IndustryEssentials/ymir-viewer/common/protos"
 	"github.com/IndustryEssentials/ymir-viewer/tools"
 )
 
-func loadAndCacheAssets(mongo MongoServer, mirRepo constants.MirRepo) {
+func loadAndCacheAssets(mongo MongoServer, mirLoader loader.BaseMirRepoLoader) {
 	defer tools.TimeTrack(time.Now())
+
+	mirRepo := mirLoader.GetMirRepo()
 	if mongo.checkExistence(mirRepo) {
 		log.Printf("Mongodb ready for %s.", fmt.Sprint(mirRepo))
 	} else {
 		log.Printf("No data for %s, reading & building cache.", fmt.Sprint(mirRepo))
-		mirAssetDetails := pbreader.LoadAssetsInfo(mirRepo)
+		mirAssetsDetail := mirLoader.LoadAssetsDetail()
 
 		newData := make([]interface{}, 0)
-		for _, v := range mirAssetDetails {
+		for _, v := range mirAssetsDetail {
 			newData = append(newData, v)
 		}
 		mongo.IndexMongoData(mirRepo, newData)
@@ -29,7 +31,7 @@ func loadAndCacheAssets(mongo MongoServer, mirRepo constants.MirRepo) {
 
 func GetAssetsHandler(
 	mongo MongoServer,
-	mirRepo constants.MirRepo,
+	mirLoader loader.BaseMirRepoLoader,
 	offset int,
 	limit int,
 	classIds []int,
@@ -38,16 +40,15 @@ func GetAssetsHandler(
 	cks []string,
 	tags []string,
 ) constants.QueryAssetsResult {
-	loadAndCacheAssets(mongo, mirRepo)
-	return mongo.QueryAssets(mirRepo, offset, limit, classIds, currentAssetId, cmTypes, cks, tags)
+	loadAndCacheAssets(mongo, mirLoader)
+	return mongo.QueryAssets(mirLoader.GetMirRepo(), offset, limit, classIds, currentAssetId, cmTypes, cks, tags)
 }
 
 func GetDatasetMetaCountsHandler(
-	mongo MongoServer,
-	mirRepo constants.MirRepo,
+	mirLoader loader.BaseMirRepoLoader,
 ) constants.QueryDatasetStatsResult {
 	defer tools.TimeTrack(time.Now())
-	mirContext := pbreader.LoadSingleMirData(mirRepo, constants.MirfileContext).(*protos.MirContext)
+	mirContext := mirLoader.LoadSingleMirData(constants.MirfileContext).(*protos.MirContext)
 	result := constants.NewQueryDatasetStatsResult()
 	result.TotalAssetsCount = int64(mirContext.ImagesCnt)
 
@@ -70,19 +71,19 @@ func GetDatasetMetaCountsHandler(
 
 func GetDatasetStatsHandler(
 	mongo MongoServer,
-	mirRepo constants.MirRepo,
+	mirLoader loader.BaseMirRepoLoader,
 	classIds []int,
 ) constants.QueryDatasetStatsResult {
-	loadAndCacheAssets(mongo, mirRepo)
-	return mongo.QueryDatasetStats(mirRepo, classIds)
+	loadAndCacheAssets(mongo, mirLoader)
+	return mongo.QueryDatasetStats(mirLoader.GetMirRepo(), classIds)
 }
 
 func GetDatasetDupHandler(
 	mongo MongoServer,
-	mirRepo0 constants.MirRepo,
-	mirRepo1 constants.MirRepo,
+	mirLoader0 loader.BaseMirRepoLoader,
+	mirLoader1 loader.BaseMirRepoLoader,
 ) (int, int64, int64) {
-	loadAndCacheAssets(mongo, mirRepo0)
-	loadAndCacheAssets(mongo, mirRepo1)
-	return mongo.QueryDatasetDup(mirRepo0, mirRepo1)
+	loadAndCacheAssets(mongo, mirLoader0)
+	loadAndCacheAssets(mongo, mirLoader1)
+	return mongo.QueryDatasetDup(mirLoader0.GetMirRepo(), mirLoader1.GetMirRepo())
 }
---
+++ ymir/backend/src/ymir_viewer/services/handler_test.go
@@ -0,0 +1,87 @@
+package services
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/IndustryEssentials/ymir-viewer/common/constants"
+	"github.com/IndustryEssentials/ymir-viewer/common/protos"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockMirRepoLoader struct {
+	mock.Mock
+}
+
+func (m *MockMirRepoLoader) GetMirRepo() constants.MirRepo {
+	return constants.MirRepo{}
+}
+
+func (m *MockMirRepoLoader) LoadSingleMirData(mirFile constants.MirFile) interface{} {
+
+	args := m.Called(mirFile)
+	return args.Get(0).(*protos.MirContext)
+}
+
+func (m *MockMirRepoLoader) LoadAssetsDetail() []constants.MirAssetDetail {
+
+	args := m.Called()
+	return args.Get(0).([]constants.MirAssetDetail)
+}
+
+func TestGetDatasetMetaCountsHandler(t *testing.T) {
+	mirFile := constants.MirfileContext
+	mockMirContext := protos.MirContext{}
+	json.Unmarshal([]byte(`{
+		"images_cnt": 20,
+		"pred_stats":
+		{
+			"positive_asset_cnt": 8,
+			"negative_asset_cnt": 5,
+			"class_ids_cnt":
+			{
+				"1": 8
+			}
+		},
+		"gt_stats":
+		{
+			"positive_asset_cnt": 3,
+			"negative_asset_cnt": 2,
+			"class_ids_cnt":
+			{
+				"0": 3
+			}
+		}
+	}`), &mockMirContext)
+
+	pbreaderMock := MockMirRepoLoader{}
+	pbreaderMock.On("LoadSingleMirData", mirFile).Return(&mockMirContext)
+
+	expectedResult := constants.QueryDatasetStatsResult{}
+	result := GetDatasetMetaCountsHandler(&pbreaderMock)
+	json.Unmarshal([]byte(`{
+		"gt":
+		{
+			"class_ids_count":
+			{
+				"0": 3
+			},
+			"negative_images_count": 2,
+			"positive_images_count": 3
+		},
+		"pred":
+		{
+			"class_ids_count":
+			{
+				"1": 8
+			},
+			"negative_images_count": 5,
+			"positive_images_count": 8
+		},
+		"total_assets_count": 20
+	}`), &expectedResult)
+	assert.Equal(t, result, expectedResult)
+	pbreaderMock.AssertExpectations(t)
+
+}
