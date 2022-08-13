9e97a322eb4c6888d33f2d2a21e4103b6385975c
Phoenix <89957974+phoenix-xhuang@users.noreply.github.com>
2022-08-12 23:52:24 -0700

    try ci

--- ymir/backend/src/ymir_viewer/common/loader/loader_test.go
+++ ymir/backend/src/ymir_viewer/common/loader/loader_test.go
@@ -45,7 +45,7 @@ func TestGetMirRepo(t *testing.T) {
 }
 
 func TestLoadModelInfo(t *testing.T) {
-	workDir := fmt.Sprintf("%s/modelinfo", t.TempDir())
+	workDir := fmt.Sprintf("%s/modelinfo", "/tmp")
 	mirRepo := createTestMirRepo(workDir)
 	mirRoot, mirRev := mirRepo.BuildRepoID()
 
@@ -76,7 +76,7 @@ func TestLoadModelInfo(t *testing.T) {
 }
 
 func TestLoadAssetsDetail(t *testing.T) {
-	workDir := fmt.Sprintf("%s/assets_detail", t.TempDir())
+	workDir := fmt.Sprintf("%s/assets_detail", "/tmp")
 	mirRepo := createTestMirRepo(workDir)
 	mirRoot, mirRev := mirRepo.BuildRepoID()
 
