50a2f145416cdafdb4d44f9a97fb419c3194ad51
Elliot <li.shanlue@intellif.com>
2022-08-18 11:39:05 +0800

    add ck and tag on asset detail

--- ymir/web/src/components/dataset/assetAnnotation.js
+++ ymir/web/src/components/dataset/assetAnnotation.js
@@ -1,6 +1,11 @@
-import styles from "./common.less"
 import { useEffect, useState, useRef } from "react"
-import { percent } from "../../utils/number"
+import { Col, Popover, Row, Space } from "antd"
+
+import { percent } from "@/utils/number"
+import t from '@/utils/t'
+import { evaluationLabel } from '@/constants/dataset'
+
+import styles from "./common.less"
 
 function AssetAnnotation({
   url,
@@ -31,7 +36,20 @@ function AssetAnnotation({
 
   const renderAnnotations = () => {
     return annotations.map((annotation, index) => {
-      return (
+      const evaluatedLabel = evaluationLabel(annotation.cm)
+      const emptyTags = Object.keys(annotation.tags).length === 0
+      const popContent = <>
+        <Row><Col flex={'100px'}>{t('keyword.column.name')}</Col><Col flex={1}>{annotation.keyword}</Col></Row>
+        {evaluatedLabel ? <Row><Col flex={'100px'}>Evaluation</Col><Col flex={1}>{evaluationLabel(annotation.cm)}</Col></Row> : null}
+        {annotation.confidence ? <Row><Col flex={'100px'}>{t('model.verify.confidence')}</Col><Col flex={1}>{annotation.confidence}</Col></Row> : null}
+        {!emptyTags ? <Row><Col flex={'100px'}>{t('dataset.assets.keyword.selector.types.tags')}</Col><Col flex={1}>
+          {Object.keys(annotation.tags).map(tag => <Space key={tag} style={{ width: '100%' }}>
+            <span style={{ fontWeight: 'bold' }}>{tag}: </span> <span>{annotation.tags[tag]}</span>
+          </Space>)}
+        </Col>
+        </Row> : null}
+      </>
+      return <Popover content={popContent} placement='right'>
         <div
           title={`${annotation.keyword}` + (annotation.score ? `\nConference:${annotation.score}` : '')}
           className={`${styles.annotation} ${annotation.gt ? styles.gt : ''}`}
@@ -49,7 +67,7 @@ function AssetAnnotation({
           <span className={styles.annotationTitle} style={{ backgroundColor: annotation.color }}>{annotation.keyword}
             {annotation.score ? <> {annotation.score}</> : null}</span>
         </div>
-      )
+      </Popover>
     })
   }
 
