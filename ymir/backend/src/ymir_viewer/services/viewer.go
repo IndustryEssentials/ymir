aaa78009663354e5ebf9e1fb550303f3e28ba61c
Phoenix <89957974+phoenix-xhuang@users.noreply.github.com>
2022-08-18 20:37:54 -0700

    [ymir] add dev metrics (#1068)
    add dev metrics

--- ymir/backend/src/ymir_viewer/services/viewer.go
+++ ymir/backend/src/ymir_viewer/services/viewer.go
@@ -69,6 +69,8 @@ func NewViewerServer(config constants.Config) ViewerServer {
 	// get global Monitor object
 	m := ginmetrics.GetMonitor()
 
+	// +optional set metric path, default /debug/metrics
+	m.SetMetricPath("/metrics")
 	// +optional set slow time, default 5s
 	m.SetSlowTime(10)
 	// +optional set request duration, default {0.1, 0.3, 1.2, 5, 10}
