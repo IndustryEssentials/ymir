f715e7f382537619ca27c85ffb7330fd212a3fdb
Elliot <li.shanlue@intellif.com>
2022-08-22 14:14:16 +0800

    openpai invisible on mining and inference

--- ymir/web/src/pages/task/inference/index.js
+++ ymir/web/src/pages/task/inference/index.js
@@ -5,6 +5,7 @@ import { useHistory, useParams, useLocation } from "umi"
 
 import { formLayout } from "@/config/antd"
 import t from "@/utils/t"
+import { HIDDENMODULES } from '@/constants/common'
 import { string2Array } from "@/utils/string"
 import { OPENPAI_MAX_GPU_COUNT } from '@/constants/common'
 import { TYPES } from '@/constants/image'
@@ -66,7 +67,9 @@ function Inference({ datasetCache, datasets, ...func }) {
 
   useEffect(() => {
     setGPU(sys.gpu_count || 0)
-    setOpenpai(!!sys.openpai_enabled)
+    if (!HIDDENMODULES.OPENPAI) {
+      setOpenpai(!!sys.openpai_enabled)
+    }
   }, [sys])
 
   useEffect(() => {
--- ymir/web/src/pages/task/mining/index.js
+++ ymir/web/src/pages/task/mining/index.js
@@ -6,6 +6,7 @@ import { useHistory, useParams, useLocation } from "umi"
 
 import { formLayout } from "@/config/antd"
 import t from "@/utils/t"
+import { HIDDENMODULES } from '@/constants/common'
 import { string2Array } from '@/utils/string'
 import { OPENPAI_MAX_GPU_COUNT } from '@/constants/common'
 import { TYPES } from '@/constants/image'
@@ -52,7 +53,9 @@ function Mining({ datasetCache, ...func }) {
 
   useEffect(() => {
     setGPU(sys.gpu_count || 0)
-    setOpenpai(!!sys.openpai_enabled)
+    if (!HIDDENMODULES.OPENPAI) {
+      setOpenpai(!!sys.openpai_enabled)
+    }
   }, [sys])
 
   useEffect(() => {
