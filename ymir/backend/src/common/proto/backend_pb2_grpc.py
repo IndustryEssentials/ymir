b268d6d532a647168ae178d88c2b90583bedafc9
Phoenix <89957974+phoenix-xhuang@users.noreply.github.com>
2022-08-25 16:59:05 -0700

    update proto

--- ymir/backend/src/common/proto/backend_pb2_grpc.py
+++ ymir/backend/src/common/proto/backend_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-from mir.protos import backend_pb2 as backend__pb2
+import backend_pb2 as backend__pb2
 
 
 class mir_controller_serviceStub(object):
--- ymir/backend/src/common/proto/build-proto.sh
+++ ymir/backend/src/common/proto/build-proto.sh
@@ -11,4 +11,4 @@ python -m grpc_tools.protoc \
        --python_out=$OUTPUT_DIR \
        "$INPUT_DIR/backend.proto"
 
-sed -i.bak -r 's/^import (.*_pb2.*)/from mir.protos import \1/g' $OUTPUT_DIR/*_pb2*.py && rm *.bak
+sed -i.bak -r 's/^import (.*_pb2.*)/from mir.protos import \1/g' $OUTPUT_DIR/*_pb2.py && rm *.bak
