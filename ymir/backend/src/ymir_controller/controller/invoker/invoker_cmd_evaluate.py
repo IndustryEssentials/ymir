cc3f626f91cba9b7d38dedb65b4adbd5e8816b37
ymir <ymir@intellif.com>
2022-08-26 16:20:26 +0800

    fix CI

--- ymir/backend/src/ymir_controller/controller/invoker/invoker_cmd_evaluate.py
+++ ymir/backend/src/ymir_controller/controller/invoker/invoker_cmd_evaluate.py
@@ -1,7 +1,7 @@
 from controller.invoker.invoker_cmd_base import BaseMirControllerInvoker
 from controller.utils import checker, utils
 from id_definition.error_codes import CTLResponseCode
-from mir.tools import det_eval, revs_parser
+from mir.tools import det_eval_ops, revs_parser
 from proto import backend_pb2
 
 
@@ -36,11 +36,11 @@ class EvaluateInvoker(BaseMirControllerInvoker):
         ec = self._request.evaluate_config
         rev_tid = revs_parser.parse_single_arg_rev(ec.pred_dataset_ids[0], need_tid=False)
 
-        evaluation, _ = det_eval.det_evaluate(mir_root=self._repo_root,
-                                              rev_tid=rev_tid,
-                                              conf_thr=ec.conf_thr,
-                                              iou_thrs=ec.iou_thrs_interval,
-                                              need_pr_curve=ec.need_pr_curve)
+        evaluation, _ = det_eval_ops.det_evaluate(mir_root=self._repo_root,
+                                                  rev_tid=rev_tid,
+                                                  conf_thr=ec.conf_thr,
+                                                  iou_thrs=ec.iou_thrs_interval,
+                                                  need_pr_curve=ec.need_pr_curve)
 
         response = backend_pb2.GeneralResp()
         response.code = CTLResponseCode.CTR_OK
