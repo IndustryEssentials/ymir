499969720a605cc1f12ad5047230f09f6b9f3695
ymir <ymir@intellif.com>
2022-08-31 15:56:28 +0800

    add invoker_cmd_evaluate and it's test case back

---
+++ ymir/backend/src/ymir_controller/controller/invoker/invoker_cmd_evaluate.py
@@ -0,0 +1,48 @@
+from controller.invoker.invoker_cmd_base import BaseMirControllerInvoker
+from controller.utils import checker, utils
+from id_definition.error_codes import CTLResponseCode
+from mir.tools import det_eval, revs_parser
+from proto import backend_pb2
+
+
+class EvaluateInvoker(BaseMirControllerInvoker):
+    def _need_work_dir(self) -> bool:
+        return False
+
+    def pre_invoke(self) -> backend_pb2.GeneralResp:
+        checker_resp = checker.check_invoker(invoker=self,
+                                             prerequisites=[
+                                                 checker.Prerequisites.CHECK_USER_ID,
+                                                 checker.Prerequisites.CHECK_REPO_ID,
+                                                 checker.Prerequisites.CHECK_REPO_ROOT_EXIST,
+                                                 checker.Prerequisites.CHECK_TASK_ID,
+                                                 checker.Prerequisites.CHECK_IN_DATASET_IDS,
+                                             ])
+        if checker_resp.code != CTLResponseCode.CTR_OK:
+            return checker_resp
+
+        ec = self._request.evaluate_config
+        if ec.conf_thr < 0 or ec.conf_thr >= 1:
+            return utils.make_general_response(CTLResponseCode.ARG_VALIDATION_FAILED,
+                                               f"invalid evaluate conf thr: {ec.conf_thr:.2f}")
+
+        if not ec.iou_thrs_interval:
+            return utils.make_general_response(CTLResponseCode.ARG_VALIDATION_FAILED,
+                                               "invalid evaluate iou thrs interval: {}".format(ec.iou_thrs_interval))
+
+        return utils.make_general_response(CTLResponseCode.CTR_OK, "")
+
+    def invoke(self) -> backend_pb2.GeneralResp:
+        ec = self._request.evaluate_config
+        rev_tid = revs_parser.parse_single_arg_rev(ec.pred_dataset_ids[0], need_tid=False)
+
+        evaluation, _ = det_eval.det_evaluate(mir_root=self._repo_root,
+                                              rev_tid=rev_tid,
+                                              conf_thr=ec.conf_thr,
+                                              iou_thrs=ec.iou_thrs_interval,
+                                              need_pr_curve=ec.need_pr_curve)
+
+        response = backend_pb2.GeneralResp()
+        response.code = CTLResponseCode.CTR_OK
+        response.evaluation.CopyFrom(evaluation)
+        return response
---
+++ ymir/backend/src/ymir_controller/tests/unit/test_invoker_cmd_evaluate.py
@@ -0,0 +1,84 @@
+import os
+import shutil
+import unittest
+from unittest import mock
+
+from controller.utils.invoker_call import make_invoker_cmd_call
+from controller.utils.invoker_mapping import RequestTypeToInvoker
+from proto import backend_pb2
+from mir.protos import mir_command_pb2 as mir_cmd_pb
+
+import tests.utils as test_utils
+
+
+class TestInvokerCmdEvaluate(unittest.TestCase):
+    # life cycle
+    def __init__(self, methodName: str) -> None:
+        # dir structure:
+        # test_involer_CLSNAME_sandbox_root
+        # ├── media_storage_root
+        # └── test_user
+        #     └── ymir-dvc-test
+        super().__init__(methodName=methodName)
+        self._user_name = "user"
+        self._mir_repo_name = "repoid"
+        self._storage_name = "media_storage_root"
+        self._in_dataset_ids = ['t000aaaabbbbbbzzzzzzzzzzzzzzz1']
+        self._gt_dataset_id = 't000aaaabbbbbbzzzzzzzzzzzzzzz2'
+        self._task_id = 't000aaaabbbbbbzzzzzzzzzzzzzzz4'
+        self._dst_dataset_id = 't000aaaabbbbbbzzzzzzzzzzzzzzz4'
+
+        self._conf_thr = 0.3
+        self._iou_thrs_interval = '0.5:1.0:0.05'
+
+        self._sandbox_root = test_utils.dir_test_root(self.id().split(".")[-3:])
+        self._user_root = os.path.join(self._sandbox_root, self._user_name)
+        self._mir_repo_root = os.path.join(self._user_root, self._mir_repo_name)
+        self._storage_root = os.path.join(self._sandbox_root, self._storage_name)
+
+    def setUp(self) -> None:
+        test_utils.check_commands()
+        self._prepare_dirs()
+        self._prepare_mir_repo()
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        if os.path.isdir(self._sandbox_root):
+            shutil.rmtree(self._sandbox_root)
+        return super().tearDown()
+
+    # protected: setup and teardown
+    def _prepare_dirs(self):
+        if os.path.isdir(self._sandbox_root):
+            shutil.rmtree(self._sandbox_root)
+        os.makedirs(self._sandbox_root)
+        os.mkdir(self._user_root)
+        os.mkdir(self._mir_repo_root)
+        os.mkdir(self._storage_root)
+
+    def _prepare_mir_repo(self):
+        # init repo
+        test_utils.mir_repo_init(self._mir_repo_root)
+
+    # protected: mocked
+    def _mock_run_func(*args, **kwargs):
+        return mir_cmd_pb.Evaluation(), None
+
+    # public: test cases
+    @mock.patch("mir.tools.det_eval.det_evaluate", side_effect=_mock_run_func)
+    def test_evaluate_00(self, mock_run):
+        evaluate_config = mir_cmd_pb.EvaluateConfig()
+        evaluate_config.pred_dataset_ids[:] = ["abc"]
+        evaluate_config.conf_thr = self._conf_thr
+        evaluate_config.iou_thrs_interval = self._iou_thrs_interval
+
+        response = make_invoker_cmd_call(invoker=RequestTypeToInvoker[backend_pb2.CMD_EVALUATE],
+                                         sandbox_root=self._sandbox_root,
+                                         req_type=backend_pb2.CMD_EVALUATE,
+                                         user_id=self._user_name,
+                                         repo_id=self._mir_repo_name,
+                                         task_id=self._task_id,
+                                         in_dataset_ids=self._in_dataset_ids,
+                                         singleton_op=self._gt_dataset_id,
+                                         evaluate_config=evaluate_config)
+        self.assertEqual(response.code, 0)
