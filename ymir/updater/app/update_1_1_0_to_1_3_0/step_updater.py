3b9f985d0a3b3a4cde27e89d7302705882b9e42b
ymir <ymir@intellif.com>
2022-09-11 22:28:53 +0800

    update

--- ymir/updater/app/update_1_1_0_to_1_3_0/step_updater.py
+++ ymir/updater/app/update_1_1_0_to_1_3_0/step_updater.py
@@ -3,6 +3,9 @@ import logging
 
 from common_utils.sandbox import SandboxInfo
 
+from ymir_1_1_0.mir.protos import mir_command_pb2 as mirpb110
+from ymir_1_3_0.mir.protos import mir_command_pb2 as mirpb130
+
 
 def update_all(sandbox_info: SandboxInfo) -> None:
     logging.info('1.1.0 -> 1.3.0')
