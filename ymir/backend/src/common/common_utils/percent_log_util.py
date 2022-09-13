34e6aa41f72c1117dd71087b35a8c4f1be1e01a0
ymir <ymir@intellif.com>
2022-09-13 15:45:20 +0800

    update

--- ymir/backend/src/common/common_utils/percent_log_util.py
+++ ymir/backend/src/common/common_utils/percent_log_util.py
@@ -32,11 +32,8 @@ class PercentLogHandlerError(Exception):
 class PercentLogHandler:
     @staticmethod
     def parse_percent_log(log_file: str) -> PercentResult:
-        try:
-            with open(log_file, "r") as f:
-                monitor_file_lines = f.readlines()
-        except FileNotFoundError as e:
-            raise PercentLogHandlerError(f"can not open log file: {log_file}") from e
+        with open(log_file, "r") as f:
+            monitor_file_lines = f.readlines()
         content_row_one = monitor_file_lines[0].strip().split("\t")
         if not monitor_file_lines or len(content_row_one) < 4:
             raise PercentLogHandlerError(f"invalid percent log file: {log_file}")
