55efae44705e02f0f30559d6b284bc958b0a9b2c
ymir <ymir@intellif.com>
2022-09-15 15:34:22 +0800

    exodus: update

--- ymir/command/mir/tools/exodus.py
+++ ymir/command/mir/tools/exodus.py
@@ -5,8 +5,10 @@ some mir commands, such as `mir search`, `mir merge` will use this module
 
 import io
 import os
+import subprocess
 
 from mir import scm
+from mir.scm.cmd import stream_copy
 from mir.tools.code import MirCode
 from mir.tools.errors import MirRuntimeError
 
@@ -20,28 +22,19 @@ def read_mir(mir_root: str, rev: str, file_name: str) -> bytes:
     if not blob_hash:
         raise MirRuntimeError(MirCode.RC_CMD_INVALID_MIR_REPO, f"found no file: {rev}:{file_name}")
 
-    bio = io.BytesIO()
-    scm_git.cat_file(['-p', blob_hash], output_stream=bio)
-    if not os.path.isfile(os.path.join(mir_root, '.gitattributes')) or not bio.getvalue():
+    if os.path.isfile(os.path.join(mir_root, '.gitattributes')):
+        # git lfs: git cat-file -p blob_hash | git lfs smudge
+        catfile_ps = subprocess.Popen(['git', 'cat-file', '-p', blob_hash], stdout=subprocess.PIPE, cwd=mir_root)
+        smudge_ps = subprocess.Popen(['git', 'lfs', 'smudge'],
+                                     stdin=catfile_ps.stdout,
+                                     stdout=subprocess.PIPE,
+                                     cwd=mir_root)
+
+        bio = io.BytesIO()
+        stream_copy(source=smudge_ps.stdout, destination=bio, chunk_size=io.DEFAULT_BUFFER_SIZE)
         return bio.getvalue()
-
-    cat_file_result_lines = bio.getvalue().decode('utf-8').splitlines()
-    if len(cat_file_result_lines) == 3 and cat_file_result_lines[1].startswith('oid sha256:'):
-        lfs_file_hash = cat_file_result_lines[1][11:]
-        lfs_file_path = os.path.join(
-            mir_root, '.git', 'lfs', 'objects', lfs_file_hash[:2], lfs_file_hash[2:4], lfs_file_hash)
-        with open(lfs_file_path, 'rb') as f:
-            return f.read()
-    raise MirRuntimeError(MirCode.RC_CMD_INVALID_MIR_REPO, f"found no lfs file: {rev}:{file_name}")
-
-    # bio2 = io.BytesIO()
-    # # subprocess.run(['git', 'cat-file', '-p', blob_hash, '|', 'git', 'lfs', 'smudge'], stdout=bio2)
-    # catfile_ps = subprocess.Popen(['git', 'cat-file', '-p', blob_hash], stdout=subprocess.PIPE, cwd=mir_root)
-    # smudge_ps = subprocess.Popen(['git', 'lfs', 'smudge'], stdin=catfile_ps.stdout, stdout=bio2, cwd=mir_root)
-    # catfile_ps.wait()
-    # return bio2.getvalue()
-    # bio = io.BytesIO()
-    # bio2 = io.BytesIO()
-    # scm_git.cat_file(['-p', blob_hash], output_stream=bio)
-    # scm_git.lfs(['smudge'], istream=bio.getvalue(), output_stream=bio2)
-    # return bio2.getvalue()
+    else:
+        # git without lfs: git cat-file -p blob_hash
+        bio = io.BytesIO()
+        scm_git.cat_file(['-p', blob_hash], output_stream=bio)
+        return bio.getvalue()
