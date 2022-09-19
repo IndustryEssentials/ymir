2e56cc6fe8d079c2aea2058adad87307a591954d
ymir <ymir@intellif.com>
2022-09-19 15:43:50 +0800

    update

--- ymir/backend/src/common/common_utils/sandbox.py
+++ ymir/backend/src/common/common_utils/sandbox.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import os
 import re
+import shutil
 from typing import List, Dict, Set
 
 import yaml
@@ -20,7 +21,75 @@ class SandboxError(Exception):
         return f"code: {self.error_code}, content: {self.error_message}"
 
 
-def detect_users_and_repos(sandbox_root: str) -> Dict[str, Set[str]]:
+def detect_sandbox_src_ver(sandbox_root: str) -> str:
+    """
+    detect user space versions in this sandbox
+
+    Args:
+        sandbox_root (str): root of this sandbox
+
+    Returns:
+        str: sandbox version
+
+    Raises:
+        SandboxError if labels.yaml not found, or can not be parsed as yaml;
+        found no user space version or multiple user space versions.
+    """
+    user_to_repos = _detect_users_and_repos(sandbox_root)
+    ver_to_users: Dict[str, List[str]] = defaultdict(list)
+    for user_id in user_to_repos:
+        user_label_file = os.path.join(sandbox_root, user_id, 'labels.yaml')
+        try:
+            with open(user_label_file, 'r') as f:
+                user_label_dict = yaml.safe_load(f)
+        except (FileNotFoundError, yaml.YAMLError) as e:
+            raise SandboxError(error_code=UpdaterErrorCode.INVALID_USER_LABEL_FILE,
+                               error_message=f"invalid label file: {user_label_file}") from e
+
+        ver_to_users[user_label_dict.get('ymir_version', version.DEFAULT_YMIR_SRC_VERSION)].append(user_id)
+
+    if len(ver_to_users) != 1:
+        raise SandboxError(error_code=UpdaterErrorCode.INVALID_USER_SPACE_VERSIONS,
+                           error_message=f"invalid user space versions: {ver_to_users}")
+
+    return list(ver_to_users.keys())[0]
+
+
+def backup(sandbox_root: str) -> str:
+    backup_dir = os.path.join(sandbox_root, 'backup')
+    os.makedirs(backup_dir, exist_ok=True)
+    if os.listdir(backup_dir):
+        raise SandboxError(error_code=UpdaterErrorCode.BACKUP_DIR_NOT_EMPTY,
+                           error_message=f"Backup directory not empty: {backup_dir}")
+
+    user_to_repos = _detect_users_and_repos(sandbox_root)
+    for user_id, repo_ids in user_to_repos.items():
+        src_user_dir = os.path.join(sandbox_root, user_id)
+        dst_user_dir = os.path.join(backup_dir, user_id)
+        _copy_user_space(src_user_dir=src_user_dir, dst_user_dir=dst_user_dir, repo_ids=repo_ids)
+
+    return backup_dir
+
+
+def roll_back(sandbox_root: str) -> None:
+    user_to_repos = _detect_users_and_repos(sandbox_root)
+    for user_id, repo_ids in user_to_repos.items():
+        src_user_dir = os.path.join(os.path.join(sandbox_root, 'backup'), user_id)
+        dst_user_dir = os.path.join(sandbox_root, user_id)
+        shutil.rmtree(dst_user_dir)
+        _copy_user_space(src_user_dir=src_user_dir, dst_user_dir=dst_user_dir, repo_ids=repo_ids)
+
+
+def remove_backup(sandbox_root: str) -> None:
+    shutil.rmtree(os.path.join(sandbox_root, 'backup'))
+
+
+def get_all_repo_rel_paths(sandbox_root: str) -> Set[str]:
+    user_to_repos = _detect_users_and_repos(sandbox_root)
+    return {os.path.join(user_id, repo_id) for user_id, repo_ids in user_to_repos.items() for repo_id in repo_ids}
+
+
+def _detect_users_and_repos(sandbox_root: str) -> Dict[str, Set[str]]:
     """
     detect user and repo directories in this sandbox
 
@@ -43,35 +112,8 @@ def detect_users_and_repos(sandbox_root: str) -> Dict[str, Set[str]]:
     return user_to_repos
 
 
-def detect_sandbox_src_ver(sandbox_root: str) -> str:
-    """
-    detect user space versions in this sandbox
-
-    Args:
-        sandbox_root (str): root of this sandbox
-
-    Returns:
-        str: sandbox version
-
-    Raises:
-        SandboxError if labels.yaml not found, or can not be parsed as yaml;
-        found no user space version or multiple user space versions.
-    """
-    user_to_repos = detect_users_and_repos(sandbox_root)
-    ver_to_users: Dict[str, List[str]] = defaultdict(list)
-    for user_id in user_to_repos:
-        user_label_file = os.path.join(sandbox_root, user_id, 'labels.yaml')
-        try:
-            with open(user_label_file, 'r') as f:
-                user_label_dict = yaml.safe_load(f)
-        except (FileNotFoundError, yaml.YAMLError) as e:
-            raise SandboxError(error_code=UpdaterErrorCode.INVALID_USER_LABEL_FILE,
-                               error_message=f"invalid label file: {user_label_file}") from e
-
-        ver_to_users[user_label_dict.get('ymir_version', version.DEFAULT_YMIR_SRC_VERSION)].append(user_id)
-
-    if len(ver_to_users) != 1:
-        raise SandboxError(error_code=UpdaterErrorCode.INVALID_USER_SPACE_VERSIONS,
-                           error_message=f"invalid user space versions: {ver_to_users}")
-
-    return list(ver_to_users.keys())[0]
+def _copy_user_space(src_user_dir: str, dst_user_dir: str, repo_ids: Set[str]) -> None:
+    os.makedirs(dst_user_dir, exist_ok=True)
+    shutil.copy(src=os.path.join(src_user_dir, 'labels.yaml'), dst=os.path.join(dst_user_dir, 'labels.yaml'))
+    for repo_id in repo_ids:
+        shutil.copytree(src=os.path.join(src_user_dir, repo_id), dst=os.path.join(dst_user_dir, repo_id))
--- ymir/updater/app/start.py
+++ ymir/updater/app/start.py
@@ -26,37 +26,6 @@ def _exc_update_steps(update_steps: Tuple[ModuleType, ...], sandbox_root: str) -
         step_func(sandbox_root)
 
 
-def _backup(sandbox_root: str) -> str:
-    backup_dir = os.path.join(sandbox_root, 'backup')
-    os.makedirs(backup_dir, exist_ok=True)
-    if os.listdir(backup_dir):
-        raise update_errors.BackupDirNotEmpty()
-
-    user_to_repos = sandbox.detect_users_and_repos(sandbox_root)
-    for user_id, repo_ids in user_to_repos.items():
-        src_user_dir = os.path.join(sandbox_root, user_id)
-        dst_user_dir = os.path.join(backup_dir, user_id)
-        _copy_user_space(src_user_dir=src_user_dir, dst_user_dir=dst_user_dir, repo_ids=repo_ids)
-
-    return backup_dir
-
-
-def _roll_back(backup_dir: str, sandbox_root: str) -> None:
-    user_to_repos = sandbox.detect_users_and_repos(sandbox_root)
-    for user_id, repo_ids in user_to_repos.items():
-        src_user_dir = os.path.join(backup_dir, user_id)
-        dst_user_dir = os.path.join(sandbox_root, user_id)
-        shutil.rmtree(dst_user_dir)
-        _copy_user_space(src_user_dir=src_user_dir, dst_user_dir=dst_user_dir, repo_ids=repo_ids)
-
-
-def _copy_user_space(src_user_dir: str, dst_user_dir: str, repo_ids: Set[str]) -> None:
-    os.makedirs(dst_user_dir, exist_ok=True)
-    shutil.copy(src=os.path.join(src_user_dir, 'labels.yaml'), dst=os.path.join(dst_user_dir, 'labels.yaml'))
-    for repo_id in repo_ids:
-        shutil.copytree(src=os.path.join(src_user_dir, repo_id), dst=os.path.join(dst_user_dir, repo_id))
-
-
 def main() -> int:
     if os.environ['EXPECTED_YMIR_VERSION'] != YMIR_VERSION:
         raise update_errors.EnvVersionNotMatch()
@@ -67,15 +36,15 @@ def main() -> int:
     if not update_steps:
         raise update_errors.SandboxVersionNotSupported(sandbox_version=src_ver)
 
-    backup_dir = _backup(sandbox_root)
+    sandbox.backup(sandbox_root)
     try:
         _exc_update_steps(update_steps=update_steps, sandbox_root=sandbox_root)
     except Exception as e:
-        _roll_back(backup_dir=backup_dir, sandbox_root=sandbox_root)
+        sandbox.roll_back(sandbox_root)
         raise e
 
     # cleanup
-    shutil.rmtree(backup_dir)
+    sandbox.remove_backup(sandbox_root)
 
     return 0
 
