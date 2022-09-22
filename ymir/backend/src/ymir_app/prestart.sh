6990d3c1e1f198467900e73e408f92e88a2c0153
IJtLJZ8Rm4Yr <90443055+IJtLJZ8Rm4Yr@users.noreply.github.com>
2022-09-22 10:46:23 +0800

    [misc] models and datasets data migration (#1167)
    * [misc] app models and datasets data migration
    
    * add empty pred key
    
    * update comment
    
    * remove comment
    
    * skip migration by check alembic version when possible
    
    * fix tests
    
    * update env
    
    * use default model_stage when possible
    
    Co-authored-by: Fenrir <90444968+fenrir-z@users.noreply.github.com>

--- ymir/backend/src/ymir_app/prestart.sh
+++ ymir/backend/src/ymir_app/prestart.sh
@@ -3,11 +3,14 @@
 # Let the DB start
 python app/backend_pre_start.py
 
+# Get current alembic version if possible
+base_alembic_revision=$(alembic current)
+
 # Run migrations for MySQL
 alembic upgrade head
 
 # Create initial data in DB
-python app/initial_data.py
+python app/initial_data.py ${base_alembic_revision}
 
 # Clean legacy tasks
 python app/clean_tasks.py
