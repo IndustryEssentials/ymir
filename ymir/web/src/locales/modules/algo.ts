b27465da669e11727af6da5d2adceed29dc87e2b
Elliot <li.shanlue@intellif.com>
2022-09-24 19:28:57 +0800

    update en label

--- ymir/web/src/locales/modules/algo.ts
+++ ymir/web/src/locales/modules/algo.ts
@@ -1,9 +1,9 @@
 const algo = {
-  'algo.label': { cn: '模型部署', en: 'Logout', },
-  'algo.public.label': { cn: '公有算法', en: 'Logout', },
-  'algo.mine.label': { cn: '我的算法', en: 'Logout', },
-  'algo.device.label': { cn: '设备列表', en: 'Logout', },
-  'algo.support.label': { cn: '支持设备', en: 'Logout', },
+  'algo.label': { cn: '模型部署', en: 'Model Deployment', },
+  'algo.public.label': { cn: '公有算法', en: 'Public Algorithm', },
+  'algo.mine.label': { cn: '我的算法', en: 'My Algorithm', },
+  'algo.device.label': { cn: '设备列表', en: 'Devices', },
+  'algo.support.label': { cn: '支持设备', en: 'Supported Devices', },
 }
 
 export default algo
