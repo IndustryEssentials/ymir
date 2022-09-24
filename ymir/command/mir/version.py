bbc6f75ab68f43779c54386886da26fffbf816e0
ymir <ymir@intellif.com>
2022-09-24 11:47:24 +0800

    fix mypy & flake8 errors

--- ymir/command/mir/version.py
+++ ymir/command/mir/version.py
@@ -7,6 +7,7 @@ DEFAULT_YMIR_SRC_VERSION = '1.1.0'
 # Protocol version for training, mining and infer executors
 TMI_PROTOCOL_VERSION = '1.0.0'
 
+
 def ymir_salient_version(ver: str) -> str:
     _SALIENT_VERSIONS = {
         DEFAULT_YMIR_SRC_VERSION: DEFAULT_YMIR_SRC_VERSION,
