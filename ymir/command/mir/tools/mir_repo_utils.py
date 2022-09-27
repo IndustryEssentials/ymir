d0c73951587dd1ca0ed65017ce0ee9bb934c0cc1
ymir <ymir@intellif.com>
2022-09-27 22:17:14 +0800

    revert changes to mir storage ops in ymir-cmd

--- ymir/command/mir/tools/mir_repo_utils.py
+++ ymir/command/mir/tools/mir_repo_utils.py
@@ -4,7 +4,9 @@ from typing import List, Optional
 
 from mir import scm
 
-from mir.tools import mir_storage
+
+_MIR_FILES = ('metadatas.mir', 'annotations.mir', 'keywords.mir', 'tasks.mir', 'context.mir')
+_MIR_ASSOCIATED_FILES = ['.git', '.gitattributes', '.gitignore', '.mir', '.mir_lock']
 
 
 def find_extra_items(mir_root: str) -> List[str]:
@@ -12,7 +14,7 @@ def find_extra_items(mir_root: str) -> List[str]:
     find all extra items not in mir_settings.MIR_FILES_LIST
     """
     items = os.listdir(path=mir_root)
-    return list(set(items) - set(mir_storage.get_all_mir_paths()) - set(mir_storage.MIR_ASSOCIATED_FILES))
+    return list(set(items) - set(_MIR_FILES) - set(_MIR_ASSOCIATED_FILES))
 
 
 def mir_check_repo_git_dirty(mir_root: str = ".") -> bool:
