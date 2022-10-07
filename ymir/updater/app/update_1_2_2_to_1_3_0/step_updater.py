ca0093dd133782fd5447ed1590a52fd9798a44f5
ymir <ymir@intellif.com>
2022-10-07 20:19:18 +0800

    WIP

--- ymir/updater/app/update_1_2_2_to_1_3_0/step_updater.py
+++ ymir/updater/app/update_1_2_2_to_1_3_0/step_updater.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import re
+from typing import Tuple
 
 from id_definition.task_id import IDProto
 from mir.tools import revs_parser
@@ -9,7 +10,11 @@ from mir.tools import mir_storage_ops_122 as mso_src, mir_storage_ops_130 as mso
 
 from tools import get_repo_tags, remove_old_tag, get_model_hashes
 
+_MirDatasSrc = Tuple[pb_src.MirMetadatas, pb_src.MirAnnotations, pb_src.Task]
+_MirDatasDst = Tuple[pb_dst.MirMetadatas, pb_dst.MirAnnotations, pb_dst.Task]
 
+
+# update user repo
 def update_repo(mir_root: str, assets_root: str, models_root: str) -> None:
     logging.info(f"updating repo: {mir_root}, 122 -> 130")
 
@@ -24,7 +29,73 @@ def update_repo(mir_root: str, assets_root: str, models_root: str) -> None:
 
         logging.info(f"    updating: {tag}")
         rev_tid = revs_parser.parse_single_arg_rev(src_rev=tag, need_tid=True)
+        datas_src = _load(mir_root, rev_tid)
+        datas_dst = _update(datas_src, assets_root, models_root)
+        _save(mir_root, rev_tid, datas_dst)
+
+
+def _load(mir_root: str, rev_tid: revs_parser.TypRevTid) -> _MirDatasSrc:
+    m, a, t = mso_src.MirStorageOps.load_multiple_storages(
+        mir_root=mir_root,
+        mir_branch=rev_tid.rev,
+        mir_task_id=rev_tid.tid,
+        ms_list=[pb_src.MIR_METADATAS, pb_src.MIR_ANNOTATIONS, pb_src.MIR_TASKS])
+    return (m, a, t.tasks[t.head_task_id])
+
+
+def _update(datas_src: _MirDatasSrc, assets_root: str, models_root: str) -> _MirDatasDst:
+    mir_metadatas_src, mir_annotations_src, task_src = datas_src
+    return (_update_metadatas(mir_metadatas_src, assets_root), _update_annotations(mir_annotations_src),
+            _update_task(task_src, models_root))
+
+
+def _save(mir_root: str, rev_tid: revs_parser.TypRevTid, datas_dst: _MirDatasDst) -> None:
+    pass
+
+
+def _update_metadatas(mir_metadatas_src: pb_src.MirMetadatas, assets_root: str) -> pb_dst.MirMetadatas:
+    pass
+
+
+def _update_annotations(mir_annotations_src: pb_src.MirAnnotations) -> pb_dst.MirAnnotations:
+    pass
+
+
+def _update_task(task_src: pb_src.Task, models_root: str) -> pb_dst.Task:
+    pass
 
 
 def update_models(models_root: str) -> None:
     logging.info(f"updating models: {models_root}, 122 -> 130")
+
+
+# MetadataAttributes:
+# 	remove: dataset_name
+# 	add: origin_filename = empty
+
+# MirAnnotations:
+# 	remove: head_task_id
+
+# SingleTaskAnnotations:
+# 	add: type = AnnoType.AT_DET_BOX
+# 	add: task_class_ids = empty
+# 	add: map_id_color = empty
+# 	add: eval_class_ids = empty
+# 	add: model = empty
+# 	add: executor_config = empty
+
+# SingleImageAnnotations:
+# 	move: annotations -> boxes
+# 	add: polygons = empty
+# 	add: mask = empty
+# 	add: img_class_ids = empty
+	
+# Annotation:
+# 	add: class_name = empty
+# 	add: polygon = empty
+
+# ModelMeta:
+# 	add: class_names = (from serialized executor config)
+	
+# Evaluation: no need to update
+# EvaluateConfig: no need to update
