58e1f6734d08946d007ee766bbce8ae1d978741b
jingyiliu <liu.jingyi@intellif.com>
2022-10-12 15:25:42 +0800

    [backend] remove fiftyone

--- ymir/backend/src/ymir_app/app/models/task_visual_relationship.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer
-
-from app.db.base_class import Base
-
-
-class TaskVisualRelationship(Base):
-    __tablename__ = "task_visual_relationship"
-    task_id = Column(Integer, primary_key=True, nullable=False)
-    visualization_id = Column(Integer, primary_key=True, nullable=False)
