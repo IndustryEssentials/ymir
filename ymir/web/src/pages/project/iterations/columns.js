aca9aab3658e4a4b2250e9e9aced7a1731853bf7
Elliot <li.shanlue@intellif.com>
2022-10-14 20:52:28 +0800

    update gt/pred label

--- ymir/web/src/pages/project/iterations/columns.js
+++ ymir/web/src/pages/project/iterations/columns.js
@@ -47,14 +47,14 @@ function showTitle(str) {
     dataIndex: "keywords",
     render: (_, record) => {
       const { gt, pred, } = record
-      const renderLine = (keywords = [], label = t('annotation.gt')) => <div>
-        <div>{label}:</div>
+      const renderLine = (keywords = [], label = 'gt') => <div>
+        <div>{t(`annotation.${label}`)}:</div>
         {t('dataset.column.keyword.label', {
           keywords: keywords.join(', '),
           total: keywords.length
         })}
       </div>
-      const label = <>{renderLine(gt?.keywords)}{renderLine(pred?.keywords, 'prediction')}</>
+      const label = <>{renderLine(gt?.keywords)}{renderLine(pred?.keywords, 'pred')}</>
       return validDataset(record) ? <Tooltip title={label}
         color='white' overlayInnerStyle={{ color: 'rgba(0,0,0,0.45)', fontSize: 12 }}
         mouseEnterDelay={0.5}
