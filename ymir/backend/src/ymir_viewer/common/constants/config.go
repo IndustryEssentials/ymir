f537eee171983ce8d656e294e6b4bf48d251862c
Your Name <you@example.com>
2022-10-18 11:15:12 +0800

    typo

--- ymir/backend/src/ymir_viewer/common/constants/config.go
+++ ymir/backend/src/ymir_viewer/common/constants/config.go
@@ -54,7 +54,7 @@ const (
 
 var ConstAssetsMirHist map[string]MirHist = map[string]MirHist{
 	"quality": {Ops: "$quality", LowerBNDs: []float64{0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0}},
-	"bytes": {Ops: bson.M{"$devide": bson.A{"$metadata.byte_size", BytesPerMB}},
+	"bytes": {Ops: bson.M{"$divide": bson.A{"$metadata.byte_size", BytesPerMB}},
 		LowerBNDs: []float64{0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0}},
 	"area": {Ops: bson.M{"$multiply": bson.A{"$metadata.width", "$metadata.height"}},
 		LowerBNDs: []float64{0, 100000, 500000, 1000000, 2000000, 4000000, 6000000, 8000000}},
