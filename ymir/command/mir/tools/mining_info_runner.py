85144cfeadc874a1c5b2991b41026b000f932dcd
ymir <ymir@intellif.com>
2022-10-20 16:15:02 +0800

    WIP

---
+++ ymir/command/mir/tools/mining_info_runner.py
@@ -0,0 +1,15 @@
+import time
+
+
+def run_mining_or_infer(
+    work_dir: str,
+    mir_root: str,
+    config_file: str,
+    # executor: str,
+    # executant_name: str,
+    run_as_root: bool,
+    # task_id: str = f"task-{time.time()}",
+    # run_infer: bool = False,
+    # run_mining: bool = False,
+) -> int:
+    return 0
