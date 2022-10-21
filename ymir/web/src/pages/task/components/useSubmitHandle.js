ebde7bd0a8e8b3a14325d71e89822054c2f1fb87
Elliot <li.shanlue@intellif.com>
2022-10-21 21:07:04 +0800

    clear cache after task

--- ymir/web/src/pages/task/components/useSubmitHandle.js
+++ ymir/web/src/pages/task/components/useSubmitHandle.js
@@ -1,13 +1,20 @@
 import { useHistory, useParams } from 'umi'
+import useFetch from '@/hooks/useFetch'
 
 function useSubmitHandle(type = 'dataset') {
   const history = useHistory()
   const { id: pid } = useParams()
+  const [_d, clearDatasetCache] = useFetch('dataset/clearCache')
+  const [_m, clearModelCache] = useFetch('model/clearCache')
 
   const handle = (result = {}) => {
-    const group =(result[`result_${type}`] || {})[`${type}_group_id`]
+    console.log('result:', result, type)
+    const group =(result[`result_${type}`] || result || {})[`${type}_group_id`] || result.id
     let redirect = `/home/project/${pid}/${type}#${group || ''}`
+    console.log('redirect:', redirect)
     history.replace(redirect)
+    clearModelCache()
+    clearDatasetCache()
   }
   return handle
 }
