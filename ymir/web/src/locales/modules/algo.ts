84e02ec9f1a72bae394208617abe028c5b0ea0a8
elliotmessi <90443217+elliotmessi@users.noreply.github.com>
2022-11-01 17:01:19 +0800

    [web] 130 algo update label (#1338)

--- ymir/web/src/locales/modules/algo.ts
+++ ymir/web/src/locales/modules/algo.ts
@@ -1,7 +1,7 @@
 const algo = {
   'algo.title': { cn: '算法商城', en: 'Algorithm Store', },
   'algo.label': { cn: '模型部署', en: 'Model Deployment', },
-  'algo.public.label': { cn: '公有算法', en: 'Public Algorithm', },
+  'algo.public.label': { cn: '公共算法', en: 'Public Algorithm', },
   'algo.mine.label': { cn: '我的算法', en: 'My Algorithm', },
   'algo.device.label': { cn: '设备列表', en: 'Devices', },
   'algo.support.label': { cn: '支持设备', en: 'Supported Devices', },
