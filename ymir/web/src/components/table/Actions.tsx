e7fbfe43970463c9450614b5b3e454f838b9a09a
Elliot <li.shanlue@intellif.com>
2022-11-01 10:39:35 +0800

    update ts

--- ymir/web/src/components/table/Actions.tsx
+++ ymir/web/src/components/table/Actions.tsx
@@ -1,42 +1,60 @@
 import { Button, Dropdown, Menu, Space } from "antd"
 import s from './table.less'
 import { More1Icon } from "../common/Icons"
+import React from "react"
 
-const actions = (menus) => menus.map((menu, i) => action(menu, i === menus.length - 1))
+type Action = {
+  key: string, 
+  label: string, 
+  onclick?: Function,
+  icon?: string, 
+  link?: string, 
+  target?: string,
+  disabled?: boolean,
+  hidden?: Function,
+}
 
-const isOuterLink = (link) => /^http(s)?:/i.test(link)
+type Props = {
+  actions: Action[],
+  showCount?: number,
+}
 
-const moreActions = (menus) => <Menu
+const renderActions = (menus: Action[]) => menus.map((menu, i) => renderAction(menu, i === menus.length - 1))
+
+const isOuterLink = (link: string) => /^http(s)?:/i.test(link)
+
+const moreActions = (ations: Action[]) => <Menu
   className={s.more}
   style={{ color: 'rgba(0, 0, 0, 0.65)' }}
-  items={menus.map(menu => ({
-    key: menu.key,
-    label: action(menu)
+  items={ations.map(action => ({
+    key: action.key,
+    label: renderAction(action)
   }))} />
 
-function action({ key, onclick = () => { }, icon, label, link, target, disabled }, last) {
+function renderAction(action: Action, last: boolean = false) {
+  const { key, onclick = () => { }, icon, label, link, target, disabled } = action
   const cls = `${s.action} ${last ? s.last : ''}`
   const btn = (
-    <Button key={key} type='link' disabled={disabled} className={cls} onClick={onclick}>
+    <Button key={key} type='link' disabled={disabled} className={cls} onClick={() => onclick()}>
       {icon}{label}
     </Button>
   )
   return link ? <a
     key={key}
     className={`${cls} ant-btn ant-btn-link`}
-    target={target ? target : (isOuterLink(link) && '_blank')}
+    target={target ? target : (isOuterLink(link) ? '_blank' : '')}
     href={link}
   >
     {icon} {label}
   </a> : btn
 }
 
-const Actions = ({ menus, showCount = 3 }) => {
-  const showActions = menus.filter(menu => !(menu.hidden && menu.hidden()))
+const Actions: React.FC<Props> = ({ actions = [], showCount = 3 }) => {
+  const showActions = actions.filter(action => !(action.hidden && action.hidden()))
   const isMore = () => showActions.length > showCount
   return (
     <Space className={s.actions} size={4}>
-      {actions(showActions.filter((item, i) => i < showCount))}
+      {renderActions(showActions.filter((item, i) => i < showCount))}
       {isMore() ? (
         <Dropdown overlay={moreActions(showActions.filter((item, i) => i >= showCount))}>
           <More1Icon />
