d375dde843cab7d5492200dee45b6d71db31936f
Elliot <li.shanlue@intellif.com>
2022-11-03 12:04:59 +0800

    update top username styles

--- ymir/web/src/pages/project/diagnose/InferDataset.tsx
+++ ymir/web/src/pages/project/diagnose/InferDataset.tsx
@@ -3,12 +3,13 @@ import { Card, Table, TableColumnsType } from "antd"
 import { useHistory, useParams, useSelector } from "umi"
 
 import useFetch from "@/hooks/useFetch"
-import t from '@/utils/t'
+import t from "@/utils/t"
 import { getInferDatasetColumns } from "@/components/table/Columns"
 import Actions from "@/components/table/Actions"
 
-import s from './index.less'
-import {EyeOnIcon, DiagnosisIcon } from '@/components/common/Icons'
+// test lint-staged
+import s from "./index.less"
+import { EyeOnIcon, DiagnosisIcon } from "@/components/common/Icons"
 
 const initQuery = { current: 1, offset: 0, limit: 20 }
 
@@ -17,29 +18,42 @@ const InferDataset: React.FC = () => {
   const history = useHistory()
   const [datasets, setDatasets] = useState<YModels.InferDataset[]>([])
   const [query, setQuery] = useState(initQuery)
-  const [{ items, total }, getDatasets] = useFetch('dataset/queryInferDatasets', { items: [], total: 0 })
+  const [{ items, total }, getDatasets] = useFetch(
+    "dataset/queryInferDatasets",
+    { items: [], total: 0 }
+  )
   const cols = getInferDatasetColumns()
-  const cacheDatasets = useSelector((state: YStates.DatasetState) => state.dataset.dataset)
-  const cacheModels = useSelector((state: YStates.ModelState) => state.model.model)
-const actions = (record: YModels.InferDataset): YComponents.Action[] => [
-  {
-    key: "diagnose",
-    label: t("common.action.diagnose"),
-    onclick: () => history.push(`/home/project/${pid}/diagnose#metrics`, { mid: record.inferModelId}),
-    icon: <DiagnosisIcon />,
-  },
-  {
-    key: "preview",
-    label: t("common.action.preview"),
-    onclick: () => history.push(`/home/project/${pid}/dataset/${record.id}/assets`),
-    icon: <EyeOnIcon />,
-  },
-]
-  const actionCol: TableColumnsType<YModels.InferDataset> = [{
-    dataIndex: 'action',
-    title: t('common.action'),
-    render: (_, record) => <Actions actions={actions(record)} />,
-  }]
+  const cacheDatasets = useSelector(
+    (state: YStates.DatasetState) => state.dataset.dataset
+  )
+  const cacheModels = useSelector(
+    (state: YStates.ModelState) => state.model.model
+  )
+  const actions = (record: YModels.InferDataset): YComponents.Action[] => [
+    {
+      key: "diagnose",
+      label: t("common.action.diagnose"),
+      onclick: () =>
+        history.push(`/home/project/${pid}/diagnose#metrics`, {
+          mid: record.inferModelId,
+        }),
+      icon: <DiagnosisIcon />,
+    },
+    {
+      key: "preview",
+      label: t("common.action.preview"),
+      onclick: () =>
+        history.push(`/home/project/${pid}/dataset/${record.id}/assets`),
+      icon: <EyeOnIcon />,
+    },
+  ]
+  const actionCol: TableColumnsType<YModels.InferDataset> = [
+    {
+      dataIndex: "action",
+      title: t("common.action"),
+      render: (_, record) => <Actions actions={actions(record)} />,
+    },
+  ]
   const columns = [...cols, ...actionCol]
 
   useEffect(() => pid && fetchInferDatasets(), [pid, query])
@@ -47,26 +61,33 @@ const actions = (record: YModels.InferDataset): YComponents.Action[] => [
   useEffect(() => setDatasets(items), [items])
 
   useEffect(() => {
-    setDatasets((datasets) => datasets.map(dataset => {
-      const { inferDatasetId, inferModelId } = dataset
-      const inferModel = inferModelId[0] ? cacheModels[inferModelId[0]] : undefined
-      const inferDataset = inferDatasetId ? cacheDatasets[inferDatasetId] : undefined
-      return { ...dataset, inferModel, inferDataset }
-    }))
+    setDatasets((datasets) =>
+      datasets.map((dataset) => {
+        const { inferDatasetId, inferModelId } = dataset
+        const inferModel = inferModelId[0]
+          ? cacheModels[inferModelId[0]]
+          : undefined
+        const inferDataset = inferDatasetId
+          ? cacheDatasets[inferDatasetId]
+          : undefined
+        return { ...dataset, inferModel, inferDataset }
+      })
+    )
   }, [cacheDatasets, cacheModels, items])
 
   function pageChange(current: number, size: number) {
     const limit = size
     const offset = (current - 1) * size
-    setQuery(query => ({ ...query, current, limit, offset }))
+    setQuery((query) => ({ ...query, current, limit, offset }))
   }
 
   function fetchInferDatasets() {
     return getDatasets({ pid, ...query })
   }
 
-  return <div className={s.inferDataset}>
-    <Table
+  return (
+    <div className={s.inferDataset}>
+      <Table
         columns={columns}
         dataSource={datasets}
         rowKey={(record) => record.id}
@@ -79,7 +100,8 @@ const actions = (record: YModels.InferDataset): YComponents.Action[] => [
           showSizeChanger: true,
         }}
       />
-  </div>
+    </div>
+  )
 }
 
 export default InferDataset
