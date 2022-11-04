eb0b837978b3c28f5be4dbbfd3428936a64109a4
jingyiliu <liu.jingyi@intellif.com>
2022-11-04 11:35:23 +0800

    add more step details in iteration result

--- ymir/backend/src/ymir_app/app/schemas/iteration.py
+++ ymir/backend/src/ymir_app/app/schemas/iteration.py
@@ -1,10 +1,12 @@
-from typing import List, Optional
+from typing import List, Optional, Union, Dict
 from pydantic import BaseModel
 
 from app.constants.state import IterationStage, ResultState, TaskType
 from app.schemas.common import (
     Common,
     DateTimeModelMixin,
+    DatasetResult,
+    ModelResult,
     IdModelMixin,
     IsDeletedModelMixin,
 )
@@ -72,6 +74,8 @@ class IterationStepLite(BaseModel):
     is_finished: Optional[bool]
     state: Optional[ResultState]
     percent: Optional[float]
+    presetting: Optional[Dict]
+    result: Union[DatasetResult, ModelResult, None]
 
     class Config:
         orm_mode = True
