1d5712e183af06f10e9919b60f42e0a5124fb050
jingyiliu <liu.jingyi@intellif.com>
2022-11-16 16:37:34 +0800

    [backend] exclude training and validation dataset_id from last training step for prepare mining step

--- ymir/backend/src/ymir_app/app/libs/iteration_steps.py
+++ ymir/backend/src/ymir_app/app/libs/iteration_steps.py
@@ -56,6 +56,13 @@ class IterationStepTemplate:
         presetting = {"mining_dataset_id": project.mining_dataset_id}
         if not previous_iteration:
             return presetting
+
+        last_training_step = self.get_step("training", previous_iteration)
+        presetting["exclude_datasets"] = [
+            last_training_step.presetting["dataset_id"],
+            last_training_step.presetting["validation_dataset_id"],
+        ]
+
         sticky_parameters = ["mining_strategy", "exclude_last_result", "sampling_count"]
         prior_presetting = self.get_prior_presetting(previous_iteration, sticky_parameters)
         presetting.update(prior_presetting)
