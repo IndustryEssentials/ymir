b7e7ad9cfc6c599b630fa0bfcb526b6c9d6d0eb1
Elliot <li.shanlue@intellif.com>
2022-11-28 17:21:28 +0800

    update form item project types

--- ymir/web/src/components/project/ProjectTypes.tsx
+++ ymir/web/src/components/project/ProjectTypes.tsx
@@ -3,21 +3,18 @@ import React from 'react'
 
 import t from '@/utils/t'
 import RadioGroup from '@/components/form/RadioGroup'
-import { PROJECTTYPES } from '@/constants/project'
+import { getProjectTypes, PROJECTTYPES } from '@/constants/project'
 
 type Props = {
   label?: string
   name?: string
 }
 
-const options = [
-  { label: 'det', value: PROJECTTYPES.ObjectDetection },
-  { label: 'seg', value: PROJECTTYPES.SemanticSegmentation },
-]
+const options = getProjectTypes()
 const ProjectTypes: React.FC<Props> = ({ label, name = 'type', ...rest }) => {
   return (
     <Form.Item name={name} label={t(label ? label : 'project.types.label')} initialValue={PROJECTTYPES.ObjectDetection}>
-      <RadioGroup options={options} prefix='project.types.' {...rest} />
+      <RadioGroup options={options} {...rest} />
     </Form.Item>
   )
 }
