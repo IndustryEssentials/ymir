b090312a7a2bd4b7925eb8e7a38d7ac892da9c59
Elliot <li.shanlue@intellif.com>
2022-11-30 12:40:15 +0800

    update ts typing

--- ymir/web/src/hooks/useRequest.ts
+++ ymir/web/src/hooks/useRequest.ts
@@ -1,12 +1,38 @@
 import { useDispatch } from 'umi'
 import { useRequest as useAhRequest } from 'ahooks'
 import { useEffect } from 'react'
-
-type OptionsType = {
+type OptionsType<TData, TParams extends any[]> = {
   loading?: boolean
+  manual?: boolean
+  onBefore?: (params: TParams) => void
+  onSuccess?: (data: TData, params: TParams) => void
+  onError?: (e: Error, params: TParams) => void
+  onFinally?: (params: TParams, data?: TData, e?: Error) => void
+  defaultParams?: TParams
+  refreshDeps?: (string|number)[]
+  refreshDepsAction?: () => void
+  loadingDelay?: number
+  pollingInterval?: number
+  pollingWhenHidden?: boolean
+  pollingErrorRetryCount?: number
+  refreshOnWindowFocus?: boolean
+  focusTimespan?: number
+  debounceWait?: number
+  debounceLeading?: boolean
+  debounceTrailing?: boolean
+  debounceMaxWait?: number
+  throttleWait?: number
+  throttleLeading?: boolean
+  throttleTrailing?: boolean
+  cacheKey?: string
+  cacheTime?: number
+  staleTime?: number
+  retryCount?: number
+  retryInterval?: number
+  ready?: boolean
 }
 
-const useRequest = (effect: string, options: OptionsType = {}) => {
+const useRequest = <TData, TParams extends any[]>(effect: string, options: OptionsType<TData, TParams> = {}) => {
   const dispatch = useDispatch()
   const { loading = true } = options
   const setLoading = (loading: Boolean) =>
@@ -15,7 +41,7 @@ const useRequest = (effect: string, options: OptionsType = {}) => {
       payload: loading,
     })
 
-  const fetch = (payload: any) =>
+  const fetch = (payload?: any, ...args: any[]): Promise<TData> =>
     dispatch({
       type: effect,
       payload,
