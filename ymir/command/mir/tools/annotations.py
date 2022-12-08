7f11a4e5db567d68d293040cfba22d1e1a18466f
ymir <ymir@intellif.com>
2022-12-08 21:58:23 +0800

    update

--- ymir/command/mir/tools/annotations.py
+++ ymir/command/mir/tools/annotations.py
@@ -116,8 +116,8 @@ def _coco_object_dict_to_annotation(anno_dict: dict, category_id_to_cids: Dict[i
     # ymir defined
     obj_anno.cm = mirpb.ConfusionMatrixType.NotSet
     obj_anno.det_link_id = -1
-    obj_anno.score = anno_dict.get('confidence', -1)
-    obj_anno.anno_quality = anno_dict.get('box_quality', -1)
+    obj_anno.score = float(anno_dict.get('confidence', '-1.0'))
+    obj_anno.anno_quality = float(anno_dict.get('box_quality', '-1.0'))
 
     return obj_anno
 
