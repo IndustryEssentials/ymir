ba80c18c15ef0f1526f01a3fe41e67ddad6a6e43
jingyiliu <liu.jingyi@intellif.com>
2022-12-12 14:32:47 +0800

    use object type in mir pb

--- ymir/backend/src/ymir_controller/controller/invoker/invoker_cmd_pull_image.py
+++ ymir/backend/src/ymir_controller/controller/invoker/invoker_cmd_pull_image.py
@@ -10,12 +10,8 @@ from controller.config import common_task as common_task_config
 from controller.invoker.invoker_cmd_base import BaseMirControllerInvoker
 from controller.utils import checker, utils
 from id_definition.error_codes import CTLResponseCode
+from mir.protos import mir_command_pb2 as mir_cmd_pb
 from proto import backend_pb2
-
-
-class ObjectType(IntEnum):
-    object_detect = 1
-    segmentation = 2
 
 
 class ImageHandler(BaseMirControllerInvoker):
@@ -68,8 +64,8 @@ class ImageHandler(BaseMirControllerInvoker):
         # manifest
         serialized_manifest_config = self.inspect_file_in_docker_image(common_task_config.IMAGE_MANIFEST_PATH)
         manifest_config = json.loads(serialized_manifest_config) if serialized_manifest_config else {}
-        response.enable_livecode = manifest_config.get("support_livecode", False)
-        response.object_type = manifest_config.get("object_type", ObjectType.object_detect.value)
+        response.enable_livecode = bool(manifest_config.get("support_livecode", False))
+        response.object_type = int(manifest_config.get("object_type", mir_cmd_pb.ObjectType.OT_DET_BOX))
 
         if len(response.docker_image_config) == 0:
             return utils.make_general_response(
