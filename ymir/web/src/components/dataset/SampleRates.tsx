ed05892b2265932f66206bfc8bdbd250c24c3e82
Elliot <li.shanlue@intellif.com>
2022-12-29 16:52:25 +0800

    update ts format

--- ymir/web/src/components/dataset/SampleRates.tsx
+++ ymir/web/src/components/dataset/SampleRates.tsx
@@ -1,17 +1,24 @@
-import { useState, useEffect } from "react"
-import { useParams, useSelector } from 'umi'
-import { Button } from "antd"
+import { useState, useEffect, FC } from 'react'
+import { useParams } from 'umi'
+import { useSelector } from 'react-redux'
+import { Button } from 'antd'
 
-import t from "@/utils/t"
+import t from '@/utils/t'
 import useFetch from '@/hooks/useFetch'
-import KeywordRates from "./keywordRates"
-
-function SampleRates({ keywords, dataset, negative, label, progressWidth = 0.5 }) {
-  const { id: pid } = useParams()
-  const [did, setDid] = useState(null)
+import KeywordRates from './KeywordRates'
+type Props = {
+  keywords: string[]
+  dataset?: YModels.Dataset
+  negative?: boolean
+  label?: string
+  progressWidth?: number
+}
+const SampleRates: FC<Props> = ({ keywords, dataset, negative, label, progressWidth = 0.5 }) => {
+  const { id: pid } = useParams<{ id: string }>()
+  const [did, setDid] = useState(0)
   const effect = 'dataset/getNegativeKeywords'
   const [stats, getNegativeKeywords, setStats] = useFetch(effect, {}, true)
-  const loading = useSelector(({ loading }) => loading.effects[effect])
+  const loading = useSelector<YStates.Root, boolean>(({ loading }) => loading.effects[effect])
 
   useEffect(() => {
     dataset?.id && setDid(dataset.id)
@@ -21,9 +28,9 @@ function SampleRates({ keywords, dataset, negative, label, progressWidth = 0.5 }
     setStats({})
   }, [did, keywords])
 
-  const addNegativeInfo = (stat = {}) => {
-    if (!stat.keywords?.length) {
-      return {}
+  const addNegativeInfo = (stat: { keywords?: string[]; count?: { [key: string]: number }; negative?: number } = {}) => {
+    if (!stat?.keywords?.length) {
+      return
     }
     const key = t('dataset.samples.negative')
     return {
@@ -32,7 +39,7 @@ function SampleRates({ keywords, dataset, negative, label, progressWidth = 0.5 }
       count: {
         ...stat.count,
         [key]: stat.negative,
-      }
+      },
     }
   }
 
@@ -43,24 +50,24 @@ function SampleRates({ keywords, dataset, negative, label, progressWidth = 0.5 }
     }
   }, [did, keywords])
 
-  function fetchKeywords(pid, keywords = [], did) {
+  function fetchKeywords(pid: string | number, keywords: string[] = [], did: number | null) {
     keywords.length && getNegativeKeywords({ pid, keywords, did })
   }
 
-  return <div>
-    {label ? <h3 style={{ marginBottom: 10, fontWeight: 'bold' }}>{label}</h3> : null}
-    {negative && !stats.gt ? <div>
-      <Button type="primary"
-        disabled={!did || !keywords?.length}
-        onClick={() => fetchKeywords(pid, keywords, did)}
-        loading={loading}
-      >
-        {t('task.train.btn.calc.negative')}
-      </Button>
-    </div> : null}
-    <KeywordRates title={t('annotation.gt')} stats={addNegativeInfo(stats.gt)} progressWidth={progressWidth} />
-    <KeywordRates title={t('annotation.pred')} stats={addNegativeInfo(stats.pred)} progressWidth={progressWidth} />
-  </div>
+  return (
+    <div>
+      {label ? <h3 style={{ marginBottom: 10, fontWeight: 'bold' }}>{label}</h3> : null}
+      {negative && !stats.gt ? (
+        <div>
+          <Button type="primary" disabled={!did || !keywords?.length} onClick={() => fetchKeywords(pid, keywords, did)} loading={loading}>
+            {t('task.train.btn.calc.negative')}
+          </Button>
+        </div>
+      ) : null}
+      <KeywordRates title={t('annotation.gt')} stats={addNegativeInfo(stats.gt)} progressWidth={progressWidth} />
+      <KeywordRates title={t('annotation.pred')} stats={addNegativeInfo(stats.pred)} progressWidth={progressWidth} />
+    </div>
+  )
 }
 
 export default SampleRates
--- ymir/web/src/components/project/TestingSet.tsx
+++ ymir/web/src/components/project/TestingSet.tsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react"
-import useFetch from "@/hooks/useFetch"
-import { Col, Popover, Row, Tag } from "antd"
-import SampleRates from "@/components/dataset/sampleRates"
-import t from "@/utils/t"
-import s from "../detail.less"
-
-export const TestingSet = ({ project }) => {
+import React, { FC, ReactElement, useEffect } from 'react'
+import useFetch from '@/hooks/useFetch'
+import { Col, Popover, Row, Tag } from 'antd'
+import SampleRates from '@/components/dataset/sampleRates'
+import t from '@/utils/t'
+import s from '../detail.less'
+type Props = {
+  project: YModels.Project
+}
+const TestingSet: FC<Props> = ({ project }) => {
   const [datasets, fetchDatasets] = useFetch('dataset/batchDatasets', [])
 
   useEffect(() => {
@@ -13,44 +15,59 @@ export const TestingSet = ({ project }) => {
   }, [project.testingSets])
 
   function renderProjectTestingSetLabel() {
-    const getDsName = (ds = {}) => ds.name ? (ds.name + ' ' + (ds.versionName || '')) : ''
-    const getAssetCount = (ds = {}) => ds.assetCount ? ds.assetCount : ''
+    const getDsName = (ds: YModels.Dataset) => (ds.name ? ds.name + ' ' + (ds.versionName || '') : '')
+    const getAssetCount = (ds: YModels.Dataset) => (ds.assetCount ? ds.assetCount : '')
     const getDatasetGroup = (dsg = []) => {
-      return dsg.map(ds => {
+      return dsg.map((ds) => {
         return {
           name: getDsName(ds),
           assetCount: getAssetCount(ds),
-          dataset: ds
+          dataset: ds,
         }
       })
     }
-    const maps = [
-      { label: 'project.add.form.testing.set', datasetGroup: getDatasetGroup(datasets) }
-    ]
+    const maps = [{ label: 'project.add.form.testing.set', datasetGroup: getDatasetGroup(datasets) }]
 
     return maps.map(({ label, datasetGroup }) => {
-      return <Row key={label} className={s.testingSets} align='left'>
-        <Col className={s.datasetTitle}>
-          <span>{t(label)}: </span>
-        </Col>
-        <Col flex={1}>
-          {datasetGroup.map(({ name, dataset, assetCount }) => {
-            const rlabel = name ? <Tag className={s.nameTag}>{name}{assetCount ? `(${assetCount})` : ''}</Tag> : ''
-            return <span key={name} title={name}>{dataset ? renderPop(rlabel, dataset) : rlabel}</span>
-          })}
-        </Col>
-      </Row>
+      return (
+        <Row key={label} className={s.testingSets}>
+          <Col className={s.datasetTitle}>
+            <span>{t(label)}: </span>
+          </Col>
+          <Col flex={1}>
+            {datasetGroup.map(({ name, dataset, assetCount }) => {
+              const rlabel = name ? (
+                <Tag className={s.nameTag}>
+                  {name}
+                  {assetCount ? `(${assetCount})` : ''}
+                </Tag>
+              ) : (
+                ''
+              )
 
+              return (
+                <span key={name} title={name}>
+                  {dataset ? renderPop(rlabel, dataset) : rlabel}
+                </span>
+              )
+            })}
+          </Col>
+        </Row>
+      )
     })
   }
 
-  function renderPop(label, dataset = {}) {
+  function renderPop(label: string | ReactElement, dataset: YModels.Dataset) {
     dataset.project = project
     const content = <SampleRates keywords={project?.keywords} dataset={dataset} progressWidth={0.4} />
-    return <Popover content={content} overlayInnerStyle={{ minWidth: 500 }}>
-      <span>{label}</span>
-    </Popover>
+    return (
+      <Popover content={content} overlayInnerStyle={{ minWidth: 500 }}>
+        <span>{label}</span>
+      </Popover>
+    )
   }
 
-  return datasets.length ? renderProjectTestingSetLabel() : null
+  return datasets.length ? <>{renderProjectTestingSetLabel()}</> : null
 }
+
+export default TestingSet
