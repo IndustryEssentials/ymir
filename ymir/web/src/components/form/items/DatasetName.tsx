7699e0cc663c95632137ad80ba997682a5620124
Elliot <li.shanlue@intellif.com>
2023-01-04 15:43:20 +0800

    update dataset name typing

--- ymir/web/src/components/form/items/DatasetName.tsx
+++ ymir/web/src/components/form/items/DatasetName.tsx
@@ -1,24 +1,18 @@
-import { Form, Input } from "antd"
+import { Form, Input } from 'antd'
 import t from '@/utils/t'
 
-const DatasetName = ({
-  name = 'name',
-  itemProps = {},
-  inputProps = {},
-  prefix = 'dataset.add.form.name',
-}) => {
-  const rules = [
-    { required: true, whitespace: true, message: t(`${prefix}.required`) },
-    { type: 'string', min: 2, max: 80 },
-  ]
-  return <Form.Item
+const DatasetName = ({ name = 'name', itemProps = {}, inputProps = {}, prefix = 'dataset.add.form.name' }) => (
+  <Form.Item
     {...itemProps}
     label={t(`${prefix}.label`)}
     name={name}
-    rules={rules}
+    rules={[
+      { required: true, whitespace: true, message: t(`${prefix}.required`) },
+      { type: 'string', min: 2, max: 80 },
+    ]}
   >
     <Input autoComplete={'off'} placeholder={t(`${prefix}.placeholder`)} {...inputProps} allowClear />
   </Form.Item>
-}
+)
 
 export default DatasetName
