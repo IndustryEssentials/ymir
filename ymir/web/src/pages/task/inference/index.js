77ab7b7b2aa0c635a54279ad320328bdb22c681c
Elliot <li.shanlue@intellif.com>
2023-01-06 12:05:51 +0800

    turn to infer result page after inference

--- ymir/web/src/pages/task/inference/index.js
+++ ymir/web/src/pages/task/inference/index.js
@@ -193,8 +193,7 @@ function Inference({...func }) {
         message.warn(t('task.inference.failure.some'))
       }
       await func.clearCache()
-      const groups = result.map((item) => item.result_dataset?.dataset_group_id || '')
-      history.replace(`/home/project/${pid}/dataset#${groups.join(',')}`)
+      history.replace(`/home/project/${pid}/diagnose`)
     }
   }
 
