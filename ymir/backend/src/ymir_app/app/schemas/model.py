247337fb26ed9b000798080c341c7f510cd1600d
IJtLJZ8Rm4Yr <90443055+IJtLJZ8Rm4Yr@users.noreply.github.com>
2023-01-11 16:04:21 +0800

    [backend] add object_type in model (#1495)
    * [backend] add object_type in model
    
    * add test

--- ymir/backend/src/ymir_app/app/schemas/model.py
+++ ymir/backend/src/ymir_app/app/schemas/model.py
@@ -3,7 +3,7 @@ import json
 
 from pydantic import BaseModel, Field, root_validator, validator
 from app.config import settings
-from app.constants.state import ResultState, TaskType
+from app.constants.state import ResultState, TaskType, ObjectType
 from app.schemas.common import (
     Common,
     DateTimeModelMixin,
@@ -72,6 +72,7 @@ class ModelInDBBase(IdModelMixin, DateTimeModelMixin, IsDeletedModelMixin, Model
     is_visible: bool
     related_stages: List[ModelStageInDBBase]
     recommended_stage: Optional[int] = None
+    object_type: Optional[ObjectType] = ObjectType.object_detect
 
     class Config:
         orm_mode = True
