24ad5503b53fc999740a6333ce2dc93ccb66819a
Elliot <li.shanlue@intellif.com>
2023-01-13 20:05:46 +0800

    forbidden browser cache

--- ymir/web/src/config/config.ts
+++ ymir/web/src/config/config.ts
@@ -1,24 +1,25 @@
-import { defineConfig } from "umi"
-import locale from "./locale"
-import routes from "./routes"
+import { defineConfig } from 'umi'
+import locale from './locale'
+import routes from './routes'
 import theme from './theme'
 
 const Config = defineConfig({
   locale,
   routes,
   theme,
-  outputPath: "ymir",
+  outputPath: 'ymir',
   hash: true,
   nodeModulesTransform: {
-    type: "none",
+    type: 'none',
   },
-  headScripts: [{ src: '/config/config.js' } ],
+  metas: [
+    { httpEquiv: 'Cache-Control', content: 'no-cache, no-store, must-revalidate' },
+    { httpEquiv: 'Pragma', content: 'no-cache' },
+    { httpEquiv: 'Expires', content: '0' },
+  ],
+  headScripts: [{ src: '/config/config.js' }],
   fastRefresh: {},
-
-  // open mfsu
-  // webpack5: false,
   dynamicImport: {},
-  // mfsu: {},
 })
 
 export default Config
