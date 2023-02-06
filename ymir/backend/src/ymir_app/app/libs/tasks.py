d74cd0c06ff3f6133bde90a92644faa3375290f3
jingyiliu <liu.jingyi@intellif.com>
2023-02-06 10:56:27 +0800

    limit batch update tasks

--- ymir/backend/src/ymir_app/app/libs/tasks.py
+++ ymir/backend/src/ymir_app/app/libs/tasks.py
@@ -39,6 +39,7 @@ from app.utils.cache import CacheClient
 from app.utils.err import retry
 from app.utils.ymir_controller import ControllerClient, gen_task_hash
 from app.utils.ymir_viz import VizClient
+from app.utils.data import split_seq
 from common_utils.labels import UserLabels
 
 
@@ -72,9 +73,11 @@ async def batch_update_task_status(events: List[Tuple[str, Dict]]) -> List[str]:
         payloads.append(schemas.TaskUpdateStatus.from_monitor_event(msg["payload"]).dict())
 
     async with aiohttp.ClientSession() as session:
-        tasks = [post_task_update(session, payload) for payload in payloads]
-        results = await asyncio.gather(*tasks, return_exceptions=True)
-        success_id_selectors = [not isinstance(res, Exception) for res in results]
+        success_id_selectors = []
+        for payload_group in split_seq(payloads):
+            tasks = [post_task_update(session, payload) for payload in payload_group]
+            results = await asyncio.gather(*tasks, return_exceptions=True)
+            success_id_selectors += [not isinstance(res, Exception) for res in results]
         return list(itertools.compress(ids, success_id_selectors))
 
 
--- ymir/backend/src/ymir_app/tests/utils/test_data.py
+++ ymir/backend/src/ymir_app/tests/utils/test_data.py
@@ -7,3 +7,16 @@ class TestGroupby:
         groups = list(m.groupby(items, "rank"))
         for i in range(4):
             assert groups[i][0] == i + 1
+
+
+class TestSplitSeq:
+    def test_split_seq(self, mocker):
+        seq = (i for i in range(5))
+        result = list(m.split_seq(seq, 2))
+        assert len(result) == 3
+        assert result[0] == [0, 1]
+        assert result[1] == [2, 3]
+        assert result[2] == [4]
+
+        result = list(m.split_seq(range(2), 2))
+        assert result == [[0, 1]]
