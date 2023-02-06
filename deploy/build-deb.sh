8e99948bfb339956d8a4d51d8a253560028ab721
ymir <ymir@intellif.com>
2023-02-06 21:35:17 +0800

    move build-deb.sh to deploy dir

--- deploy/build-deb.sh
+++ deploy/build-deb.sh
@@ -2,15 +2,17 @@
 
 set -e
 
+YMIR_VERSION="2.1.0"
+
 mkdir deb-workplace/DEBIAN/ -p
 mkdir deb-workplace/usr/ymir -p
-cp ymir.sh .env docker-compose.dev.yml docker-compose.labelfree.yml \
-   docker-compose.label_studio.yml docker-compose.modeldeploy.yml docker-compose.yml \
+cp ../ymir.sh ../.env ../docker-compose.dev.yml ../docker-compose.labelfree.yml \
+   ../docker-compose.label_studio.yml ../docker-compose.modeldeploy.yml ../docker-compose.yml \
    deb-workplace/usr/ymir
 
 cat <<EOT > deb-workplace/DEBIAN/control
 Package: YMIR
-Version: 2.1.0
+Version: ${YMIR_VERSION}
 Priority: optional
 Depends:
 Suggests:
@@ -27,7 +29,6 @@ EOT
 
 chmod +x deb-workplace/DEBIAN/postinst
 
-mkdir deploy
-dpkg-deb -b deb-workplace deploy/ymir-linux-amd64.deb
+dpkg-deb -b deb-workplace ymir-linux-amd64-${YMIR_VERSION}.deb
 
 rm -rf deb-workplace
