d72d89b7a9a4f930c2e14df8237fb2029e415e4c
Elliot <li.shanlue@intellif.com>
2023-02-17 13:41:01 +0800

    fixed merge strategy for training

--- ymir/web/src/hooks/useDuplicatedCheck.tsx
+++ ymir/web/src/hooks/useDuplicatedCheck.tsx
@@ -1,50 +1,63 @@
-import { useCallback, useEffect, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { Modal, Radio } from 'antd'
 
 import t from '@/utils/t'
-import { MERGESTRATEGY } from '@/constants/dataset'
+import { MERGESTRATEGYFORTRAIN } from '@/constants/dataset'
 import useFetch from '@/hooks/useFetch'
+
+type ContentProps = {
+  duplicated: number
+  strategy?: MERGESTRATEGYFORTRAIN
+  disabled?: MERGESTRATEGYFORTRAIN
+  onChange?: (s: MERGESTRATEGYFORTRAIN) => void
+}
 
 const { confirm, error } = Modal
 
 const options = [
-  { value: MERGESTRATEGY.HOST, label: 'task.train.duplicated.option.train' },
-  { value: MERGESTRATEGY.GUEST, label: 'task.train.duplicated.option.validation' }
+  { value: MERGESTRATEGYFORTRAIN.ASTRAIN, label: 'task.train.duplicated.option.train' },
+  { value: MERGESTRATEGYFORTRAIN.ASVALIDATION, label: 'task.train.duplicated.option.validation' },
 ]
 
-const ContentRender = ({ duplicated, strategy, disabled, onChange = () => { } }) => {
+const ContentRender: FC<ContentProps> = ({ duplicated, strategy, disabled, onChange = () => {} }) => {
   const [s, setS] = useState(strategy)
-  useEffect(() => onChange(s), [s])
-  return <div>
-    <span>{t('task.train.duplicated.tip', { duplicated })}</span>
-    <Radio.Group
-      value={s}
-      onChange={({ target: { value } }) => { setS(value) }}
-      options={options.map(opt => ({
-        ...opt,
-        disabled: disabled === opt.value,
-        label: <p>{t(opt.label)}</p>
-      }))}
-    />
-  </div>
+  useEffect(() => {
+    s && onChange(s)
+  }, [s])
+  return (
+    <div>
+      <span>{t('task.train.duplicated.tip', { duplicated })}</span>
+      <Radio.Group
+        value={s}
+        onChange={({ target: { value } }) => {
+          setS(value)
+        }}
+        options={options.map((opt) => ({
+          ...opt,
+          disabled: disabled === opt.value,
+          label: <p>{t(opt.label)}</p>,
+        }))}
+      />
+    </div>
+  )
 }
 
-const useDuplicatedCheck = (onChange = () => { }) => {
+const useDuplicatedCheck = (onChange: (s: MERGESTRATEGYFORTRAIN) => void = () => {}): ((t: YModels.Dataset, v: YModels.Dataset) => void) => {
   const [_, checkDuplication] = useFetch('dataset/checkDuplication', 0)
-  let strategy = MERGESTRATEGY.NORMAL
+  let strategy = MERGESTRATEGYFORTRAIN.STOP
 
   const ok = () => {
     onChange(strategy)
   }
 
-  const check = async (trainDataset, validationDataset) => {
+  const check = async (trainDataset: YModels.Dataset, validationDataset: YModels.Dataset) => {
     const result = await checkDuplication({ trainSet: trainDataset?.id, validationSet: validationDataset?.id })
     if (typeof result !== 'undefined') {
       checkHandle(result, trainDataset, validationDataset)
     }
   }
 
-  const checkHandle = (duplicated, trainDataset, validationDataset) => {
+  const checkHandle = (duplicated: number, trainDataset: YModels.Dataset, validationDataset: YModels.Dataset) => {
     if (!duplicated) {
       return ok()
     }
@@ -59,21 +72,14 @@ const useDuplicatedCheck = (onChange = () => { }) => {
     PopConfirm(duplicated, allValidation, allTrain)
   }
 
-  const PopConfirm = (duplicated, allValidation, allTrain) => {
-
-    const disabled = allValidation ? MERGESTRATEGY.GUEST : (allTrain ? MERGESTRATEGY.HOST : null)
-    const value = allTrain ? MERGESTRATEGY.GUEST : MERGESTRATEGY.HOST
+  const PopConfirm = (duplicated: number, allValidation: boolean, allTrain: boolean) => {
+    const disabled = allValidation ? MERGESTRATEGYFORTRAIN.ASVALIDATION : allTrain ? MERGESTRATEGYFORTRAIN.ASTRAIN : undefined
+    const value = allTrain ? MERGESTRATEGYFORTRAIN.ASVALIDATION : MERGESTRATEGYFORTRAIN.ASTRAIN
     strategy = value
     confirm({
       visible: true,
-      content: <ContentRender
-        duplicated={duplicated}
-        disabled={disabled}
-        strategy={value}
-        onChange={value => (strategy = value)}
-      />,
+      content: <ContentRender duplicated={duplicated} disabled={disabled} strategy={value} onChange={(value) => (strategy = value)} />,
       onOk: ok,
-      destroyOnClose: true,
     })
   }
 
