82a403148db78628546db26ea02c2a20125cd1f5
Phoenix <89957974+phoenix-xhuang@users.noreply.github.com>
2023-02-21 11:28:18 +0800

    [viewer] update hist x-axis (#1582)
    * add ex_cm_types field
    
    * fix
    
    * update

--- ymir/backend/src/ymir_hel/common/constants/histograms.go
+++ ymir/backend/src/ymir_hel/common/constants/histograms.go
@@ -18,13 +18,15 @@ type MirHist struct {
 	Ops           interface{}          `json:"-" bson:"-"`
 	Output        *[]map[string]string `json:"-" bson:"output"`
 	SkipUnwind    bool                 `json:"-" bson:"-"`
+	DecDigits     int16                `json:"-" bson:"-"`
 }
 
 func (h *MirHist) BuildMirHist(bucket *map[string]int32) {
 	h.Output = &[]map[string]string{}
 	h.SparseBuckets = bucket
 	for _, LowerBND := range h.LowerBNDs {
-		histKey := fmt.Sprintf("%.2f", LowerBND)
+		histKeyFormat := fmt.Sprintf("%%.%df", h.DecDigits)
+		histKey := fmt.Sprintf(histKeyFormat, LowerBND)
 		value := "0"
 		if data, ok := (*h.SparseBuckets)[histKey]; ok {
 			value = fmt.Sprintf("%d", data)
@@ -40,60 +42,74 @@ func (h *MirHist) MarshalJSON() ([]byte, error) {
 
 // Pre-defined historgram schemas.
 var ConstAssetsMirHist map[string]MirHist = map[string]MirHist{
-	"quality": {Ops: "$quality", LowerBNDs: []float64{0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0}},
-	"bytes": {Ops: bson.M{"$divide": bson.A{"$metadata.byte_size", BytesPerMB}},
+	"quality": {
+		Ops: "$quality", DecDigits: 2,
+		LowerBNDs: []float64{0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0}},
+	"bytes": {Ops: bson.M{"$divide": bson.A{"$metadata.byte_size", BytesPerMB}}, DecDigits: 2,
 		LowerBNDs: []float64{0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0}},
-	"area": {Ops: bson.M{"$multiply": bson.A{"$metadata.width", "$metadata.height"}},
+	"area": {Ops: bson.M{"$multiply": bson.A{"$metadata.width", "$metadata.height"}}, DecDigits: 0,
 		LowerBNDs: []float64{0, 100000, 500000, 1000000, 2000000, 4000000, 6000000, 8000000}},
-	"hw_ratio": {Ops: bson.M{"$divide": bson.A{"$metadata.height", "$metadata.width"}},
+	"hw_ratio": {Ops: bson.M{"$divide": bson.A{"$metadata.height", "$metadata.width"}}, DecDigits: 2,
 		LowerBNDs: []float64{0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5}},
 }
 
 var ConstGtMirHist map[string]MirHist = map[string]MirHist{
-	"quality": {Ops: "$gt.anno_quality", LowerBNDs: []float64{0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0}},
+	"quality": {
+		Ops:       "$gt.anno_quality",
+		DecDigits: 2,
+		LowerBNDs: []float64{0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0},
+	},
 	"box_area": {
-		Ops:       bson.M{"$multiply": bson.A{"$gt.box.w", "$gt.box.h"}},
+		Ops: bson.M{"$multiply": bson.A{"$gt.box.w", "$gt.box.h"}}, DecDigits: 0,
 		LowerBNDs: []float64{0, 1, 50, 500, 2500, 5000, 10000, 50000, 100000, 200000},
 	},
 	"box_area_ratio": {
 		Ops: bson.M{"$divide": bson.A{bson.M{"$multiply": bson.A{"$gt.box.w", "$gt.box.h"}},
-			bson.M{"$multiply": bson.A{"$metadata.width", "$metadata.height"}}}},
+			bson.M{"$multiply": bson.A{"$metadata.width", "$metadata.height"}}}}, DecDigits: 2,
 		LowerBNDs: []float64{0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0}},
-	"mask_area": {Ops: "$gt.mask_area", LowerBNDs: []float64{0, 1, 50, 500, 2500, 5000, 10000, 50000, 100000, 200000}},
+	"mask_area": {
+		Ops:       "$gt.mask_area",
+		DecDigits: 0,
+		LowerBNDs: []float64{0, 1, 50, 500, 2500, 5000, 10000, 50000, 100000, 200000},
+	},
 	"obj_counts": {
-		Ops:        bson.M{"$size": "$gt"},
-		LowerBNDs:  []float64{0, 1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50},
+		Ops: bson.M{"$size": "$gt"}, DecDigits: 0,
+		LowerBNDs:  []float64{0, 1, 2, 3, 4, 5, 10, 15, 20, 30, 40, 50, 100},
 		SkipUnwind: true,
 	},
 	"class_counts": {
-		Ops:        bson.M{"$size": "$gt_class_ids"},
-		LowerBNDs:  []float64{0, 1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 100},
+		Ops: bson.M{"$size": "$gt_class_ids"}, DecDigits: 0,
+		LowerBNDs:  []float64{0, 1, 2, 3, 4, 5, 10, 15, 20, 30, 40, 50, 100},
 		SkipUnwind: true,
 	},
 }
 
 var ConstPredMirHist map[string]MirHist = map[string]MirHist{
-	"quality": {Ops: "$pred.anno_quality", LowerBNDs: []float64{0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0}},
+	"quality": {
+		Ops:       "$pred.anno_quality",
+		DecDigits: 2,
+		LowerBNDs: []float64{0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0},
+	},
 	"box_area": {
-		Ops:       bson.M{"$multiply": bson.A{"$pred.box.w", "$pred.box.h"}},
+		Ops: bson.M{"$multiply": bson.A{"$pred.box.w", "$pred.box.h"}}, DecDigits: 0,
 		LowerBNDs: []float64{0, 1, 50, 500, 2500, 5000, 10000, 50000, 100000, 200000},
 	},
 	"box_area_ratio": {
 		Ops: bson.M{"$divide": bson.A{bson.M{"$multiply": bson.A{"$pred.box.w", "$pred.box.h"}},
-			bson.M{"$multiply": bson.A{"$metadata.width", "$metadata.height"}}}},
+			bson.M{"$multiply": bson.A{"$metadata.width", "$metadata.height"}}}}, DecDigits: 2,
 		LowerBNDs: []float64{0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0}},
 	"mask_area": {
-		Ops:       "$pred.mask_area",
+		Ops: "$pred.mask_area", DecDigits: 0,
 		LowerBNDs: []float64{0, 1, 50, 500, 2500, 5000, 10000, 50000, 100000, 200000},
 	},
 	"obj_counts": {
-		Ops:        bson.M{"$size": "$pred"},
-		LowerBNDs:  []float64{0, 1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50},
+		Ops: bson.M{"$size": "$pred"}, DecDigits: 0,
+		LowerBNDs:  []float64{0, 1, 2, 3, 4, 5, 10, 15, 20, 30, 40, 50, 100},
 		SkipUnwind: true,
 	},
 	"class_counts": {
-		Ops:        bson.M{"$size": "$pred_class_ids"},
-		LowerBNDs:  []float64{0, 1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 100},
+		Ops: bson.M{"$size": "$pred_class_ids"}, DecDigits: 0,
+		LowerBNDs:  []float64{0, 1, 2, 3, 4, 5, 10, 15, 20, 30, 40, 50, 100},
 		SkipUnwind: true,
 	},
 }
