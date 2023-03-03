a4ebed31f9b94daef9f5e33f0ced07d0be970c15
jingyiliu <liu.jingyi@intellif.com>
2023-03-03 16:19:15 +0800

    fix tests

--- ymir/backend/src/ymir_app/tests/libs/test_datasets.py
+++ ymir/backend/src/ymir_app/tests/libs/test_datasets.py
@@ -1,5 +1,4 @@
 from typing import Any
-from random import randint
 from app.libs import datasets as m
 from tests.utils.utils import random_lower_string
 
@@ -14,38 +13,3 @@ class TestImportDatasetPaths:
         assert p.pred_dir == str(input_path / "pred")
         assert p.asset_dir == str(input_path / "images")
         assert p.gt_dir is None
-
-
-class TestEvaluateDataset:
-    def test_evaluate_datasets(self, mocker: Any) -> None:
-        user_id = randint(100, 200)
-        project_id = randint(1000, 2000)
-        confidence_threshold = 0.233
-        iou = 0.5
-        require_average_iou = True
-        need_pr_curve = True
-        ctrl = mocker.Mock(evaluate_dataset=mocker.Mock(return_value={}))
-        user_labels = mocker.Mock()
-        datasets_mapping = {"a": 1, "b": 2}
-        m.evaluate_datasets(
-            ctrl,
-            user_id,
-            project_id,
-            user_labels,
-            confidence_threshold,
-            iou,
-            require_average_iou,
-            need_pr_curve,
-            "main_ck",
-            datasets_mapping,
-        )
-
-        ctrl.evaluate_dataset.assert_called()
-
-
-class TestConvertIou:
-    def test_convert_to_iou_thrs_interval(self, mocker: Any) -> None:
-        assert m.convert_to_iou_thrs_interval(None, True) == "0.5:0.95:0.05"
-        assert m.convert_to_iou_thrs_interval(0.1, True) == "0.1:0.95:0.05"
-        assert m.convert_to_iou_thrs_interval(0.1, False) == "0.1"
-        assert m.convert_to_iou_thrs_interval(None, False) is None
