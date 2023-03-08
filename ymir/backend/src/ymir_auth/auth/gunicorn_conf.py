9b4fc13f33700fab540ab5e8aea40d3d18d5f151
jingyiliu <liu.jingyi@intellif.com>
2023-03-08 10:50:27 +0800

    update port to 8088 for auth

--- ymir/backend/src/ymir_auth/auth/gunicorn_conf.py
+++ ymir/backend/src/ymir_auth/auth/gunicorn_conf.py
@@ -10,7 +10,7 @@ if max_workers_str:
 web_concurrency_str = os.getenv("WEB_CONCURRENCY", None)
 
 host = os.getenv("HOST", "0.0.0.0")
-port = os.getenv("PORT", "80")
+port = os.getenv("PORT", "8088")
 bind_env = os.getenv("BIND", None)
 use_loglevel = os.getenv("LOG_LEVEL", "info")
 if bind_env:
