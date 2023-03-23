903bc57256ef9deeb8b4b2542e20a2d109f405a6
jingyiliu <liu.jingyi@intellif.com>
2023-03-23 14:00:43 +0800

    [auth] add name in JWT token

--- ymir/backend/src/ymir_auth/auth/api/api_v1/endpoints/login.py
+++ ymir/backend/src/ymir_auth/auth/api/api_v1/endpoints/login.py
@@ -63,6 +63,7 @@ def login_access_token(
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     token_payload = {
         "id": user.id,
+        "name": user.username,
         "email": user.email,
         "role": role.name,
     }
