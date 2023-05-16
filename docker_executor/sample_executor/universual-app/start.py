e9633ae2482eb1db4a8de18b1123f4f70bce323a
ymir <ymir@intellif.com>
2023-05-16 20:17:29 +0800

    semantic segmentation sample docker image -> universual sample docker image;

--- docker_executor/sample_executor/universual-app/start.py
+++ docker_executor/sample_executor/universual-app/start.py
@@ -148,6 +148,7 @@ def _run_infer(env_config: env.EnvConfig) -> None:
     class_names = executor_config['class_names']
     idle_seconds: float = executor_config.get('idle_seconds', 60)
     crash_code: bool = executor_config.get('crash_code', ExecutorReturnCode.RC_EXEC_NO_ERROR)
+    prompt: str = executor_config.get('prompt', None)
 
     #! use `logging` or `print` to write log to console
     logging.info(f"infer config: {executor_config}")
@@ -172,7 +173,7 @@ def _run_infer(env_config: env.EnvConfig) -> None:
             'height': 300,  # width and height should get from real image
         })
         for category_index, cname in enumerate(class_names):
-            annotations_list.append({
+            annotation = {
                 'id': len(annotations_list) + 1,
                 'category_id': category_index + 1,
                 'image_id': image_index + 1,
@@ -180,7 +181,10 @@ def _run_infer(env_config: env.EnvConfig) -> None:
                 'segmentation': [[50, 100, 100, 50, 150, 100, 100, 150]],
                 "area": 0,  # mask area
                 'confidence': 1  # confidence of this segmentation, 0 <= conf <= 1
-            })
+            }
+            if prompt:
+                annotation['prompt'] = f"box prompt: {prompt}"
+            annotations_list.append(annotation)
 
     coco_dict = {
         'images': images_list,
