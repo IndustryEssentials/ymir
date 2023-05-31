c1e57876970b958dc2d7441d0a744c1fe4adac59
Elliot <li.shanlue@intellif.com>
2023-05-31 19:15:17 +0800

    update single infer page

--- ymir/web/src/locales/modules/llmm.ts
+++ ymir/web/src/locales/modules/llmm.ts
@@ -1,6 +1,8 @@
 const llmm = {
   'llmm.tabs.single': { cn: '单张图片验证', en: 'Single Picture Inference' },
   'llmm.tabs.dataset': { cn: '数据集推理', en: 'Dataset Inference' },
+  'llmm.prompt.label': { cn: '文本描述', en: 'Text Prompt' },
+  'llmm.prompt.placeholder': { cn: '请输入文本描述，将为您生成相应的标注。', en: 'Please input text prompt, for generate responding annotations.' },
 }
 
 export default llmm
--- ymir/web/src/pages/llmm/components/SingleInfer.tsx
+++ ymir/web/src/pages/llmm/components/SingleInfer.tsx
@@ -1,11 +1,159 @@
 import { FC, useState, useEffect } from 'react'
-import { useSelector } from 'umi'
+import { useHistory, useParams, useSelector } from 'umi'
 import t from '@/utils/t'
-type Props = {}
-const SingleInfer: FC<Props> = ({}) => {
-  const [url, setUrl] = useState<string>()
+import { Button, Card, Col, Descriptions, Form, Input, InputNumber, Row, Slider, Tag } from 'antd'
+import { Annotation, Project } from '@/constants'
+import AssetAnnotation, { Asset } from '@/components/dataset/asset/AssetAnnotations'
+import { isSemantic, ObjectType } from '@/constants/objectType'
+import useRequest from '@/hooks/useRequest'
 
-  useEffect(() => {}, [])
-  return <></>
+import styles from '../inference.less'
+import ImgDef from '@/assets/img_def.png'
+import { NavDatasetIcon, NoXlmxIcon, SearchEyeIcon } from '@/components/common/Icons'
+import { UpOutlined, DownOutlined } from '@ant-design/icons'
+import Uploader, { UploadFile } from '@/components/form/uploader'
+import { generateDatasetColors } from '@/constants/asset'
+import DockerConfigForm from '@/components/form/items/DockerConfig'
+import { getConfig, TYPES } from '@/constants/image'
+import PromptInput from './PromptInput'
+import { transHyperParams } from './_utils'
+
+type Props = {}
+
+const SingleInfer: FC<Props> = ({}) => {
+  const params = useParams<{ id: string }>()
+  const pid = Number(params.id)
+  const [virtualAsset, setVirtualAsset] = useState<Asset>()
+  const IMGSIZELIMIT = 10
+  const history = useHistory()
+  const [confidence, setConfidence] = useState(20)
+  const [annotations, setAnnotations] = useState<Annotation[]>([])
+  const [selectedKeywords, setSelectedKeywords] = useState<string[]>([])
+  const [form] = Form.useForm()
+  const [seniorConfig, setSeniorConfig] = useState<{ [key: string]: any }>({})
+  const { groundedSAM: image } = useSelector(({ image }) => image)
+  const { data: verifyResult, run: verify } = useRequest<Annotation[]>('model/verify')
+  const { data: project, run: getProject } = useRequest<Project, [{ id: number }]>('project/getProject', {
+    cacheKey: 'getProject',
+    loading: false,
+  })
+  useRequest('image/getGroundedSAMImage', {
+    loading: false,
+    manual: false,
+  })
+
+  const renderUploader = (
+    <div className={styles.uploader}>
+      <div className={styles.emptyImg}>
+        <img src={ImgDef} />
+        <p>{t('model.verify.upload.tip')}</p>
+        <p>{t('model.verify.upload.info', { size: IMGSIZELIMIT })}</p>
+        <Uploader
+          key={'uploader'}
+          // className={styles.btn}
+          onChange={(files, url: string) => {
+            setVirtualAsset({ annotations: [], url })
+            setAnnotations([])
+          }}
+          format="img"
+          label={t('model.verify.upload.label')}
+          showUploadList={false}
+          max={IMGSIZELIMIT}
+        />
+      </div>
+    </div>
+  )
+
+  useEffect(() => {
+    if (!image) {
+      return
+    }
+    const config = getConfig(image, TYPES.INFERENCE, ObjectType.InstanceSegmentation)
+    config && setSeniorConfig(config.config)
+  }, [image])
+
+  useEffect(() => {
+    const confidenceFilter = (anno: Annotation) => isSemantic(project?.type) || !anno.score || anno.score * 100 > confidence
+    const annos = annotations.length ? annotations.filter((anno) => confidenceFilter(anno) && selectedKeywords.indexOf(anno.keyword) > -1) : []
+    setVirtualAsset((asset) => ({ ...asset, annotations: annos }))
+  }, [confidence, annotations, selectedKeywords, project?.type])
+
+  useEffect(() => {
+    if (verifyResult) {
+      const all = verifyResult || []
+      const keywords = [...new Set(all.map((anno) => anno.keyword))]
+      const colors = generateDatasetColors(keywords)
+      setAnnotations(all.map((anno) => ({ ...anno, color: colors[anno.keyword] })))
+      all.length && setSelectedKeywords([...new Set(all.map((anno) => anno.keyword))])
+    }
+  }, [verifyResult])
+
+  useEffect(() => {
+    pid && getProject({ id: pid })
+  }, [pid])
+
+  const infer = () => {
+    form
+      .validateFields()
+      .then(() => {
+        const { hyperparam, prompt } = form.getFieldsValue()
+        const config = transHyperParams(hyperparam, prompt)
+
+        verify({ projectId: pid, urls: [virtualAsset?.url], modelStage: [1, 1], image: image?.id, config })
+      })
+      .catch((err) => {})
+  }
+
+  return (
+    <Row className={styles.infoRow} wrap={false}>
+      <Col span={18} className={`${styles.asset_img} scrollbar`}>
+        {virtualAsset?.url ? <AssetAnnotation asset={virtualAsset} /> : renderUploader}
+        {virtualAsset?.url && !isSemantic(project?.type) ? (
+          <Form className={styles.confidence}>
+            <Form.Item label={t('model.verify.confidence')}>
+              <Row gutter={10}>
+                <Col flex={1}>
+                  <Slider
+                    marks={{ 0: '0%', 100: '100%' }}
+                    style={{ width: 200 }}
+                    tipFormatter={(value) => `${value}%`}
+                    value={confidence}
+                    onChange={setConfidence}
+                  />
+                </Col>
+                <Col>
+                  <InputNumber
+                    value={confidence}
+                    style={{ width: 60, borderColor: 'rgba(0, 0, 0, 0.15)', margin: 5, height: 35 }}
+                    precision={0}
+                    min={0}
+                    max={100}
+                    onChange={setConfidence}
+                  />
+                </Col>
+              </Row>
+            </Form.Item>
+          </Form>
+        ) : null}
+      </Col>
+      <Col span={6} className={`${styles.asset_info} scrollbar`}>
+        <Form form={form} className={styles.asset_form}>
+          <PromptInput />
+          <DockerConfigForm form={form} itemProps={{ wrapperCol: { span: 24 } }} seniorConfig={seniorConfig} />
+        </Form>
+        <div>
+          <Button
+            type="primary"
+            disabled={!virtualAsset?.url}
+            icon={<NoXlmxIcon className={styles.modelIcon} />}
+            style={{ marginLeft: 20 }}
+            onClick={() => infer()}
+          >
+            {t('breadcrumbs.model.verify')}
+          </Button>
+        </div>
+      </Col>
+    </Row>
+  )
 }
 export default SingleInfer
