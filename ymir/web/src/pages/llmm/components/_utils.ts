0a871ff35e63c841de249fc838488cf36ab87bcf
Elliot <li.shanlue@intellif.com>
2023-06-02 14:33:56 +0800

    trim prompt

--- ymir/web/src/pages/llmm/components/_utils.ts
+++ ymir/web/src/pages/llmm/components/_utils.ts
@@ -1,8 +1,8 @@
 import { ImageConfig } from '@/constants'
 
-const transHyperParams = (params: { key: string; value: any }[], prompt: string, gpu: number) => {
+const transHyperParams = (params: { key: string; value: any }[], prompt: string = '', gpu: number = 1) => {
   const config = params.reduce<ImageConfig>((prev, { key, value }) => ({ ...prev, [key]: value }), {})
-  config.text_prompt = prompt
+  config.text_prompt = prompt.trim()
   config.gpu_count = gpu
   return config
 }
