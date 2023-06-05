3cfa3212415837e00e056b43b61a03cba79b3352
Elliot <li.shanlue@intellif.com>
2023-06-05 17:50:41 +0800

    add tip to llmm pred

--- ymir/web/src/locales/modules/llmm.ts
+++ ymir/web/src/locales/modules/llmm.ts
@@ -3,6 +3,10 @@ const llmm = {
   'llmm.tabs.dataset': { cn: '数据集推理', en: 'Dataset Inference' },
   'llmm.prompt.label': { cn: '文本描述', en: 'Text Prompt' },
   'llmm.prompt.placeholder': { cn: '请输入文本描述，将为您生成相应的标注。', en: 'Please input text prompt, for generate responding annotations.' },
+  'llmm.prediction.classes.tip': {
+    cn: '多模态大模型镜像能够预测模型训练目标之外的类别',
+    en: "Multi-modal large-scale models can predict categories beyond the model's training targets",
+  },
 }
 
 export default llmm
